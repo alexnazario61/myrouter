59bfe402d8277c40b61916c9b6c495df886c83c2
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:48:47 -0300

    improve

--- app/remessa/_src/Cnab/Remessa/Cnab400/Trailer.php
+++ app/remessa/_src/Cnab/Remessa/Cnab400/Trailer.php
@@ -1,12 +1,17 @@
 <?php
+
 namespace Cnab\Remessa\Cnab400;
 
-class Trailer extends \Cnab\Format\Linha
+use Cnab\Format\Linha;
+use Cnab\Format\YamlLoad;
+use Cnab\Remessa\IArquivo;
+
+class Trailer extends Linha
 {
-	public function __construct(\Cnab\Remessa\IArquivo $arquivo)
+    public function __construct(IArquivo $arquivo)
     {
-    	$codigo_banco = $arquivo->codigo_banco;
-        $yamlLoad = new \Cnab\Format\YamlLoad($codigo_banco);
+        $codigoBanco = $arquivo->getCodigoBanco();
+        $yamlLoad = new YamlLoad($codigoBanco);
         $yamlLoad->load($this, 'cnab400', 'remessa/trailer_arquivo');
-	}
+    }
 }

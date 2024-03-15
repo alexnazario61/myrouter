2232dff7c1f895b9abe709013080dce38b771c81
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:50:18 -0300

    improve

--- vendor/andersondanilo/cnab_php/src/Cnab/Retorno/CodigoOcorrencia.php
+++ vendor/andersondanilo/cnab_php/src/Cnab/Retorno/CodigoOcorrencia.php
@@ -1,38 +1,39 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Cnab\Retorno;
+
+use Cnab\Format\YamlLoad;
 
 class CodigoOcorrencia
 {
     /**
-     * This method returns the name of the ocorrence code for a given bank code and ocorrence code.
+     * Returns the name of the ocorrence code for a given bank code and ocorrence code.
      * It currently only supports the CNAB400 format.
      *
-     * @param int $codigo_banco The bank code
-     * @param int $codigo_ocorrencia The ocorrence code
-     * @param string $format The format of the ocorrence code, either 'cnab400' (default)
-     * @return string The name of the ocorrence code
-     *
-     * @throws \Exception If the format is not supported
+     * @param int                     $codigoBanco     The bank code
+     * @param int                     $codigoOcorrencia The ocorrence code
+     * @param string                  $format          The format of the ocorrence code, either 'cnab400' (default)
+     * @return string                 The name of the ocorrence code
+     * @throws \Exception If the format is not supported or the bank code or ocorrence code is invalid.
      */
-    public function getNome($codigo_banco, $codigo_ocorrencia, $format='cnab400')
+    public function getNome(int $codigoBanco, int $codigoOcorrencia, string $format = 'cnab400'): string
     {
         // Convert the format, bank code, and ocorrence code to lowercase and integers
-        $format             = strtolower($format);
-        $codigo_banco       = (int)$codigo_banco;
-        $codigo_ocorrencia  = (int)$codigo_ocorrencia;
+        $format = strtolower($format);
 
         // Load the format from a YAML file
-        $yamlLoad           = new \Cnab\Format\YamlLoad($codigo_banco);
-        $array              = $yamlLoad->loadFormat($format, 'retorno/codigo_ocorrencia');
+        $yamlLoad = new YamlLoad();
+        $array = $yamlLoad->loadFormat($format, 'retorno/codigo_ocorrencia');
 
         // Check if the bank code and ocorrence code exist in the array
-        if(array_key_exists($codigo_banco, $array) && array_key_exists($codigo_ocorrencia, $array[$codigo_banco]))
-        {
+        if ($array !== null && array_key_exists($codigoBanco, $array) && array_key_exists($codigoOcorrencia, $array[$codigoBanco])) {
             // Return the name of the ocorrence code
-            return $array[$codigo_banco][$codigo_ocorrencia];
+            return $array[$codigoBanco][$codigoOcorrencia];
         }
 
         // If the bank code or ocorrence code do not exist in the array, throw an exception
-        throw new \Exception("The format '{$format}' is not supported or the bank code or ocorrence code is invalid.");
+        throw new \Exception("The format '{$format}' is not supported or the bank code '{$codigoBanco}' or ocorrence code '{$codigoOcorrencia}' is invalid.");
     }
 }

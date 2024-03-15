37041871b5516ba725860fa3601149ccfabd4bf0
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:50:25 -0300

    improve

--- vendor/andersondanilo/cnab_php/src/Cnab/Format/Linha.php
+++ vendor/andersondanilo/cnab_php/src/Cnab/Format/Linha.php
@@ -1,123 +1,137 @@
 <?php
+
 namespace Cnab\Format;
 
-class Linha {
-	private $fields     = array();
-	public  $last_error = false;
-	
-	public function __set($key, $valor)
-	{
-		if(\array_key_exists($key, $this->fields))
-			$this->fields[$key]->set($valor);
-		else
-			throw new \InvalidArgumentException("field '$key' dont exists");
-	}
-	
-	public function __get($key)
-	{
-		if(\array_key_exists($key, $this->fields))
-			return $this->fields[$key]->getValue();
-		else
-			throw new \InvalidArgumentException("field '$key' dont exists");
-	}
-	
-	public static function cmpSortFields(Field $field1, Field $field2)
-	{
-		return $field1->pos_start > $field2->pos_start ? 1 : -1;
-	}
-	
-	public function addField($nome, $pos_start, $pos_end, $format, $default, $options)
-	{		
-		foreach($this->fields as $key => $field)
-		{
-			$current_pos_start = $field->pos_start;
-			$current_pos_end = $field->pos_end;
+class Linha
+{
+    private array $fields = [];
+    public bool  $lastError = false;
 
-            if ( ($pos_start >= $current_pos_start && $pos_start <= $current_pos_end) ||
-                 ($pos_end <= $current_pos_end && $pos_end >= $current_pos_start) ||
-                 ($current_pos_start >= $pos_start && $current_pos_start <= $pos_end) ||
-                 ($current_pos_end <= $pos_end && $current_pos_end >= $pos_start)) 
-            {
-				unset($this->fields[$key]);
+    public function __set(string $key, $valor): void
+    {
+        if (!array_key_exists($key, $this->fields)) {
+            throw new \InvalidArgumentException("Field '$key' does not exist");
+        }
+
+        $this->fields[$key]->set($valor);
+    }
+
+    public function __get(string $key): string
+    {
+        if (!array_key_exists($key, $this->fields)) {
+            throw new \InvalidArgumentException("Field '$key' does not exist");
+        }
+
+        return $this->fields[$key]->getValue() ?? '';
+    }
+
+    public static function cmpSortFields(Field $field1, Field $field2): int
+    {
+        return $field1->posStart > $field2->posStart ? 1 : -1;
+    }
+
+    public function addField(string $nome, int $posStart, int $posEnd, string $format, $default = false, array $options = []): void
+    {
+        foreach ($this->fields as $key => $field) {
+            $currentPosStart = $field->posStart;
+            $currentPosEnd = $field->posEnd;
+
+            if (
+                ($posStart >= $currentPosStart && $posStart <= $currentPosEnd) ||
+                ($posEnd <= $currentPosEnd && $posEnd >= $currentPosStart) ||
+                ($currentPosStart >= $posStart && $currentPosStart <= $posEnd) ||
+                ($currentPosEnd <= $posEnd && $currentPosEnd >= $posStart)
+            ) {
+                unset($this->fields[$key]);
             }
-		}
-		
-		$this->fields[$nome] = new Field($this, $nome, $format, $pos_start, $pos_end, $options);
-		if($default !== false)
-			$this->fields[$nome]->set($default);
-	}
-	
-	public function loadFromString($text)
-	{
-		foreach($this->fields as $field)
-		{
-			$field->set(Picture::decode(\substr($text, $field->pos_start - 1, $field->length), $field->format, $field->options));
-		}
-	}
-	
-	public function getEncoded()
-	{
-		if($this->validate())
-		{
-			$max_pos_end = 0;
-			$dados = "";
-			$fields = $this->fields;
-			usort($fields, 'self::cmpSortFields');
+        }
+
+        $this->fields[$nome] = new Field($this, $nome, $format, $posStart, $posEnd, $options);
+
+        if ($default !== false) {
+            $this->fields[$nome]->set($default);
+        }
+    }
+
+    public function loadFromString(string $text): void
+    {
+        foreach ($this->fields as $field) {
+            $picture = Picture::decode(
+                substr($text, $field->posStart - 1, $field->length),
+                $field->format,
+                $field->options
+            );
+
+            $field->set($picture);
+        }
+    }
+
+    public function getEncoded(): string
+    {
+        if ($this->validate()) {
+            $maxPosEnd = 0;
+            $dados = '';
+            $fields = $this->fields;
+            usort($fields, [self::class, 'cmpSortFields']);
             $lastField = null;
-			foreach($fields as $field)
-			{
-                if($lastField && $field->pos_start != $lastField->pos_end + 1)
-                    throw new \Exception("gap between {$lastField->nome} and {$field->nome}");
-				$dados .= $field->getEncoded();
-				if($field->pos_end > $max_pos_end)
-					$max_pos_end = $field->pos_end; 
+
+            foreach ($fields as $field) {
+                if ($lastField && $field->posStart != @$lastField->posEnd + 1) {
+                    throw new \Exception("Gap between {$lastField->nome} and {$field->nome}");
+                }
+
+                $dados .= $field->getEncoded();
+
+                if ($field->posEnd > $maxPosEnd) {
+                    $maxPosEnd = $field->posEnd;
+                }
+
                 $lastField = $field;
-			}
-			
-			if(strlen($dados) <> $max_pos_end) {
-				throw new \Exception("line length is " . \strlen($dados) . " and max pos_end is $max_pos_end");
             }
-			
-			return $dados;
-		}
-		else
-			return false;
-	}
 
-	public function getFields()
-	{
-		return $this->fields;
-	}
-	
-	public function validate()
-	{
-		foreach($this->fields as $fieldNome => $field)
-			if($field->getValue() === null || $field->getValue() === false)
-			{
-				$this->last_error = "$fieldNome dont be null or false"; 
-				return false;
-			}
-		return true;
+            if (strlen($dados) !== $maxPosEnd) {
+                throw new \Exception("Line length is " . strlen($dados) . " and max pos_end is $maxPosEnd");
+            }
+
+            return $dados;
+        } else {
+            return false;
+        }
     }
 
-    public function existField($name)
+    public function getFields(): array
     {
-        $exist = \array_key_exists($name, $this->fields);
-        return $exist ? true : false;
+        return $this->fields;
     }
 
-    public function dump()
+    public function validate(): bool
     {
-    	$dump  = '';
+        foreach ($this->fields as $fieldName => $field) {
+            if ($field->getValue() === null || $field->getValue() === false) {
+                $this->lastError = "$fieldName cannot be null or false";
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public function existField(string $name): bool
+    {
+        return array_key_exists($name, $this->fields);
+    }
+
+    public function dump(): string
+    {
+        $dump  = '';
         $dump .= PHP_EOL;
         $dump .= '============= Dump ==============';
         $dump .= PHP_EOL;
-        foreach($this->fields as $fieldNome => $field)
-        {
-            $dump .= $fieldNome . ': ';
+
+        foreach ($this->fields as $fieldName => $field) {
+            $dump .= $fieldName . ': ';
             $dump .= $field->getValue();
             $dump .= PHP_EOL;
         }
+
         return $dump;
-    }
-}

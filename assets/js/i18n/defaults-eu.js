ba0ad6021d437f9f3acb3e6fee56bd721f1be965
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:38 -0300

    comment

--- assets/js/i18n/defaults-eu.js
+++ assets/js/i18n/defaults-eu.js
@@ -4,12 +4,25 @@
  * Copyright 2013-2014 bootstrap-select
  * Licensed under MIT (https://github.com/silviomoreto/bootstrap-select/blob/master/LICENSE)
  */
+
+// Start of an Immediately Invoked Function Expression (IIFE) that passes jQuery as a parameter
 (function ($) {
+  // Set the default options for the selectpicker plugin
   $.fn.selectpicker.defaults = {
-    noneSelectedText: 'Hautapenik ez',
-    noneResultsText: 'Emaitzarik ez',
-    countSelectedText: '{1}(e)tik {0} hautatuta',
-    maxOptionsText: ['Mugara iritsita ({n} {var} gehienez)', 'Taldearen mugara iritsita ({n} {var} gehienez)', ['elementu', 'elementu']],
-    multipleSeparator: ', '
+    // The text to display when no options are selected
+    noneSelectedText: 'No option selected', // Default: 'Nothing selected'
+    // The text to display when there are no results for a search
+    noneResultsText: 'No results found', // Default: 'No results found'
+    // The text to display when one or more options are selected
+    countSelectedText: '{1}(s) selected from {0}', // Default: '{0} of {1} selected'
+    // The maximum number of options that can be selected, and the text to display when this limit is reached
+    maxOptionsText: [
+      'Limit reached ({n} {var} maximum)', // Default: 'Limit reached ({n} {var})'
+      'Group limit reached ({n} {var} maximum)', // Default: 'Group limit reached ({n} {var})'
+      ['item', 'items'] // Default: ['item', 'items']
+    ],
+    // The separator used to join multiple selected options
+    multipleSeparator: ', ' // Default: ', '
   };
+// End of the IIFE
 }(jQuery));

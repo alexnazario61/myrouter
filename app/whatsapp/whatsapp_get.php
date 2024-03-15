c2d89d4dfc48c84e961ab1836c40c64660df048c
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:26 -0300

    comment

--- app/whatsapp/whatsapp_get.php
+++ app/whatsapp/whatsapp_get.php
@@ -1,19 +1,33 @@
 <?php
+
+// Get the 'fone' and 'mensagem' parameters from the GET request
 $fone = $_GET['fone'];
 $mensagem = $_GET['mensagem'];
 
-$telefone = $fone = $_GET['fone'];
-$mensagens = $mensagem = $_GET['mensagem'];
+// Assign the same values to new variables with more descriptive names
+$telefone = $fone;
+$mensagens = $mensagem;
 
-if($fone !='' && $mensagem !=''){
-    shell_exec("yowsup-cli demos -s 55{$telefone} \"{$mensagens}\" -c /etc/whatsapp/whatsapp.conf");
+// Check if both 'fone' and 'mensagem' parameters have values
+if ($fone != '' && $mensagem != '') {
+    // Concatenate the country code and phone number
+    $phoneNumber = "55{$telefone}";
+
+    // Construct the shell command using the yowsup-cli tool
+    $shellCommand = "yowsup-cli demos -s {$phoneNumber} \"{$mensagens}\" -c /etc/whatsapp/whatsapp.conf";
+
+    // Execute the shell command
+    shell_exec($shellCommand);
+
+    // Display a success message
     echo "MENSAGEM ENVIADA";
-}else{
+} else {
+    // Display an error message
     echo "NAO FOI ENVIADO";
 }
 
-//$content = file_get_contents("http://whatsapp.myrouter.com.br/api.php?fone=$telefone&mensagem=$mensagens");
-//echo $content;
+// Previous commented code, possibly for testing or alternative functionality
+// $content = file_get_contents("http://whatsapp.myrouter.com.br/api.php?fone=$telefone&mensagem=$mensagens");
+// echo $content;
 
 ?>
-

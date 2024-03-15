c4ce79c64c8dfe719d6fba737f7682e5543303ca
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:40:49 -0300

    comment

--- testeprocessaretorno.php
+++ testeprocessaretorno.php
@@ -1,10 +1,16 @@
 <?php
 
-// prepara o ambiente para testar o processo
+//******************************************************************************
+// Setup the testing environment by simulating a received RET file
+//******************************************************************************
 $_FILES['arquivo'] = array(
-    'type' => 'ret',
-    'name' => 'CN01075A.RET',
-    'tmp_name' => __DIR__.'/CN01075A.RET',
+    'type'   => 'ret',          // File type (in this case, a RET file)
+    'name'   => 'CN01075A.RET',  // Name of the RET file
+    'tmp_name'=> __DIR__.'/CN01075A.RET'  // Temporary location of the RET file
 );
 
+//******************************************************************************
+// Include the script that processes the RET file
+//******************************************************************************
 include 'processa.retorno.php';
+

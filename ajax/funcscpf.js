ee964a52c5cfa423d9d67f247f93fc0563716753
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:19 -0300

    comment

--- ajax/funcscpf.js
+++ ajax/funcscpf.js
@@ -1,43 +1,43 @@
-// Vari·vel que receber· o objeto XMLHttpRequest
+// Vari√°vel que receber√° o objeto XMLHttpRequest
+// This variable will hold the XMLHttpRequest object
 var req;
- 
+
 function validarCPF(campo, valor) {
  
 // Verificar o Browser
-// Firefox, Google Chrome, Safari e outros
+// Function to check the browser
+// Firefox, Google Chrome, Safari and others
 if(window.XMLHttpRequest) {
+   // Create a new XMLHttpRequest object for modern browsers
    req = new XMLHttpRequest();
 }
 // Internet Explorer
 else if(window.ActiveXObject) {
+   // Create a new ActiveXObject for Internet Explorer
    req = new ActiveXObject("Microsoft.XMLHTTP");
 }
  
-// Aqui vai o valor e o nome do campo que pediu a requisiÁ„o.
+// Aqui vai o valor e o nome do campo que pediu a requisi√ß√£o.
+// The value and name of the field that made the request will go here
 var url = "ajax/cpf.php?campo="+campo+"&valor="+valor;
  
-// Chamada do mÈtodo open para processar a requisiÁ„o
+// Chamada do m√©todo open para processar a requisi√ß√£o
+// Call the open method to process the request
 req.open("Get", url, true);
  
-// Quando o objeto recebe o retorno, È chamada a seguinte funÁ„o;
+// Quando o objeto recebe o retorno, √© chamada a seguinte fun√ß√£o;
+// When the object receives the return, the following function is called
 req.onreadystatechange = function() {
  
 	// Exibe a mensagem "Verificando" enquanto carrega
+	// Display the message "Verifying" while loading
 	if(req.readyState == 1) {
+		// Set the content of the element with id 'campo_cpf' to 'Verifying...'
 		document.getElementById('campo_cpf').innerHTML = '<font color="gray">Verificando...</font>';
 	}
  
-	// Verifica se o Ajax realizou todas as operaÁıes corretamente
+	// Verifica se o Ajax realizou todas as opera√ß√µes corretamente
+	// Check if the Ajax request was successful
 	if(req.readyState == 4 && req.status == 200) {
-	// Resposta retornada pelo validacao.php (pagina de conex„o com o BD)
-	var resposta = req.responseText;
- 
-	// Resposta na div do campo que fez a requisiÁ„o
-	document.getElementById('campo_cpf').innerHTML = resposta;
-	}
- 
-}
- 
-req.send(null);
- 
-}
+	// Resposta retornada pelo validacao.php (pagina de conex√£o com o BD)
+	// Response returned by validacao

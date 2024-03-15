8a67686bdf82c657746985bc9ed8d262b4679302
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:48:48 -0300

    improve

--- app/clientes/cep.php
+++ app/clientes/cep.php
@@ -1,11 +1,18 @@
 <?php
-if (isset($_GET['cep'])){
-    $reg = simplexml_load_file("http://cep.republicavirtual.com.br/web_cep.php?formato=xml&cep=" . $_GET['cep']);
-    $dados['sucesso'] = (string) $reg->resultado;
-    $dados['endereco']     = (string) $reg->tipo_logradouro . ' ' . $reg->logradouro;
-    $dados['bairro']  = (string) $reg->bairro;
-    $dados['cidade']  = (string) $reg->cidade;
-    $dados['estado']  = (string) $reg->uf;
-    echo json_encode($dados);
-}
-?>
+// Check if the 'cep' parameter is set and is a valid Brazilian postal code
+if (isset($_GET['cep']) && preg_match('/^\d{5}-?\d{3}$/', $_GET['cep'])) {
+    // Get the XML data from the web service
+    $xml_data = file_get_contents("http://cep.republicavirtual.com.br/web_cep.php?formato=xml&cep=" . $_GET['cep']);
+
+    // Parse the XML data
+    $reg = simplexml_load_string($xml_data);
+
+    // Check if the XML data is valid
+    if ($reg && isset($reg->resultado)) {
+        $dados = [
+            'sucesso' => false,
+            'endereco' => '',
+            'bairro' => '',
+            'cidade' => '',
+            'estado' => ''
+

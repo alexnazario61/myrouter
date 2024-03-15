1de5a8bb81be40217deaf8d91372c91d5f0d30e3
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:48:42 -0300

    improve

--- app/financeiro/boletos.php
+++ app/financeiro/boletos.php
@@ -1,5 +1,6 @@
-        <div class="breadcrumb clearfix">
-          <ul>
-            <li><a href="index.php?app=Dashboard">Dashboard</a></li> <!-- This link leads to the Dashboard page -->
-            <li class="active">Financeiro</li> <!-- This is the current page, which is the Financeiro page -->
-         
+<div class="breadcrumb clearfix">
+  <ul>
+    <li><a href="/dashboard">Dashboard</a></li> <!-- This link leads to the Dashboard page -->
+    <li class="active">Financeiro</li> <!-- This is the current page, which is the Financeiro page -->
+  </ul>
+</div>

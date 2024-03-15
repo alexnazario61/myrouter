8e85e09d0714985b7555ef0aff9d50a3d938bc35
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:50:23 -0300

    improve

--- config/cron.php
+++ config/cron.php
@@ -1,122 +1,85 @@
-
 <?php
 
 // Enable error reporting for debugging purposes
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(E_ERROR | E_PARSE | E_WARNING );
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Require necessary files
 require_once 'conexao.class.php';
-require_once 'conexao.php';
 require_once 'crud.class.php';
 require_once 'mikrotik.class.php';
+require_once 'cliente.class.php';
+require_once 'plano.class.php';
+require_once 'servidor.class.php';
 
 // Get the company information from the database
-$empresa1 = $mysqli->query("SELECT * FROM empresa WHERE id = '1'");
-$Cempresa = mysqli_fetch_array($empresa1);
-$dias_bloc = $Cempresa['dias_bloc'];
-
-// Connect to the database
-$con = new conexao(); // instancia classe de conxao
-$con->connect(); // abre conexao com o banco
+$conexao = new Conexao();
+$connection = $conexao->connect();
+$query = "SELECT * FROM empresa WHERE id = 1";
+$result = mysqli_query($connection, $query);
+$empresa = mysqli_fetch_assoc($result);
+$dias_bloc = $empresa['dias_bloc'];
 
 // Get the list of unpaid invoices that are overdue by the number of days specified in the company settings
-$sxd = $mysqli->query("select * from financeiro  where  date_add(vencimento, interval '$dias_bloc' day) < now() and   situacao = 'N'");
+$query = "SELECT * FROM financeiro  WHERE date_add(vencimento, interval '$dias_bloc' day) < now() AND situacao = 'N'";
+$result = mysqli_query($connection, $query);
 
 // Loop through each overdue invoice
-while($daa = mysqli_fetch_array($sxd)){
- 
- // Count the number of overdue invoices
- $verificazeros = mysqli_num_rows($sxd);
+while ($daa = mysqli_fetch_assoc($result)) {
+    // Count the number of overdue invoices
+    $verificazeros = mysqli_num_rows($result);
 
- // If there are any overdue invoices
- if($verificazeros > 0) {
+    // If there are any overdue invoices
+    if ($verificazeros > 0) {
+        // Update the invoice status to 'B' (blocked)
+        $crud = new Crud('financeiro');
+        $crud->atualizar(["situacao" => 'B'], ["id" => $daa['id']]);
 
- // Update the invoice status to 'B' (blocked)
- $idprd = $daa['id'];
- $crud = new crud('financeiro'); // instancia classe com as operações crud, passando o nome da tabela como parametro
- $crud->atualizar("situacao='B'", "id='$idprd'");
+        // Get the associated client information
+        $cliente = new Cliente();
+        $cliente->setId($daa['pedido']);
+        $cliente->carregarPorId();
 
- // Get the associated client information
- $codass = $daa['pedido'];
- $ccss = $mysqli->query("SELECT * FROM assinaturas WHERE pedido = '$codass'");
- $cliente = mysqli_fetch_array($ccss);
+        // Get the client's ID
+        $cliente_id = $cliente->getId();
 
- // Get the client's ID
- $ccvbv = $cliente['cliente'];
- $ccsscv = $mysqli->query("SELECT * FROM clientes WHERE id = '$ccvbv'");
- $vcsms = mysqli_fetch_array($ccsscv);
+        // Get the plan information
+        $plano = new Plano();
+        $plano->setId($cliente->getPlano());
+        $plano->carregarPorId();
 
- // Get the plan information
- $plano = $cliente['plano'];
- $ppss = $mysqli->query("SELECT * FROM planos WHERE id = '$plano'");
- $plano = mysqli_fetch_array($ppss);
+        // Get the server information
+        $servidor = new Servidor();
+        $servidor->setId($plano->getServidor());
+        $servidor->carregarPorId();
 
- // Get the server information
- $servidor = $plano['servidor'];
- $ssrv = $mysqli->query("SELECT * FROM servidores WHERE id = '$servidor'");
- $servidor = mysqli_fetch_array($ssrv);
+        // If the client has autobloqueio enabled
+        if ($cliente->getAutobloqueio() == 'S') {
+            // Block the client's access
+            echo 'block ' . $cliente->getLogin() . "\n";
 
- // If the client has autobloqueio enabled
- if($cliente['autobloqueio'] == 'S') {
+            // Determine the type of connection (hotspot, PPPoE, or IPARP) and block it accordingly
+            switch ($cliente->getTipo()) {
+                case 'HOTSPOT':
+                    // Connect to the MikroTik router and disable the user's hotspot session
+                    $mikrotik = new Mikrotik($servidor->getIp(), $servidor->getLogin(), $servidor->getSenha());
+                    $mikrotik->desabilitarSessaoHotspot($cliente->getLogin());
+                    break;
+                case 'PPPoE':
+                    // Connect to the MikroTik router and disable the user's PPPoE session
+                    $mikrotik = new Mikrotik($servidor->getIp(), $servidor->getLogin(), $servidor->getSenha());
+                    $mikrotik->desabilitarSessaoPPPoE($cliente->getLogin());
+                    break;
+                case 'IPARP':
+                    // Connect to the MikroTik router and disable the user's IPARP session
+                    $mikrotik = new Mikrotik($servidor->getIp(), $servidor->getLogin(), $servidor->getSenha());
+                    $mikrotik->desabilitarSessaoIPARP($cliente->getLogin());
+                    break;
+            }
+        }
+    }
+}
 
- // Block the client's access
- echo 'block  ';
- echo $cliente['login'];
-
- // Determine the type of connection (hotspot, PPPoE, or IPARP) and block it accordingly
- if($cliente['tipo'] == 'HOTSPOT') {
- // Connect to the MikroTik router and disable the user's hotspot session
- $API = new routeros_api();
- $API->debug = false;
- if ($API->connect(''.$servidor['ip'].'', ''.$servidor['login'].'', ''.$servidor['senha'].''))
- {
-
- // Get the user's hotspot session information
- $username = $cliente['login'];
-
- $API->write('/ip/hotspot/active/print', false);
- $API->write('?=user='.$username.'');
- $res = $API->read($res);
-
- // Disable the user's hotspot session
- $user_login = $res['1'];
- if(!empty($user_login)){
- $API->write('/ip/hotspot/active/remove', false);
- $API->write($user_login);
- $res = $API->read($res);
- }
- }
- $API->disconnect();
- }
-
- if($cliente['tipo'] == 'PPPoE') {
- // Connect to the MikroTik router and disable the user's PPPoE session
- $API = new routeros_api();
- $API->debug = false;
- if ($API->connect(''.$servidor['ip'].'', ''.$servidor['login'].'', ''.$servidor['senha'].''))
- {
- $username = $cliente['login'];
- $API->write('/ppp/active/print',false);
- $API->write('?=name='.$username.'');
- $res = $API->read($res);
-
- // Disable the user's PPPoE session
- $user_login = $res['1'];
- if(!empty($user_login)){
- $API->write('/ppp/active/remove',false);
- $API->write($user_login);
- $res = $API->read($res);
- }
- }
- $API->disconnect();
- }
-
- if($cliente['tipo'] == 'IPARP') {
- // Connect to the MikroTik router and disable the user's IPARP session
- $API = new routeros_api();
- $API->debug = false;
- if ($API->connect(''.$servidor['ip'].'', ''.$servidor['login'].'', ''.$servidor['senha'].''))
- {
- $API->
+// Close the database connection
+$conexao->disconnect();

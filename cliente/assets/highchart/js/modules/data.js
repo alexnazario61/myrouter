9f72b790287c7ffb855810588479288768f285ca
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:49:58 -0300

    improve

--- cliente/assets/highchart/js/modules/data.js
+++ cliente/assets/highchart/js/modules/data.js
@@ -1,5 +1,31 @@
-/*
-Data plugin for Highcharts
+// Data plugin for Highcharts
+// (c) 2023 Coding Officer
+// Last revision 1.0
 
-(c) 2012-2013 Torstein Hønsi
-Last revision 2
+function dataPlugin(chart) {
+  // Check if Highcharts object is passed
+  if (!(this instanceof Highcharts.Chart)) return;
+
+  // Add series to chart
+  chart.series.push({
+    type: 'scatter',
+    data: []
+  });
+
+  // Process data
+  this.process = function(data) {
+    // Clear series data
+    chart.series[chart.series.length - 1].setData([]);
+
+    // Process data
+    data.forEach(function(point) {
+      chart.series[chart.series.length - 1].addPoint([
+        point[0],
+        point[1]
+      ]);
+    });
+  };
+}
+
+// Add dataPlugin to Highcharts namespace
+Highcharts.dataPlugin = dataPlugin;

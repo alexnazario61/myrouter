2a4b363a8af8fb3f083c166a95ba87928bdbf0ce
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:48:36 -0300

    improve

--- ajax/logintecnicos.php
+++ ajax/logintecnicos.php
@@ -1,2 +1,7 @@
 <?php
-// Setting the content type to text/html and charset to ISO-8
+// Setting the content type to text/html and charset to UTF-8
+header('Content-Type: text/html; charset=UTF-8');
+
+// Your PHP code here
+
+?>

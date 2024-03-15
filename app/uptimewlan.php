2377031b00c0634ea1f547330d3fdc39e3d0b7d9
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:48:50 -0300

    improve

--- app/uptimewlan.php
+++ app/uptimewlan.php
@@ -1,16 +1,42 @@
 <?php
+// Include the MikroTik class
 require('../config/mikrotik.class.php');
-$API = new routeros_api();
-$API->debug = false;
-if ($API->connect(''.$_GET['ip'].'', ''.$_GET['login'].'', ''.$_GET['senha'].'')) {
 
-$ARRAY = $API->comm("/interface/wireless/registration-table/print");
+// Create an instance of the MikroTik class
+$api = new routeros_api();
 
-$first = $ARRAY['0'];
+// Set debug mode to false
+$api->debug = false;
+
+// Get input parameters
+$ip = filter_input(INPUT_GET, 'ip', FILTER_VALIDATE_IP);
+$login = filter_input(INPUT_GET, 'login');
+$senha = filter_input(INPUT_GET, 'senha');
+
+// Check if all input parameters are present
+if (!$ip || !$login || !$senha) {
+    die('Missing required parameters');
+}
+
+// Connect to the MikroTik router
+if (!$api->connect($ip, $login, $senha)) {
+    die('Failed to connect to the MikroTik router');
+}
+
+// Get the registration table
+$registrationTable = $api->comm("/interface/wireless/registration-table/print");
+
+// Check if the registration table is not empty
+if (empty($registrationTable)) {
+    die('Registration table is empty');
+}
+
+// Get the first item in the registration table
+$first = $registrationTable[0];
+
+// Print the uptime of the first item
+echo $first['uptime'];
+
+// Close the MikroTik router connection
+$api->disconnect();
 ?>
-<?php echo $first['uptime']; ?>
-
-<?php } ?>
-
-
-

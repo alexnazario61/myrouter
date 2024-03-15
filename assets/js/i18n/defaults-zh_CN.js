29072e595e0dea5b07086b28012379c8d3a85c34
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:48:55 -0300

    improve

--- assets/js/i18n/defaults-zh_CN.js
+++ assets/js/i18n/defaults-zh_CN.js
@@ -1,15 +1,36 @@
 /*!
- * Bootstrap-select v1.6.3 (http://silviomoreto.github.io/bootstrap-select/)
+ * Bootstrap-select v1.6.3 (https://silviomoreto.github.io/bootstrap-select/)
  *
  * Copyright 2013-2014 bootstrap-select
  * Licensed under MIT (https://github.com/silviomoreto/bootstrap-select/blob/master/LICENSE)
  */
 (function ($) {
+  // Default options for selectpicker plugin
   $.fn.selectpicker.defaults = {
-    noneSelectedText: '没有选中任何项',
-    noneResultsText: '没有找到匹配项',
-    countSelectedText: '选中{1}中的{0}项',
-    maxOptionsText: ['超出限制 (最多选择{n}项)', '组选择超出限制(最多选择{n}组)'],
-    multipleSeparator: ', '
+    noneSelectedText: 'No items selected', // Default noneSelectedText in English
+    noneResultsText: 'No results found', // Default noneResultsText in English
+    countSelectedText: '{0} of {1} selected', // Default countSelectedText in English
+    maxOptionsText: [
+      'Limit reached (max {n} items)', // Default maxOptionsText in English for single select
+      'Group limit reached (max {n} groups)' // Default maxOptionsText in English for multiple select
+    ],
+    multipleSeparator: ', ' // Default multipleSeparator in English
   };
-}(jQuery));
+
+  // Set the default options based on the current language
+  function setDefaultsBasedOnLanguage() {
+    var language = $.fn.selectpicker.Constructor.prototype.options.language;
+    if (language) {
+      $.fn.selectpicker.defaults.noneSelectedText = language.noneSelectedText || $.fn.selectpicker.defaults.noneSelectedText;
+      $.fn.selectpicker.defaults.noneResultsText = language.noneResultsText || $.fn.selectpicker.defaults.noneResultsText;
+      $.fn.selectpicker.defaults.countSelectedText = language.countSelectedText || $.fn.selectpicker.defaults.countSelectedText;
+      $.fn.selectpicker.defaults.maxOptionsText = [
+        language.maxOptionsTextSingle || $.fn.selectpicker.defaults.maxOptionsText[0],
+        language.maxOptionsTextMulti || $.fn.selectpicker.defaults.maxOptionsText[1]
+      ];
+      $.fn.selectpicker.defaults.multipleSeparator = language.multipleSeparator || $.fn.selectpicker.defaults.multipleSeparator;
+    }
+  }
+
+  // Initialize the setDefaultsBasedOnLanguage function
+  set

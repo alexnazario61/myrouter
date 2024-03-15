260a24ee29b319c409633afb93d33c205f59e087
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:48:41 -0300

    improve

--- ajax/assinatura.php
+++ ajax/assinatura.php
@@ -1,2 +1,7 @@
 <?php
-// Setting the content type to text/html and charset to ISO-885
+// Setting the content type to text/html and charset to UTF-8
+header('Content-Type: text/html; charset=UTF-8');
+
+// Your HTML and PHP code here
+
+?>

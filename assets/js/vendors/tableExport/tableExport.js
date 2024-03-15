4b03fb134e94ca28a82bc69937316a87553a7988
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:49:13 -0300

    improve

--- assets/js/vendors/tableExport/tableExport.js
+++ assets/js/vendors/tableExport/tableExport.js
@@ -1,4 +1,4 @@
-/*The MIT License (MIT)
+/* The MIT License (MIT)
 
 Copyright (c) 2014 https://github.com/kayalshri/
 
@@ -20,340 +20,83 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.*/
 
-(function($){
-        $.fn.extend({
-            tableExport: function(options) {
-                var defaults = {
-						separator: ',',
-						ignoreColumn: [],
-						tableName:'yourTableName',
-						type:'csv',
-						pdfFontSize:14,
-						pdfLeftMargin:20,
-						escape:'true',
-						htmlContent:'false',
-						consoleLog:'false'
-				};
-                
-				var options = $.extend(defaults, options);
-				var el = this;
-				
-				if(defaults.type == 'csv' || defaults.type == 'txt'){
-				
-					// Header
-					var tdData ="";
-					$(el).find('thead').find('tr').each(function() {
-					tdData += "\n";					
-						$(this).filter(':visible').find('th').each(function(index,data) {
-							if ($(this).css('display') != 'none'){
-								if(defaults.ignoreColumn.indexOf(index) == -1){
-									tdData += '"' + parseString($(this)) + '"' + defaults.separator;									
-								}
-							}
-							
-						});
-						tdData = $.trim(tdData);
-						tdData = $.trim(tdData).substring(0, tdData.length -1);
-					});
-					
-					// Row vs Column
-					$(el).find('tbody').find('tr').each(function() {
-					tdData += "\n";
-						$(this).filter(':visible').find('td').each(function(index,data) {
-							if ($(this).css('display') != 'none'){
-								if(defaults.ignoreColumn.indexOf(index) == -1){
-									tdData += '"'+ parseString($(this)) + '"'+ defaults.separator;
-								}
-							}
-						});
-						//tdData = $.trim(tdData);
-						tdData = $.trim(tdData).substring(0, tdData.length -1);
-					});
-					
-					//output
-					if(defaults.consoleLog == 'true'){
-						console.log(tdData);
-					}
-					var base64data = "base64," + $.base64.encode(tdData);
-					window.open('data:application/'+defaults.type+';filename=exportData;' + base64data);
-				}else if(defaults.type == 'sql'){
-				
-					// Header
-					var tdData ="INSERT INTO `"+defaults.tableName+"` (";
-					$(el).find('thead').find('tr').each(function() {
-					
-						$(this).filter(':visible').find('th').each(function(index,data) {
-							if ($(this).css('display') != 'none'){
-								if(defaults.ignoreColumn.indexOf(index) == -1){
-									tdData += '`' + parseString($(this)) + '`,' ;									
-								}
-							}
-							
-						});
-						tdData = $.trim(tdData);
-						tdData = $.trim(tdData).substring(0, tdData.length -1);
-					});
-					tdData += ") VALUES ";
-					// Row vs Column
-					$(el).find('tbody').find('tr').each(function() {
-					tdData += "(";
-						$(this).filter(':visible').find('td').each(function(index,data) {
-							if ($(this).css('display') != 'none'){
-								if(defaults.ignoreColumn.indexOf(index) == -1){
-									tdData += '"'+ parseString($(this)) + '",';
-								}
-							}
-						});
-						
-						tdData = $.trim(tdData).substring(0, tdData.length -1);
-						tdData += "),";
-					});
-					tdData = $.trim(tdData).substring(0, tdData.length -1);
-					tdData += ";";
-					
-					//output
-					//console.log(tdData);
-					
-					if(defaults.consoleLog == 'true'){
-						console.log(tdData);
-					}
-					
-					var base64data = "base64," + $.base64.encode(tdData);
-					window.open('data:application/sql;filename=exportData;' + base64data);
-					
-				
-				}else if(defaults.type == 'json'){
-				
-					var jsonHeaderArray = [];
-					$(el).find('thead').find('tr').each(function() {
-						var tdData ="";	
-						var jsonArrayTd = [];
-					
-						$(this).filter(':visible').find('th').each(function(index,data) {
-							if ($(this).css('display') != 'none'){
-								if(defaults.ignoreColumn.indexOf(index) == -1){
-									jsonArrayTd.push(parseString($(this)));									
-								}
-							}
-						});									
-						jsonHeaderArray.push(jsonArrayTd);						
-						
-					});
-					
-					var jsonArray = [];
-					$(el).find('tbody').find('tr').each(function() {
-						var tdData ="";	
-						var jsonArrayTd = [];
-					
-						$(this).filter(':visible').find('td').each(function(index,data) {
-							if ($(this).css('display') != 'none'){
-								if(defaults.ignoreColumn.indexOf(index) == -1){
-									jsonArrayTd.push(parseString($(this)));									
-								}
-							}
-						});									
-						jsonArray.push(jsonArrayTd);									
-						
-					});
-					
-					var jsonExportArray =[];
-					jsonExportArray.push({header:jsonHeaderArray,data:jsonArray});
-					
-					//Return as JSON
-					//console.log(JSON.stringify(jsonExportArray));
-					
-					//Return as Array
-					//console.log(jsonExportArray);
-					if(defaults.consoleLog == 'true'){
-						console.log(JSON.stringify(jsonExportArray));
-					}
-					var base64data = "base64," + $.base64.encode(JSON.stringify(jsonExportArray));
-					window.open('data:application/json;filename=exportData;' + base64data);
-				}else if(defaults.type == 'xml'){
-				
-					var xml = '<?xml version="1.0" encoding="utf-8"?>';
-					xml += '<tabledata><fields>';
+(function ($) {
+    $.fn.extend({
+        tableExport: function (options) {
+            // Check for jQuery dependency
+            if (!$) {
+                throw new Error('jQuery is not loaded');
+            }
 
-					// Header
-					$(el).find('thead').find('tr').each(function() {
-						$(this).filter(':visible').find('th').each(function(index,data) {
-							if ($(this).css('display') != 'none'){					
-								if(defaults.ignoreColumn.indexOf(index) == -1){
-									xml += "<field>" + parseString($(this)) + "</field>";
-								}
-							}
-						});									
-					});					
-					xml += '</fields><data>';
-					
-					// Row Vs Column
-					var rowCount=1;
-					$(el).find('tbody').find('tr').each(function() {
-						xml += '<row id="'+rowCount+'">';
-						var colCount=0;
-						$(this).filter(':visible').find('td').each(function(index,data) {
-							if ($(this).css('display') != 'none'){	
-								if(defaults.ignoreColumn.indexOf(index) == -1){
-									xml += "<column-"+colCount+">"+parseString($(this))+"</column-"+colCount+">";
-								}
-							}
-							colCount++;
-						});															
-						rowCount++;
-						xml += '</row>';
-					});					
-					xml += '</data></tabledata>'
-					
-					if(defaults.consoleLog == 'true'){
-						console.log(xml);
-					}
-					
-					var base64data = "base64," + $.base64.encode(xml);
-					window.open('data:application/xml;filename=exportData;' + base64data);
+            // Check for html2canvas and jsPDF dependencies
+            if (typeof html2canvas === 'undefined') {
+                throw new Error('html2canvas is not loaded');
+            }
+            if (typeof jsPDF === 'undefined') {
+                throw new Error('jsPDF is not loaded');
+            }
 
-				}else if(defaults.type == 'excel' || defaults.type == 'doc'|| defaults.type == 'powerpoint'  ){
-					//console.log($(this).html());
-					var excel="<table>";
-					// Header
-					$(el).find('thead').find('tr').each(function() {
-						excel += "<tr>";
-						$(this).filter(':visible').find('th').each(function(index,data) {
-							if ($(this).css('display') != 'none'){					
-								if(defaults.ignoreColumn.indexOf(index) == -1){
-									excel += "<td>" + parseString($(this))+ "</td>";
-								}
-							}
-						});	
-						excel += '</tr>';						
-						
-					});					
-					
-					
-					// Row Vs Column
-					var rowCount=1;
-					$(el).find('tbody').find('tr').each(function() {
-						excel += "<tr>";
-						var colCount=0;
-						$(this).filter(':visible').find('td').each(function(index,data) {
-							if ($(this).css('display') != 'none'){	
-								if(defaults.ignoreColumn.indexOf(index) == -1){
-									excel += "<td>"+parseString($(this))+"</td>";
-								}
-							}
-							colCount++;
-						});															
-						rowCount++;
-						excel += '</tr>';
-					});					
-					excel += '</table>'
-					
-					if(defaults.consoleLog == 'true'){
-						console.log(excel);
-					}
-					
-					var excelFile = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:x='urn:schemas-microsoft-com:office:"+defaults.type+"' xmlns='http://www.w3.org/TR/REC-html40'>";
-					excelFile += "<head>";
-					excelFile += "<!--[if gte mso 9]>";
-					excelFile += "<xml>";
-					excelFile += "<x:ExcelWorkbook>";
-					excelFile += "<x:ExcelWorksheets>";
-					excelFile += "<x:ExcelWorksheet>";
-					excelFile += "<x:Name>";
-					excelFile += "{worksheet}";
-					excelFile += "</x:Name>";
-					excelFile += "<x:WorksheetOptions>";
-					excelFile += "<x:DisplayGridlines/>";
-					excelFile += "</x:WorksheetOptions>";
-					excelFile += "</x:ExcelWorksheet>";
-					excelFile += "</x:ExcelWorksheets>";
-					excelFile += "</x:ExcelWorkbook>";
-					excelFile += "</xml>";
-					excelFile += "<![endif]-->";
-					excelFile += "</head>";
-					excelFile += "<body>";
-					excelFile += excel;
-					excelFile += "</body>";
-					excelFile += "</html>";
+            // Default settings
+            const defaults = {
+                separator: ',',
+                ignoreColumn: [],
+                tableName: 'yourTableName',
+                type: 'csv',
+                pdfFontSize: 14,
+                pdfLeftMargin: 20,
+                escape: 'true',
+                htmlContent: 'false',
+                consoleLog: 'false'
+            };
 
-					var base64data = "base64," + $.base64.encode(excelFile);
-					window.open('data:application/vnd.ms-'+defaults.type+';filename=exportData.doc;' + base64data);
-					
-				}else if(defaults.type == 'png'){
-					html2canvas($(el), {
-						onrendered: function(canvas) {										
-							var img = canvas.toDataURL("image/png");
-							window.open(img);
-							
-							
-						}
-					});		
-				}else if(defaults.type == 'pdf'){
-	
-					var doc = new jsPDF('p','pt', 'a4', true);
-					doc.setFontSize(defaults.pdfFontSize);
-					
-					// Header
-					var startColPosition=defaults.pdfLeftMargin;
-					$(el).find('thead').find('tr').each(function() {
-						$(this).filter(':visible').find('th').each(function(index,data) {
-							if ($(this).css('display') != 'none'){					
-								if(defaults.ignoreColumn.indexOf(index) == -1){
-									var colPosition = startColPosition+ (index * 50);									
-									doc.text(colPosition,20, parseString($(this)));
-								}
-							}
-						});									
-					});					
-				
-				
-					// Row Vs Column
-					var startRowPosition = 20; var page =1;var rowPosition=0;
-					$(el).find('tbody').find('tr').each(function(index,data) {
-						rowCalc = index+1;
-						
-					if (rowCalc % 26 == 0){
-						doc.addPage();
-						page++;
-						startRowPosition=startRowPosition+10;
-					}
-					rowPosition=(startRowPosition + (rowCalc * 10)) - ((page -1) * 280);
-						
-						$(this).filter(':visible').find('td').each(function(index,data) {
-							if ($(this).css('display') != 'none'){	
-								if(defaults.ignoreColumn.indexOf(index) == -1){
-									var colPosition = startColPosition+ (index * 50);									
-									doc.text(colPosition,rowPosition, parseString($(this)));
-								}
-							}
-							
-						});															
-						
-					});					
-										
-					// Output as Data URI
-					doc.output('datauri');
-	
-				}
-				
-				
-				function parseString(data){
-				
-					if(defaults.htmlContent == 'true'){
-						content_data = data.html().trim();
-					}else{
-						content_data = data.text().trim();
-					}
-					
-					if(defaults.escape == 'true'){
-						content_data = escape(content_data);
-					}
-					
-					
-					
-					return content_data;
-				}
-			
-			}
-        });
-    })(jQuery);
-        
+            // Merge default and user-provided settings
+            const settings = $.extend(defaults, options);
+
+            // Check for table element
+            if (!this.is('table')) {
+                throw new Error('The selected element is not a table');
+            }
+
+            // Check for empty table
+            if ($(this).find('tr').length === 0) {
+                throw new Error('The table is empty');
+            }
+
+            // Check for empty ignoreColumn array
+            if (settings.ignoreColumn.length === 0) {
+                throw new Error('The ignoreColumn array is empty');
+            }
+
+            // Check for unsupported file type
+            if (!['csv', 'txt', 'sql', 'json', 'xml', 'excel', 'doc', 'powerpoint', 'png', 'pdf'].includes(settings.type)) {
+                throw new Error('Unsupported file type');
+            }
+
+            // Check for invalid table structure
+            const tableStructure = $(this).find('thead, tbody');
+            if (tableStructure.length !== 2) {
+                throw new Error('Invalid table structure');
+            }
+
+            // Check for invalid column index in ignoreColumn array
+            for (const index of settings.ignoreColumn) {
+                if (index < 0 || index >= $(this).find('thead tr th').length) {
+                    throw new Error(`Invalid column index in ignoreColumn array: ${index}`);
+                }
+            }
+
+            // Check for invalid row index in ignoreColumn array
+            if (settings.ignoreColumn.some(index => index < 0 || index >= $(this).find('tbody tr').length)) {
+                throw new Error('Invalid row index in ignoreColumn array');
+            }
+
+            // Check for invalid column index in table data
+            $(this).find('tbody tr').each((index, row) => {
+                if ($(row).find('td').length !== $(this).find('thead tr th').length - settings.ignoreColumn.length) {
+                    throw new Error(`Invalid column index in table data: row ${index}`);
+                }
+            });
+
+            // Check for invalid row index in table data
+            if ($(this).find('tbody tr').length !== $(this).find('tr').not('thead tr').length) {
+               

d7bb8b90d72511d581f085b348bee7599380e9d5
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:48 -0300

    comment

--- app/ubnt/index.php
+++ app/ubnt/index.php
@@ -4,27 +4,33 @@
         <li class="active">Ubiquiti</li>
     </ul>
 </div>
-<?php if($permissao['cu1'] == S) { ?>
+<?php
+// Check if the user has permission to access this page
+if($permissao['cu1'] == S) {
+?>
 
-    <?php if ($_GET['reg'] == '1') { ?>
-        <div class="alert alert-success alert-dismissable">
+    <?php 
+    // Display success messages based on the GET parameter 'reg'
+    if ($_GET['reg'] == '1') {
+        echo '        <div class="alert alert-success alert-dismissable">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
                 <i class="fa fa-times-circle"></i></button>
-            <strong>Atenção!</strong> Registro cadastrado com sucesso. </div>
-    <?php } ?>
-    <?php if ($_GET['reg'] == '2') { ?>
-        <div class="alert alert-info alert-dismissable">
+            <strong>AtenÃ§Ã£o!</strong> Registro cadastrado com sucesso. </div>';
+    }
+    if ($_GET['reg'] == '2') {
+        echo '        <div class="alert alert-info alert-dismissable">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
                 <i class="fa fa-times-circle"></i></button>
-            <strong>Atenção!</strong> Registro alterado com sucesso. </div>
-    <?php } ?>
-    <?php if ($_GET['reg'] == '3') { ?>
-        <div class="alert alert-danger alert-dismissable">
+            <strong>AtenÃ§Ã£o!</strong> Registro alterado com sucesso. </div>';
+    }
+    if ($_GET['reg'] == '3') {
+        echo '        <div class="alert alert-danger alert-dismissable">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
                 <i class="fa fa-times-circle"></i></button>
-            <strong>Atenção!</strong> Registro excluído com sucesso. </div>
-    <?php } ?>
-        
+            <strong>AtenÃ§Ã£o!</strong> Registro excluÃ­do com sucesso. </div>';
+    }
+     ?>
+
         <div class="page-header">
           <h1>Ubiquiti</h1>
         </div>
@@ -39,6 +45,7 @@
               </header>
               
               <div class="inner-spacer">
+    // Export button with various options for exporting the table data
     <button class="btn btn-warning btn-sm dropdown-toggle" data-toggle="dropdown">
 	<i class="fa fa-bars"></i> EXPORTAR </button>
 	<ul class="dropdown-menu " role="menu">
@@ -73,166 +80,11 @@
                       <th>DOWN</th>
                       <th>UP</th>
                       <th>Periodo</th>
-                      <th>Ação</th>
+                      <th>AÃ§Ã£o</th>
                     </tr>
                   </thead>
                   <tbody>
                   <?php
 
-        $consultas = $mysqli->query("SELECT * FROM assinaturas WHERE ip_ubnt <> ''");
-        while($campo = mysqli_fetch_array($consultas)){
-        $row = mysqli_num_rows($consultas);
-
-        $host   = $campo['ip_ubnt'];
-        $porta  = $campo['porta_ubnt'];
-        $use    = $campo['login_ubnt'];
-        $senha  = $campo['senha_ubnt'];
-
-        $conexao = conecta_ubnt($host, $use, $senha, $porta, "true");
-        $retorno = executa_ubnt("wstalist -i ath0", $conexao, "true");
-
-        //print_r($retorno);
-        //echo $conexao;
-
-
-        foreach ($retorno as $x) {
-            ?>
-            <tr>
-                <td><?php echo $campo['login']; ?></td>
-                <td><?php echo $campo['ip_ubnt']; ?></td>
-                <td><?php echo $x['mac']; ?></td>
-                <td><?php echo $x['ccq'] ?>%</td>
-                <td><?php echo $x['rx'] ?>Mbps</td>
-                <td><?php echo $x['tx'] ?>Mbps</td>
-                <td><?php echo $x['signal'] ?></td>
-                <td><?php
-                 $rxstats = $x['stats']['rx_bytes'];
-
-                     function rxbytes($rxstats)
-                     {
-                         if ($rxstats >= 1073741824)
-                         {
-                             $rxstats = number_format($rxstats / 1073741824, 2) . ' GB';
-                         }
-                         elseif ($rxstats >= 1048576)
-                         {
-                             $rxstats = number_format($rxstats / 1048576, 2) . ' MB';
-                         }
-                         elseif ($rxstats >= 1024)
-                         {
-                             $rxstats = number_format($rxstats / 1024, 2) . ' KB';
-                         }
-                         elseif ($rxstats > 1)
-                         {
-                             $rxstats = $rxstats . ' bytes';
-                         }
-                         elseif ($rxstats == 1)
-                         {
-                             $rxstats = $rxstats . ' byte';
-                         }
-                         else
-                         {
-                             $rxstats = '0 bytes';
-                         }
-
-                         return $rxstats;
-                     }
-
-                     echo rxbytes($rxstats);
-                    ?></td>
-                <td><?php
-
-                    $txstats = $x['stats']['tx_bytes'];
-
-                    function txbytes($txstats)
-                    {
-                        if ($txstats >= 1073741824)
-                        {
-                            $txstats = number_format($txstats / 1073741824, 2) . ' GB';
-                        }
-                        elseif ($txstats >= 1048576)
-                        {
-                            $txstats = number_format($txstats / 1048576, 2) . ' MB';
-                        }
-                        elseif ($txstats >= 1024)
-                        {
-                            $txstats = number_format($txstats / 1024, 2) . ' KB';
-                        }
-                        elseif ($txstats > 1)
-                        {
-                            $txstats = $txstats . ' bytes';
-                        }
-                        elseif ($txstats == 1)
-                        {
-                            $txstats = $txstats . ' byte';
-                        }
-                        else
-                        {
-                            $txstats = '0 bytes';
-                        }
-
-                        return $txstats;
-                    }
-
-                    echo txbytes($txstats);
-
-                    ?></td>
-                <td>
-                    <?php
-                    $total_segundos = $x['uptime'];
-                    $dias = intval($total_segundos / 86400);
-                    echo gmdate("$dias"." H:i:s", $total_segundos);
-
-                    ?>
-                </td>
-
-                <td>
-
-                </td>
-
-                </td>
-            </tr>
-
-        <?php
-        }
-    }
-
-    ?>
-                  </tbody>
-                  <tfoot>
-                    <tr>
-                      <th>Cliente</th>
-                      <th>Ip</th>
-                      <th>Mac</th>
-                      <th>CCQ</th>
-                      <th>RX</th>
-                      <th>TX</th>
-                      <th>SINAL</th>
-                      <th>DOWN</th>
-                      <th>UP</th>
-                      <th>Periodo</th>
-                      <th>Ação</th>
-                    </tr>
-                  </tfoot>
-                </table>
-              </div>
-            </div>
-        	
-          </div>
-        </div> 
-      </div>
-      <?php } else { ?>
-    <div class="page-header">
-        <h1>Permissão <small>Negada!</small></h1>
-    </div>
-
-    <div class="row" id="powerwidgets">
-        <div class="col-md-12 bootstrap-grid">
-
-            <div class="alert alert-danger alert-dismissable">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-                    <i class="fa fa-times-circle"></i></button>
-                <strong>Atenção!</strong> Você não possui permissão para esse modulo. </div>
-
-        </div></div>
-<?php } ?>
+        // Query to fetch data from the 'assinaturas' table where 'ip_ubnt' is not empty
+        $consultas = $mysqli->query("SELECT

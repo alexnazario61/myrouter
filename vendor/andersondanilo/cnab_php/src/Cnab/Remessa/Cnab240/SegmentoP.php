cd8eb50ae2a3f19f912b6fccffb51c9c346278df
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:40:49 -0300

    comment

--- vendor/andersondanilo/cnab_php/src/Cnab/Remessa/Cnab240/SegmentoP.php
+++ vendor/andersondanilo/cnab_php/src/Cnab/Remessa/Cnab240/SegmentoP.php
@@ -1,11 +1,18 @@
 <?php
+// Namespace for Cnab remessa Cnab240 classes
 namespace Cnab\Remessa\Cnab240;
 
+// SegmentoP class representing a line in a Cnab240 remessa file
 class SegmentoP extends \Cnab\Format\Linha
 {
+    // Constructor for SegmentoP class
+    //
+    // @param \Cnab\Remessa\IArquivo $arquivo The remessa file object
     public function __construct(\Cnab\Remessa\IArquivo $arquivo)
     {
+        // Load Yaml configuration for Cnab240 remessa file segmento P
         $yamlLoad = new \Cnab\Format\YamlLoad($arquivo->codigo_banco, $arquivo->layoutVersao);
         $yamlLoad->load($this, 'cnab240', 'remessa/detalhe_segmento_p');
     }
 }
+

91372dc9fc2bfe58963660ee4e51d7ef415ff4bb
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:24 -0300

    comment

--- app/remessa/_src/Cnab/Remessa/Cnab240/TrailerLote.php
+++ app/remessa/_src/Cnab/Remessa/Cnab240/TrailerLote.php
@@ -3,9 +3,17 @@ namespace Cnab\Remessa\Cnab240;
 
 class TrailerLote extends \Cnab\Format\Linha
 {
+    /**
+     * Constructs a TrailerLote object with the given remessa arquivo.
+     *
+     * @param \Cnab\Remessa\IArquivo $arquivo The remessa arquivo that this trailer lote belongs to.
+     */
     public function __construct(\Cnab\Remessa\IArquivo $arquivo)
     {
+        // Load the YAML file for the trailer lote format based on the bank code and layout version.
         $yamlLoad = new \Cnab\Format\YamlLoad($arquivo->codigo_banco, $arquivo->layoutVersao);
+        
+        // Load the trailer lote format into this object.
         $yamlLoad->load($this, 'cnab240', 'trailer_lote');
     }
 }

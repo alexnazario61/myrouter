2485692ba6513cd23ef8a56a303d0adcb4082d39
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:48:39 -0300

    improve

--- app/fiberhome/fiberhome.php
+++ app/fiberhome/fiberhome.php
@@ -1,34 +1,32 @@
 <?php
-// Define the IP address of the administration interface
-define(IPADMIN, '201.87.240.9');
 
-// Define the IP address of the TL1 server
-define(IPTL1SRV, '10.10.6.146');
-
-// Define the TL1 username and password
-define(TL1USR, '1');
-define(TL1PWD, '33#sysadmin#33');
+// Define constants
+define('IPADMIN', '201.87.240.9');
+define('IPTL1SRV', '10.10.6.146');
+define('TL1USR', '1');
+define('TL1PWD', '33#sysadmin#33');
 
 // Include the FiberHome class
 include 'FiberHome.class.php';
 
-// Connect to the FiberHome device
-echo "Conectando...\n";
-$fh = new FiberHome(IPADMIN, IPTL1SRV, TL1USR, TL1PWD, false);
+try {
+    // Connect to the FiberHome device
+    echo "Connecting...\n";
+    $fh = new FiberHome(IPADMIN, IPTL1SRV, TL1USR, TL1PWD, false);
 
-// List the ONU devices
-echo "Listando...\n";
-$onus = $fh->ONUList();
+    // List the ONU devices
+    echo "Listing...\n";
+    $onus = $fh->ONUList();
 
-// Commented out as it seems to be a duplicate of the ONUInfos method
-//echo "Obtendo status...\n";
-//$fh->ONUStates($onus);
+    // Obtain additional information about the ONU devices
+    echo "Getting extra information...\n";
+    $fh->ONUInfos($onus);
 
-// Obtain additional information about the ONU devices
-echo "Obtendo informações extras...\n";
-$fh->ONUInfos($onus);
-
-// Display the ONU devices' information in a preformatted format
-echo '<pre>';
-var_dump($onus);
-echo '</pre>';
+    // Display the ONU devices' information in a preformatted format
+    echo '<pre>';
+    var_dump($onus);
+    echo '</pre>';
+} catch (Exception $e) {
+    // Handle errors
+    echo 'Error: ' . $e->getMessage() . '\n';
+}

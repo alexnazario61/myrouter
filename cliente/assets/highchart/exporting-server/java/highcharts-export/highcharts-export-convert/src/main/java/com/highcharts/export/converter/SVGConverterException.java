3b0ad141bada8e60c456ce75b94bdaabbeddccb8
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:49:31 -0300

    improve

--- cliente/assets/highchart/exporting-server/java/highcharts-export/highcharts-export-convert/src/main/java/com/highcharts/export/converter/SVGConverterException.java
+++ cliente/assets/highchart/exporting-server/java/highcharts-export/highcharts-export-convert/src/main/java/com/highcharts/export/converter/SVGConverterException.java
@@ -11,21 +11,24 @@ package com.highcharts.export.converter;
 
 public class SVGConverterException extends Exception {
 
-	private static final long serialVersionUID = -5110552374074051446L;
-	private String mistake;
+    private static final long serialVersionUID = -5110552374074051446L;
+    private String errorMessage;
 
-	public SVGConverterException() {
-		super();
-		mistake = "unknown to men";
-	}
+    public SVGConverterException() {
+        this("Unknown error");
+    }
 
-	public SVGConverterException(String err) {
-		super(err); // call super class constructor
-		mistake = err; // save message
-	}
+    public SVGConverterException(String err) {
+        super(err); // call super class constructor
+        this.errorMessage = err; // save message
+    }
 
-	public String getError() {
-		return mistake;
-	}
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 
 }

a0aad916bccca56f97335d0a2cfed9a4c5369f89
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:35 -0300

    comment

--- assets/js/i18n/defaults-ua_UA.js
+++ assets/js/i18n/defaults-ua_UA.js
@@ -3,13 +3,36 @@
  *
  * Copyright 2013-2014 bootstrap-select
  * Licensed under MIT (https://github.com/silviomoreto/bootstrap-select/blob/master/LICENSE)
+ *
+ * This script is a jQuery plugin that enhances the default functionality of the HTML
+ * select element, providing customizable dropdowns with Bootstrap styling.
  */
+
+// Define the default options for the selectpicker function
 (function ($) {
   $.fn.selectpicker.defaults = {
+    // The text to display when no options are selected
     noneSelectedText: 'Нічого не вибрано',
+
+    // The text to display when there are no results found
     noneResultsText: 'Збігів не знайдено',
+
+    // The text to display when one or more options are selected
     countSelectedText: 'Вибрано {0} із {1}',
-    maxOptionsText: ['Досягнута межа ({n} {var} максимум)', 'Досягнута межа в групі ({n} {var} максимум)', ['items', 'item']],
+
+    // The text to display when the maximum number of options is reached
+    maxOptionsText: [
+      // The text for a single selection
+      'Досягнута межа ({n} {var} максимум)',
+
+      // The text for a group of selections
+      'Досягнута межа в групі ({n} {var} максимум)',
+
+      // The singular and plural forms of the word for the items
+      ['items', 'item']
+    ],
+
+    // The separator for multiple selected options
     multipleSeparator: ', '
   };
 }(jQuery));

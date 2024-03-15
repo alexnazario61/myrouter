f72fdbf2bea9245ee248c50bff7569e0c2c04df9
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:40:25 -0300

    comment

--- cliente/assets/highchart/exporting-server/java/highcharts-export/highcharts-export-convert/src/main/java/com/highcharts/export/util/MimeType.java
+++ cliente/assets/highchart/exporting-server/java/highcharts-export/highcharts-export-convert/src/main/java/com/highcharts/export/util/MimeType.java
@@ -13,42 +13,44 @@ import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * An enumeration representing various MIME types supported for image export.
+ * Each enumerated value represents a specific MIME type and its corresponding file extension.
+ */
 public enum MimeType {
-	PNG("image/png", "png"),
-	JPEG("image/jpeg", "jpeg"),
-	PDF("application/pdf", "pdf"),
-	SVG("image/svg+xml", "svg");
+	PNG("image/png", "png"), // PNG image format
+	JPEG("image/jpeg", "jpeg"), // JPEG image format
+	PDF("application/pdf", "pdf"), // PDF document format
+	SVG("image/svg+xml", "svg"); // Scalable Vector Graphics format
 
+	/**
+	 * A static map for quick lookups of MIME types based on their type or extension.
+	 */
 	private static final Map<String, MimeType> lookup = new HashMap<String, MimeType>();
 
 	static {
+		// Initialize the lookup map with all enumerated values
 		for (MimeType m : EnumSet.allOf(MimeType.class)) {
 			lookup.put(m.getType(), m);
 			lookup.put(m.getExtension(), m);
 		}
 	}
 
+	/**
+	 * The actual MIME type string.
+	 */
 	private String type;
+
+	/**
+	 * The file extension associated with the MIME type.
+	 */
 	private String extension;
 
+	/**
+	 * Constructor for the MimeType enumeration.
+	 *
+	 * @param type    The MIME type string.
+	 * @param extension The file extension associated with the MIME type.
+	 */
 	private MimeType(String type, String extension) {
-		this.type = type;
-		this.extension = extension;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public String getExtension() {
-		return extension;
-	}
-
-	public static MimeType get(String type) {
-		MimeType mime = lookup.get(type);
-		if (mime != null) {
-			return mime;
-		}
-		return MimeType.PNG;
-	}
-}
+		this.type = type

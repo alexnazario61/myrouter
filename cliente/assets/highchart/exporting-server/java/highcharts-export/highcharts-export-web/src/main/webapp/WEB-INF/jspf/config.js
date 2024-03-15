22679eae669c5fa720d1cf4792b7a30e80c72063
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:40:28 -0300

    comment

--- cliente/assets/highchart/exporting-server/java/highcharts-export/highcharts-export-web/src/main/webapp/WEB-INF/jspf/config.js
+++ cliente/assets/highchart/exporting-server/java/highcharts-export/highcharts-export-web/src/main/webapp/WEB-INF/jspf/config.js
@@ -1,8 +1,15 @@
+// This is a JavaScript object representing a chart configuration.
 {
+	// The 'xAxis' property contains the categories for the x-axis.
 	xAxis: {
+		// The 'categories' array contains the category labels for each data point.
 		categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 	},
+	// The 'series' property contains the data to be plotted.
 	series: [{
+		// Each object in the 'series' array represents a single data series.
 		data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+		// The 'data' array contains the values for each data point in the series.
 	}]
 };
+

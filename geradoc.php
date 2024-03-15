8cfc19967890860b950a149c8ad8e4b108fe691d
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:50:16 -0300

    improve

--- geradoc.php
+++ geradoc.php
@@ -1,100 +1,86 @@
 <?php
 
-    
-    require_once 'config/conexao.class.php';
-    require_once 'config/crud.class.php';
-    $con = new conexao(); // instancia classe de conxao
-    $con->connect(); // abre conexao com o banco
-    
-    $idass = base64_decode($_GET['id']); 
-    $imp = $mysqli->query("SELECT * FROM assinaturas WHERE id = '$idass'");
-    $assinatura = mysqli_fetch_array($imp);
-    
-    $idcliente = $assinatura['cliente'];
-    $clientes = $mysqli->query("SELECT * FROM clientes WHERE id = '$idcliente'");
-    $cliente = mysqli_fetch_array($clientes);
-    
-    $idplano = $assinatura['plano'];
-    $planos = $mysqli->query("SELECT * FROM planos WHERE id = '$idplano'");
-    $planoCli = mysqli_fetch_array($planos);
-    
-    $ers = $mysqli->query("SELECT * FROM empresa");
-    $er = mysqli_fetch_array($ers);
+require_once 'config/conexao.class.php';
+require_once 'config/crud.class.php';
 
-    header( "Content-type: application/msword" );
-    header( "Content-Disposition: inline, filename=contrato.rtf");
-    
-    $enome = $er['empresa'];
-    $ecnpj = $er['cnpj'];
-    $eend = $er['endereco'];
-    $contratocliente = $er['contratocliente'];
-    $ecidade = $er['cidade'];
-    $euf = $er['estado'];
-    $ecep = $er['cep'];
-    $etel = $er['tel1'];
-    $ecel = $er['tel2'];
-    
-    $cnome = $cliente['nome'];
-    $ccpf = $cliente['cpf'];
-    $crg = $cliente['rg'];
-    $cestadocivil = $cliente['estadocivil'];
-    $cdatanascimento = $cliente['nascimento'];
-    $cend = $cliente['endereco'];
-    $cnumero = $cliente['numero'];
-    $ccomplemento = $cliente['complemento'];
-    $cbairro = $cliente['bairro'];
-    $ccidade =$cliente['cidade'];
-    $cuf = $cliente['estado'];
-    $ccep = $cliente['cep'];
-    $ctel = $cliente['tel'];
-    $ccel = $cliente['cel'];
-    $cemail = $cliente['email'];
+$con = new conexao();
+$con->connect();
 
-    $plano = $planoCli['nome'];
-    $preco = $planoCli['preco'];
-    $data_hoje = date('d-m-Y');
+$idass = filter_var(base64_decode($_GET['id']), FILTER_SANITIZE_NUMBER_INT);
 
+$imp = $con->query("SELECT * FROM assinaturas WHERE id = $idass");
+$assinatura = $imp->fetch_assoc();
 
-// Abre seu template do contrato
-    $arquivo = "assets/$contratocliente";
-    $fp = fopen ($arquivo, "r");
+$idcliente = $assinatura['cliente'];
+$clientes = $con->query("SELECT * FROM clientes WHERE id = $idcliente");
+$cliente = $clientes->fetch_assoc();
 
-    //Le o template na variavel
-    $output = fread($fp, filesize($arquivo));
-  
-    fclose ($fp);
-  
-    //Substitui as tags pelas vari�veis
-    $output = str_replace( "<<r_nome>>", $enome, $output );
-    $output = str_replace( "<<r_cnpj>>", $ecnpj, $output );
-    $output = str_replace( "<<r_end>>", $eend, $output );
-    $output = str_replace( "<<r_cidade>>", $ecidade, $output );
-    $output = str_replace( "<<r_estado>>", $euf, $output );
-    $output = str_replace( "<<r_cep>>", $ecep, $output );
-    $output = str_replace( "<<cliente_nome>>", $cnome, $output );
-    $output = str_replace( "<<cliente_cpf>>", $ccpf, $output );
-    $output = str_replace( "<<cliente_rg>>", $crg, $output );
-    $output = str_replace( "<<cliente_estadocivil>>", $cestadocivil, $output );
-    $output = str_replace( "<<cliente_end>>", $cend, $output );
-    $output = str_replace( "<<cliente_numero>>", $cnumero, $output );
-    $output = str_replace( "<<cliente_complemento>>", $ccomplemento, $output );
-    $output = str_replace( "<<cliente_bairro>>", $cbairro, $output );
-    $output = str_replace( "<<cliente_cidade>>", $ccidade, $output );
-    $output = str_replace( "<<cliente_estado>>", $cuf, $output );
-    $output = str_replace( "<<cliente_cep>>", $ccep, $output );
-    $output = str_replace( "<<cliente_tel>>", $ctel, $output );
-    $output = str_replace( "<<cliente_cel>>", $ccel, $output );
-    $output = str_replace( "<<cliente_email>>", $cemail, $output );
-    $output = str_replace( "<<cliente_datanascimento>>", $cdatanascimento, $output );
-    $output = str_replace( "<<plano_nome>>", $plano, $output );
-    $output = str_replace( "<<valor>>", $preco, $output );
-    $output = str_replace( "<<data_hoje>>", $data_hoje, $output );
+$idplano = $assinatura['plano'];
+$planos = $con->query("SELECT * FROM planos WHERE id = $idplano");
+$planoCli = $planos->fetch_assoc();
 
+$ers = $con->query("SELECT * FROM empresa");
+$er = $ers->fetch_assoc();
 
+$enome = htmlspecialchars($er['empresa']);
+$ecnpj = htmlspecialchars($er['cnpj']);
+$eend = htmlspecialchars($er['endereco']);
+$contratocliente = htmlspecialchars($er['contratocliente']);
+$ecidade = htmlspecialchars($er['cidade']);
+$euf = htmlspecialchars($er['estado']);
+$ecep = htmlspecialchars($er['cep']);
+$etel = htmlspecialchars($er['tel1']);
+$ecel = htmlspecialchars($er['tel2']);
 
+$cnome = htmlspecialchars($cliente['nome']);
+$ccpf = htmlspecialchars($cliente['cpf']);
+$crg = htmlspecialchars($cliente['rg']);
+$cestadocivil = htmlspecialchars($cliente['estadocivil']);
+$cdatanascimento = htmlspecialchars($cliente['nascimento']);
+$cend = htmlspecialchars($cliente['endereco']);
+$cnumero = htmlspecialchars($cliente['numero']);
+$ccomplemento = htmlspecialchars($cliente['complemento']);
+$cbairro = htmlspecialchars($cliente['bairro']);
+$ccidade = htmlspecialchars($cliente['cidade']);
+$cuf = htmlspecialchars($cliente['estado']);
+$ccep = htmlspecialchars($cliente['cep']);
+$ctel = htmlspecialchars($cliente['tel']);
+$ccel = htmlspecialchars($cliente['cel']);
+$cemail = htmlspecialchars($cliente['email']);
 
-//Envia documento para o browser
-    echo $output;
-	
+$plano = htmlspecialchars($planoCli['nome']);
+$preco = htmlspecialchars($planoCli['preco']);
+$data_hoje = date('d-m-Y');
 
-?>
+$arquivo = "assets/$contratocliente";
+
+if (!file_exists($arquivo) || !is_readable($arquivo)) {
+    die('O arquivo do contrato não existe ou não é legível.');
+}
+
+$fp = fopen($arquivo, "r");
+$output = fread($fp, filesize($arquivo));
+fclose($fp);
+
+$output = str_replace(
+    [
+        '<<r_nome>>', '<<r_cnpj>>', '<<r_end>>', '<<r_cidade>>', '<<r_estado>>', '<<r_cep>>',
+        '<<cliente_nome>>', '<<cliente_cpf>>', '<<cliente_rg>>', '<<cliente_estadocivil>>',
+        '<<cliente_end>>', '<<cliente_numero>>', '<<cliente_complemento>>', '<<cliente_bairro>>',
+        '<<cliente_cidade>>', '<<cliente_estado>>', '<<cliente_cep>>', '<<cliente_tel>>',
+        '<<cliente_cel>>', '<<cliente_email>>', '<<cliente_datanascimento>>', '<<plano_nome>>',
+        '<<valor>>', '<<data_hoje>>'
+    ],
+    [
+        $enome, $ecnpj, $eend, $ecidade, $euf, $ecep,
+        $cnome, $ccpf, $crg, $cestadocivil, $cend, $cnumero,
+        $ccomplemento, $cbairro, $ccidade, $cuf, $ccep, $ctel,
+        $ccel, $cemail, $cdatanascimento, $plano, $preco, $data_hoje
+    ],
+    $output
+);
+
+header('Content-Type: application/msword');
+header('Content-Disposition: inline, filename=contrato.rtf');
+
+echo $output;

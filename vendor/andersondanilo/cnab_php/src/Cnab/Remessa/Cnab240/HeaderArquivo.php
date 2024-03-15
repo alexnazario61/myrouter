21f136248546915ba47ef83a1e0fd7416149a215
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:40:48 -0300

    comment

--- vendor/andersondanilo/cnab_php/src/Cnab/Remessa/Cnab240/HeaderArquivo.php
+++ vendor/andersondanilo/cnab_php/src/Cnab/Remessa/Cnab240/HeaderArquivo.php
@@ -3,9 +3,17 @@ namespace Cnab\Remessa\Cnab240;
 
 class HeaderArquivo extends \Cnab\Format\Linha
 {
+    /**
+     * Constructor for the HeaderArquivo class.
+     *
+     * @param \Cnab\Remessa\IArquivo $arquivo The remittance archive object.
+     */
     public function __construct(\Cnab\Remessa\IArquivo $arquivo)
     {
+        // Load the YAML file for the specified bank code and layout version.
         $yamlLoad = new \Cnab\Format\YamlLoad($arquivo->codigo_banco, $arquivo->layoutVersao);
+        
+        // Load the header_arquivo format from the YAML file into this object.
         $yamlLoad->load($this, 'cnab240', 'header_arquivo');
     }
 }

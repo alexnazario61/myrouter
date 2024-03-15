c4db372c031e9ac11defdf8ac966176c067d4698
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:49:02 -0300

    improve

--- app/mikrotik/natmk.php
+++ app/mikrotik/natmk.php
@@ -1,51 +1,70 @@
 <?php
 
-// Check if the form has been submitted
-if(isset ($_POST['cadastrar'])){
+// Include the database configuration file
+include 'dbconfig.php';
 
-    // Assign form data to variables
-    $chain = $_POST['chain'];
-    $dstaddress = $_POST['dstaddress'];
-    $srcaddress = $_POST['srcaddress'];
-    $naosrcaddresslist = $_POST['naosrcaddresslist'];
-    $srcaddresslist = $_POST['srcaddresslist'];
-    $naodstaddresslist = $_POST['naodstaddresslist'];
-    $dstaddresslist = $_POST['dstaddresslist'];
-    $protocolo = $_POST['protocolo'];
-    $naointerfacein = $_POST['naointerfacein'];
-    $interfacein = $_POST['interfacein'];
-    $naointerfaceout = $_POST['naointerfaceout'];
-    $interfaceout = $_POST['interfaceout'];
-    $dstport = $_POST['dstport'];
-    $toaddresses = $_POST['toaddresses'];
-    $toports = $_POST['toports'];
-    $conteudo = $_POST['conteudo'];
-    $comentario = $_POST['comentario'];
-    $servidor = $_POST['servidor'];
-    $cliente = $_POST['cliente'];
-    $action = $_POST['action'];
+// Check if the form has been submitted
+if(isset($_POST['cadastrar'])) {
+
+    // Assign form data to variables and sanitize the inputs
+    $chain = filter_input(INPUT_POST, 'chain', FILTER_SANITIZE_STRING);
+    $dstaddress = filter_input(INPUT_POST, 'dstaddress', FILTER_SANITIZE_STRING);
+    $srcaddress = filter_input(INPUT_POST, 'srcaddress', FILTER_SANITIZE_STRING);
+    $naosrcaddresslist = filter_input(INPUT_POST, 'naosrcaddresslist', FILTER_SANITIZE_STRING);
+    $srcaddresslist = filter_input(INPUT_POST, 'srcaddresslist', FILTER_SANITIZE_STRING);
+    $naodstaddresslist = filter_input(INPUT_POST, 'naodstaddresslist', FILTER_SANITIZE_STRING);
+    $dstaddresslist = filter_input(INPUT_POST, 'dstaddresslist', FILTER_SANITIZE_STRING);
+    $protocolo = filter_input(INPUT_POST, 'protocolo', FILTER_SANITIZE_STRING);
+    $naointerfacein = filter_input(INPUT_POST, 'naointerfacein', FILTER_SANITIZE_STRING);
+    $interfacein = filter_input(INPUT_POST, 'interfacein', FILTER_SANITIZE_STRING);
+    $naointerfaceout = filter_input(INPUT_POST, 'naointerfaceout', FILTER_SANITIZE_STRING);
+    $interfaceout = filter_input(INPUT_POST, 'interfaceout', FILTER_SANITIZE_STRING);
+    $dstport = filter_input(INPUT_POST, 'dstport', FILTER_SANITIZE_NUMBER_INT);
+    $toaddresses = filter_input(INPUT_POST, 'toaddresses', FILTER_SANITIZE_STRING);
+    $toports = filter_input(INPUT_POST, 'toports', FILTER_SANITIZE_STRING);
+    $conteudo = filter_input(INPUT_POST, 'conteudo', FILTER_SANITIZE_STRING);
+    $comentario = filter_input(INPUT_POST, 'comentario', FILTER_SANITIZE_STRING);
+    $servidor = filter_input(INPUT_POST, 'servidor', FILTER_SANITIZE_NUMBER_INT);
+    $cliente = filter_input(INPUT_POST, 'cliente', FILTER_SANITIZE_STRING);
+    $action = filter_input(INPUT_POST, 'action', FILTER_SANITIZE_STRING);
 
     // Concatenate srcaddresslist variables
-    $srcaddresslistFull = $naosrcaddresslist.$srcaddresslist;
+    $srcaddresslistFull = $naosrcaddresslist . $srcaddresslist;
 
     // Concatenate dstaddresslist variables
-    $dstaddresslistFull = $naodstaddresslist.$dstaddresslist;
+    $dstaddresslistFull = $naodstaddresslist . $dstaddresslist;
 
     // Concatenate interfacein variables
-    $interfaceinFull = $naointerfacein.$interfacein;
+    $interfaceinFull = $naointerfacein . $interfacein;
 
     // Concatenate interfaceout variables
-    $interfaceoutFull = $naointerfaceout.$interfaceout;
+    $interfaceoutFull = $naointerfaceout . $interfaceout;
 
     // Create a new instance of the crud class and insert form data into the 'firewall' table
-    $crud = new crud('firewall');  // Pass table name as parameter
-    $crud->inserir("chain,dstaddress,srcaddress,naosrcaddresslist,srcaddresslist,naodstaddresslist,dstaddresslist,protocolo,naointerfacein,interfacein,naointerfaceout,interfaceout,dstport,toaddresses,toports,cliente,conteudo,comentario,servidor,action", "'$chain','$dstaddress','$srcaddress','$naosrcaddresslist','$srcaddresslist','$naodstaddresslist','$dstaddresslist','$protocolo','$naointerfacein','$interfacein','$naointerfaceout','$interfaceout','$dstport','$toaddresses','$toports','$cliente','$conteudo','$comentario','$servidor','$action'");
+    $crud = new crud('firewall'); // Pass table name as parameter
+
+    // Prepare the SQL statement and bind the parameters
+    $stmt = $mysqli->prepare("INSERT INTO firewall (chain, dstaddress, srcaddress, naosrcaddresslist, srcaddresslist, naodstaddresslist, dstaddresslist, protocolo, naointerfacein, interfacein, naointerfaceout, interfaceout, dstport, toaddresses, toports, cliente, conteudo, comentario, servidor, action) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssssssssssssssss", $chain, $dstaddress, $srcaddress, $naosrcaddresslist, $srcaddresslist, $naodstaddresslist, $dstaddresslist, $protocolo, $naointerfacein, $interfacein, $naointerfaceout, $interfaceout, $dstport, $toaddresses, $toports, $cliente, $conteudo, $comentario, $servidor, $action);
+
+    // Execute the SQL statement
+    $result = $stmt->execute();
+
+    // Check for errors
+    if ($result === false) {
+        die("Error: " . $mysqli->error);
+    }
 
     // Query the 'servidores' table to retrieve server details
-    $servidor = $mysqli->query("SELECT * FROM servidores WHERE id = '$servidor'");
+    $stmt = $mysqli->prepare("SELECT * FROM servidores WHERE id = ?");
+    $stmt->bind_param("i", $servidor);
+    $stmt->execute();
+
+    // Get the result
+    $result = $stmt->get_result();
 
     // Fetch server data from the result set
-    $mk = mysqli_fetch_array($servidor);
+    $server = $result->fetch_assoc();
 
     // Create a new instance of the routeros_api class
     $API = new routeros_api();
@@ -54,69 +73,11 @@ if(isset ($_POST['cadastrar'])){
     $API->debug = false;
 
     // Connect to the MikroTik router
-    if ($API->connect(''.$mk[ip].'', ''.$mk[login].'', ''.$mk[senha].''))
-    {
+    if ($API->connect($server['ip'], $server['login'], $server['senha'])) {
 
         // Write NAT configuration to the router
-        $API->write('/ip/firewall/nat/add',false);
-        $API->write('=chain='.$chain.'',false);
-        $API->write('=action='.$action.'',false );
-        if($comentario !='') {
-            $API->write('=comment='.$comentario.'', false);
-        }
-        if($conteudo !='') {
-            $API->write('=content='.$conteudo.'',false);
-        }
-        if($srcaddress !='') {
-            $API->write('=src-address='.$srcaddress.'',false);
-        }
-        if($dstaddress !=''){
-            $API->write('=dst-address='.$dstaddress.'',false );
-        }
+        $API->write('/ip/firewall/nat/add', false);
+        $API->write('=chain=' . $chain, false);
+        $API->write('=action=' . $action, false);
 
-        if($srcaddresslist !='') {
-            $API->write('=src-address-list='.$srcaddresslistFull.'', false);
-        }
-        if($dstaddresslist !=''){
-            $API->write('=dst-address-list='.$dstaddresslistFull.'',false );
-        }
 
-        if($interfacein !='') {
-            $API->write('=in-interface='.$interfaceinFull.'', false);
-        }
-
-        if($interfaceout !='') {
-            $API->write('=out-interface='.$interfaceoutFull.'', false);
-        }
-
-        if($toaddresses !='') {
-            $API->write('=to-addresses='.$toaddresses.'',false);
-        }
-        if($toports !='') {
-            $API->write('=to-ports='.$toports.'',false);
-        }
-        $API->write('=protocol='.$protocolo.'',false );
-        $API->write('=dst-port='.$dstport.'',false );
-        $API->write('=disabled=no' );
-
-        // Read the router's response
-        $ARRAY = $API->read();
-  
-    }
-
-    // Redirect the user to the index page
-    header("Location: index.php?app=NatMKPRINT&reg=1");
-}
-
-// Check if the 'Ex' and 'id' GET parameters are set
-if ((isset($_GET["Ex"])) && ($_GET["Ex"] == "Del")) {
-
-    // Decode the 'id' and 'srv' GET parameters
-    $id = base64_decode($_GET['id']);
-    $servidor = base64_decode($_GET['srv']);
-
-    // Create a new instance of the crud class and delete the record with the specified ID
-    $crud = new crud('firewall');  // Pass table name as parameter
-    $crud->excluir("id = $id"); // Delete record with the specified ID
-
-    // Query the 'servidores' table

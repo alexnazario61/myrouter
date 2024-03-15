2e655e841d76d9bcd6cd639a707b6d9d7e75e480
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:48:44 -0300

    improve

--- app/remessa/_src/Cnab/Format/Picture.php
+++ app/remessa/_src/Cnab/Format/Picture.php
@@ -3,18 +3,23 @@ namespace Cnab\Format;
 
 class Picture
 {
-	// Regular expression for validating the format string
-	const REGEX_VALID_FORMAT = '/(?P<tipo1>X|9)\((?P<tamanho1>[0-9]+)\)(?P<tipo2>(V9)?)\(?(?P<tamanho2>([0-9]+)?)\)?/';
+    // Regular expression for validating the format string
+    const REGEX_VALID_FORMAT = '/(?P<tipo1>X|9)\((?P<tamanho1>[0-9]+)\)(?P<tipo2>(V9)?)\(?(?P<tamanho2>([0-9]+)?)\)?/';
 
-	/**
-	 * Validates if the given format string is valid
-	 *
-	 * @param string $format The format string to validate
-	 * @return bool True if the format string is valid, false otherwise
-	 */
-	public static function validarFormato($format)
-	{
-		if(\preg_match(self::REGEX_VALID_FORMAT, $format))
-			return true;
-		else
-		
+    /**
+     * Validates if the given format string is valid
+     *
+     * @param string $format The format string to validate
+     * @return bool True if the format string is valid, false otherwise
+     */
+    public static function validarFormato($format)
+    {
+        if (preg_match(self::REGEX_VALID_FORMAT, $format, $matches)) {
+            // If the format is valid, return true
+            return true;
+        } else {
+            // If the format is invalid, return false with an error message
+            return false, "Invalid format: $format";
+        }
+    }
+}

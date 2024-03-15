d41ebe97828180cf018a7470c90cf22637f39fdb
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:50:16 -0300

    improve

--- vendor/mustangostang/spyc/examples/yaml-dump.php
+++ vendor/mustangostang/spyc/examples/yaml-dump.php
@@ -1,25 +1,32 @@
 <?php
 
+namespace MyProject\Spyc;
+
+declare(strict_types=1);
+
+use Spyc;
+
 #
 #    S P Y C
-#      a simple php yaml class
+#      A Simple PHP YAML Class
 #
 # Feel free to dump an array to YAML, and then to load that YAML back into an
-# array.  This is a good way to test the limitations of the parser and maybe
+# array. This is a good way to test the limitations of the parser and maybe
 # learn some basic YAML.
 #
 
-include('../Spyc.php');
+$array = [
+    'Sequence item',
+    'The Key' => 'Mapped value',
+    ['A sequence', 'of a sequence'],
+    ['first' => 'A sequence', 'second' => 'of mapped values'],
+    'Mapped' => ['A sequence', 'which is mapped'],
+    'A Note' => 'What if your text is too long?',
+    'Another Note' => 'If that is the case, the dumper will probably fold your text by using a block. Kinda like this.',
+    'The trick?' => 'The trick is that we overrode the default indent, 2, to 4 and the default wordwrap, 40, to 60.',
+    'Old Dog' => "And if you want\n to preserve line breaks, \ngo ahead!",
+    'key:withcolon' => "Should support this to",
+];
 
-$array[] = 'Sequence item';
-$array['The Key'] = 'Mapped value';
-$array[] = array('A sequence','of a sequence');
-$array[] = array('first' => 'A sequence','second' => 'of mapped values');
-$array['Mapped'] = array('A sequence','which is mapped');
-$array['A Note'] = 'What if your text is too long?';
-$array['Another Note'] = 'If that is the case, the dumper will probably fold your text by using a block.  Kinda like this.';
-$array['The trick?'] = 'The trick is that we overrode the default indent, 2, to 4 and the default wordwrap, 40, to 60.';
-$array['Old Dog'] = "And if you want\n to preserve line breaks, \ngo ahead!";
-$array['key:withcolon'] = "Should support this to";
-
-$yaml = Spyc::YAMLDump($array,4,60);
+$spyc = new Spyc();
+$yaml = $spyc->YAMLDump($array, 4, 60);

caa1d8c5426e28384e7c5ca9d47a7d4a998cd9d7
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:40:49 -0300

    comment

--- vendor/andersondanilo/cnab_php/src/Cnab/Retorno/Cnab240/HeaderLote.php
+++ vendor/andersondanilo/cnab_php/src/Cnab/Retorno/Cnab240/HeaderLote.php
@@ -3,9 +3,17 @@ namespace Cnab\Retorno\Cnab240;
 
 class HeaderLote extends \Cnab\Format\Linha
 {
-	public function __construct(\Cnab\Retorno\IArquivo $arquivo)
+    /**
+     * Constructor for the HeaderLote class.
+     *
+     * @param \Cnab\Retorno\IArquivo $arquivo The arquivo object that this header lote belongs to.
+     */
+    public function __construct(\Cnab\Retorno\IArquivo $arquivo)
     {
+        // Load the YAML file for the specified bank code and layout version.
         $yamlLoad = new \Cnab\Format\YamlLoad($arquivo->codigo_banco, $arquivo->layoutVersao);
+        
+        // Load the header_lote format from the YAML file into this object.
         $yamlLoad->load($this, 'cnab240', 'header_lote');
-	}
+    }
 }

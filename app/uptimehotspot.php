5b0cd12851acbc7e055eb7ddaf10dd5a7be8e7f2
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:29 -0300

    comment

--- app/uptimehotspot.php
+++ app/uptimehotspot.php
@@ -1,17 +1,28 @@
 <?php
+
+// Include the MikroTik API class
 require('../config/mikrotik.class.php');
 
+// Initialize the API object
 $API = new routeros_api();
+
+// Disable debug mode
 $API->debug = false;
-if ($API->connect(''.$_GET['ip'].'', ''.$_GET['login'].'', ''.$_GET['senha'].'')) {
 
-$ARRAY = $API->comm("/ip/hotspot/active/print");
+// Connect to the router using IP, login, and password from GET request
+if ($API->connect($_GET['ip'], $_GET['login'], $_GET['senha'])) {
 
-$first = $ARRAY['0'];
+  // Execute the command to retrieve active hotspot users
+  $ARRAY = $API->comm("/ip/hotspot/active/print");
+
+  // Get the first element from the returned array
+  $first = $ARRAY['0'];
+
+  // Output the uptime of the first user
+  ?>
+  <?php echo $first['uptime']; ?>
+
+  <?php
+}
+
 ?>
-<?php echo $first['uptime']; ?>
-
-<?php } ?>
-
-
-

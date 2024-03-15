eb87da8f98d52c03e95457f1860153dd8c52442d
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:49:33 -0300

    improve

--- cliente/assets/highchart/exporting-server/java/highcharts-export/highcharts-export-convert/src/main/java/com/highcharts/export/pool/BlockingQueuePool.java
+++ cliente/assets/highchart/exporting-server/java/highcharts-export/highcharts-export-convert/src/main/java/com/highcharts/export/pool/BlockingQueuePool.java
@@ -1,52 +1,48 @@
 package com.highcharts.export.pool;
 
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
+public class BlockingQueuePool<T> extends AbstractPool<T> {
 
-public class BlockingQueuePool<T> extends AbstractPool<T>{
+    private final BlockingQueue<T> linkQueue;
 
-	LinkedBlockingQueue<T> linkQueue;
-	/**
-	 * @param factory
-	 * @param number
-	 * @throws PoolException
-	 */
+    /**
+     * @param factory
+     * @param number
+     * @throws PoolException
+     */
+    public BlockingQueuePool(ObjectFactory<T> factory, int number, int maxWait) throws PoolException {
+        super(factory, number, maxWait);
+        linkQueue = new LinkedBlockingQueue<>(number);
+    }
 
-	public BlockingQueuePool(ObjectFactory<T> factory, int number, int maxWait) throws PoolException {
-		super(factory, number, maxWait);
-		queue = new LinkedBlockingQueue<T>();
-		linkQueue = (LinkedBlockingQueue<T>) queue;
-	}
+    @Override
+    public T borrowObject() throws InterruptedException, PoolException {
+        T object = linkQueue.poll(2000, TimeUnit.MILLISECONDS);
+        if (object == null) {
+            throw new PoolException("Could not borrow object from the pool within the allowed wait time.");
+        }
+        poolSize.getAndDecrement();
 
+        return object;
+    }
 
-	@Override
-	public T borrowObject() throws InterruptedException, PoolException {
-		T object = linkQueue.poll(2000, TimeUnit.MILLISECONDS);
-		if (object == null) {
-			throw new PoolException();
-		}
-		poolSize.getAndDecrement();
+    @Override
+    public void returnObject(T object, boolean validate) throws InterruptedException {
+        if (object == null) {
+            return;
+        }
 
-		return object;
-	}
+        boolean valid = (validate && objectFactory.validate(object)) || !validate;
 
-	@Override
-	public void returnObject(T object, boolean validate) throws InterruptedException {
-		if (object == null) {
-			return;
-		}
-
-		boolean valid = ( !validate || objectFactory.validate(object)) ? true : false;
-
-		if (!valid) {
-			destroyObject(object);
-		} else {
-			objectFactory.passivate(object);
-			linkQueue.offer(object, maxWait, TimeUnit.MILLISECONDS);
-			poolSize.incrementAndGet();
-		}
-	}
-
-
+        if (!valid) {
+            destroyObject(object);
+        } else {
+            objectFactory.passivate(object);
+            linkQueue.offer(object, maxWait, TimeUnit.MILLISECONDS);
+            poolSize.incrementAndGet();
+        }
+    }
 }

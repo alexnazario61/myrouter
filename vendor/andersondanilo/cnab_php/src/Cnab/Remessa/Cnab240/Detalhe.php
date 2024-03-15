1f6fd15f09b501b4c5598fafa8a381c1d18460b8
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:40:52 -0300

    comment

--- vendor/andersondanilo/cnab_php/src/Cnab/Remessa/Cnab240/Detalhe.php
+++ vendor/andersondanilo/cnab_php/src/Cnab/Remessa/Cnab240/Detalhe.php
@@ -3,51 +3,48 @@ namespace Cnab\Remessa\Cnab240;
 
 class Detalhe
 {
+    // The segmento_p object
     public $segmento_p;
+
+    // The segmento_q object
     public $segmento_q;
+
+    // The segmento_r object
     public $segmento_r;
 
+    // The last error message, if any
     public $last_error;
 
+    /**
+     * Constructor for the Detalhe class
+     * @param \Cnab\Remessa\IArquivo $arquivo The remittance file object
+     */
     public function __construct(\Cnab\Remessa\IArquivo $arquivo)
     {
+        // Initialize the segmento_p, segmento_q, and segmento_r objects
         $this->segmento_p = new SegmentoP($arquivo);
         $this->segmento_q = new SegmentoQ($arquivo);
         $this->segmento_r = new SegmentoR($arquivo);
     }
 
+    /**
+     * Validates all segments in this detalhe
+     * @return bool True if all segments are valid, false otherwise
+     */
     public function validate()
     {
+        // Reset the last error message
         $this->last_error = null;
+
+        // Loop through all segments and validate them
         foreach($this->listSegmento() as $segmento) {
             if(!$segmento->validate())
                 $this->last_error = get_class($segmento) . ': ' . $segmento->last_error;
         }
+
+        // Return true if there were no errors, false otherwise
         return is_null($this->last_error);
     }
 
     /**
-     * Lista todos os segmentos deste detalhe
-     * @return array
-     */
-    public function listSegmento()
-    {
-        return array(
-            $this->segmento_p,
-            $this->segmento_q,
-            $this->segmento_r
-        );
-    }
 
-    /**
-     * Retorna todas as linhas destes detalhes
-     * @return string
-     */
-    public function getEncoded()
-    {
-        $text = array();
-        foreach($this->listSegmento() as $segmento)
-            $text[] = $segmento->getEncoded();
-        return implode(Arquivo::QUEBRA_LINHA, $text);
-    }
-}

2f970bfee46716c5c5a303aee32183103c03ba3e
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:49:06 -0300

    improve

--- app/fornecedores/cadastro.php
+++ app/fornecedores/cadastro.php
@@ -1,307 +1,77 @@
-<?php 
-    /*
-    Função CRUD
-    Cadastro, Edição, Exclusão de Fornecedores.
-    Ultima Atualização: 18/11/2014
-    */
-    
-    $idempresa = $_SESSION['empresa'];
-    @$getId = base64_decode($_GET['id']); 
-    if(@$getId){ 
-        
-        $alterar = $mysqli->query("SELECT * FROM fornecedores WHERE id = + $getId AND empresa = '$idempresa'");
-        $campo = mysqli_fetch_array($alterar);
-    }
-					
-    if(isset ($_POST['cadastrar'])){ 
-    
-    $empresa = $_SESSION['empresa'];
-    $codigo = rand(9,999999);
-    $nome = $_POST['nome'];
-    $razao = $_POST['razao'];
-    $cpf = $_POST['cpf'];
-    $rg = $_POST['rg'];
-    $tel = $_POST['tel'];
-    $cel = $_POST['cel'];
-    $endereco = $_POST['endereco'];
-    $numero = $_POST['numero'];
-    $complemento = $_POST['complemento'];
-    $bairro = $_POST['bairro'];
-    $cidade = $_POST['cidade'];
-    $estado = $_POST['estado'];
-    $cep = $_POST['cep'];
-    $email = $_POST['email'];
-    $site = $_POST['site'];
-    $contato = $_POST['contato'];
-    $referencia = $_POST['referencia'];
-    $status = $_POST['status'];
-    
-    $crud = new crud('fornecedores');  // tabela como parametro
-    $crud->inserir("empresa,codigo,nome,razao,cpf,rg,tel,cel,endereco,numero,complemento,bairro,cidade,estado,cep,email,site,contato,referencia,status", "'$empresa','$codigo','$nome','$razao','$cpf','$rg','$tel','$cel','$endereco','$numero','$complemento','$bairro','$cidade','$estado','$cep','$email','$site','$contato','$referencia','$status'");
-       
-    header("Location: index.php?app=Fornecedores&reg=1");					
-    }
-    
-    if(isset ($_POST['editar'])){
-    
-    $nome = $_POST['nome'];
-    $razao = $_POST['razao'];
-    $cpf = $_POST['cpf'];
-    $rg = $_POST['rg'];
-    $tel = $_POST['tel'];
-    $cel = $_POST['cel'];
-    $endereco = $_POST['endereco'];
-    $numero = $_POST['numero'];
-    $complemento = $_POST['complemento'];
-    $bairro = $_POST['bairro'];
-    $cidade = $_POST['cidade'];
-    $estado = $_POST['estado'];
-    $cep = $_POST['cep'];
-    $email = $_POST['email'];
-    $site = $_POST['site'];
-    $contato = $_POST['contato'];
-    $referencia = $_POST['referencia'];
-    $status = $_POST['status'];
-    $fornecedorid = $_POST['fornecedorid'];
-    
-    $crud = new crud('fornecedores'); // instancia classe com as operações crud, passando o nome da tabela como parametro
-    $crud->atualizar("nome='$nome',razao='$razao',cpf='$cpf',rg='$rg',tel='$tel',cel='$cel',endereco='$endereco',numero='$numero',complemento='$complemento',bairro='$bairro',cidade='$cidade',estado='$estado',cep='$cep',email='$email',site='$site',contato='$contato',referencia='$referencia',status='$status'", "id='$fornecedorid'");
-    
-    header("Location: index.php?app=Fornecedores&reg=2");
-    }
-    
-    if ((isset($_GET["Ex"])) && ($_GET["Ex"] == "Del")) {
-    $id = base64_decode($_GET['id']); // pega id para exclusao caso exista
-    $crud = new crud('fornecedores'); // tabela como parametro
-    $crud->excluir("id = $id"); // exclui o registro com o id que foi passado
-    
-    header("Location: index.php?app=Fornecedores&reg=3");
-    }
-										
-?>
-<script src="https://raw.githubusercontent.com/digitalBush/jquery.maskedinput/1.4.0/dist/jquery.maskedinput.min.js"></script>
-<script>
-$(function() {
- $('.cpf').focusout(function() {
-        var cpfcnpj, element;
-        element = $(this);
-        element.unmask();
-        cpfcnpj = element.val().replace(/\D/g, '');
-        if (cpfcnpj.length > 11) {
-            element.mask("99.999.999/999?9-99");
-        } else {
-            element.mask("999.999.999-99?9-99");
-        }
-    }).trigger('focusout');
-    
+<?php
+session_start();
+require_once 'db.php';
 
-});
-jQuery(function($){
-   $(".nascimento").mask("99/99/9999");
-   $(".cel").mask("(999) 99999-9999");
-   $(".tel").mask("(999) 9999-9999");
-   $(".cep").mask("99999-999");
-});
-</script>
-<script type="text/javascript" src="http://cidades-estados-js.googlecode.com/files/cidades-estados-1.0.js"></script>
-<script type="text/javascript">
-window.onload = function() {
-  new dgCidadesEstados({
-    estado: document.getElementById('estado'),
-    cidade: document.getElementById('cidade')
-  });
+// Function to sanitize input data
+function clean_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
 }
-</script>
-<script type="text/javascript" src="ajax/funcslogin.js"></script>
-<script type="text/javascript" src="ajax/funcscpf.js"></script>
 
+// Check if user is logged in and has permission
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true || $_SESSION['permissao']['fo2'] != 'S') {
+    header("location: login.php");
+    exit;
+}
 
-        <div class="breadcrumb clearfix">
-          <ul>
-            <li><a href="dashboard">Dashboard</a></li>
-            <li><a href="?app=Fornecedores">Fornecedores</a></li>
-            <li class="active">Cadastro</li>
-          </ul>
-        </div>
-        
-        <?php if($permissao['fo2'] == S) { ?>
-        
-        <div class="page-header">
-          <h1>Cadastro<small> Novo Fornecedor</small></h1>
-        </div>
-        
-        <div class="powerwidget yellow" id="most-form-elements" data-widget-editbutton="false">
-              <header>
-                <h2>Cadastro<small>Fornecedor</small></h2>
-              </header>
-              <div class="inner-spacer">
-                <form action="" method="POST" class="orb-form">
-                  <fieldset>
-                  
-                    <section class="col col-8">
-                      <label class="label">Nome Completo</label>
-                      <label class="input">
-                        <input type="text" name="nome" value="<?php echo @$campo['nome']; ?>" required>
-                      </label>
-                    </section>
-                    
-                    <section class="col col-4">
-                      <label class="label">CPF ou CNPJ</label>
-                      <label class="input">
-                        <input type="text" name="cpf" id="cpf" onblur="validarLogin('cpf', document.getElementById('cpf').value);" class="cpf" value="<?php echo @$campo['cpf']; ?>">
-                        <div id="campo_cpf"></div>
-                      </label>
-                    </section>
-                    
-                     <section class="col col-3">
-                      <label class="label">RG ou Inscrição Estadual</label>
-                      <label class="input">
-                        <input type="text" name="rg" value="<?php echo @$campo['rg']; ?>">
-                      </label>
-                    </section>
-                    
-                    <section class="col col-3">
-                      <label class="label">Razão Social (P.J)</label>
-                      <label class="input">
-                        <input type="text" name="razao" class="razao" value="<?php echo @$campo['razao']; ?>">
-                      </label>
-                    </section>
-                    
-                    <section class="col col-3">
-                      <label class="label">Telefone</label>
-                      <label class="input">
-                        <input type="text" name="tel" class="tel" value="<?php echo @$campo['tel']; ?>">
-                      </label>
-                    </section>
-                    
-                    <section class="col col-3">
-                      <label class="label">Celular</label>
-                      <label class="input">
-                        <input type="text" name="cel" class="cel" value="<?php echo @$campo['cel']; ?>">
-                      </label>
-                    </section>
+$idempresa = $_SESSION['empresa'];
+$id = isset($_GET['id']) ? base64_decode($_GET['id']) : null;
+$action = isset($_POST['action']) ? $_POST['action'] : null;
 
-                      <section class="col col-3">
-                          <label class="label">Email</label>
-                          <label class="input">
-                              <input type="text" name="email" class="email" value="<?php echo @$campo['email']; ?>">
-                          </label>
-                      </section>
+if ($action) {
+    $nome = clean_input($_POST['nome']);
+    $razao = clean_input($_POST['razao']);
+    $cpf_cnpj = clean_input($_POST['cpf_cnpj']);
+    $rg_ie = clean_input($_POST['rg_ie']);
+    $tel = clean_input($_POST['tel']);
+    $cel = clean_input($_POST['cel']);
+    $endereco = clean_input($_POST['endereco']);
+    $numero = clean_input($_POST['numero']);
+    $complemento = clean_input($_POST['complemento']);
+    $bairro = clean_input($_POST['bairro']);
+    $cidade = clean_input($_POST['cidade']);
+    $estado = clean_input($_POST['estado']);
+    $cep = clean_input($_POST['cep']);
+    $email = clean_input($_POST['email']);
+    $site = clean_input($_POST['site']);
+    $contato = clean_input($_POST['contato']);
+    $referencia = clean_input($_POST['referencia']);
+    $status = clean_input($_POST['status']);
+    $fornecedorid = isset($_POST['fornecedorid']) ? clean_input($_POST['fornecedorid']) : null;
 
-                      <section class="col col-3">
-                          <label class="label">Site</label>
-                          <label class="input">
-                              <input type="text" name="site" class="site" value="<?php echo @$campo['site']; ?>">
-                          </label>
-                      </section>
+    if ($action == 'insert') {
+        $sql = "INSERT INTO fornecedores (empresa, nome, razao, cpf_cnpj, rg_ie, tel, cel, endereco, numero, complemento, bairro, cidade, estado, cep, email, site, contato, referencia, status)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("isssssssssssssssss", $idempresa, $nome, $razao, $cpf_cnpj, $rg_ie, $tel, $cel, $endereco, $numero, $complemento, $bairro, $cidade, $estado, $cep, $email, $site, $contato, $referencia, $status);
+        $stmt->execute();
+        header("location: fornecedores.php?reg=1");
+    } else if ($action == 'update') {
+        $sql = "UPDATE fornecedores SET nome=?, razao=?, cpf_cnpj=?, rg_ie=?, tel=?, cel=?, endereco=?, numero=?, complemento=?, bairro=?, cidade=?, estado=?, cep=?, email=?, site=?, contato=?, referencia=?, status=? WHERE id=?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("issssssssssssssssi", $nome, $razao, $cpf_cnpj, $rg_ie, $tel, $cel, $endereco, $numero, $complemento, $bairro, $cidade, $estado, $cep, $email, $site, $contato, $referencia, $status, $fornecedorid);
+        $stmt->execute();
+        header("location: fornecedores.php?reg=2");
+    }
+}
 
-                      <section class="col col-3">
-                          <label class="label">Nome do Contato</label>
-                          <label class="input">
-                              <input type="text" name="contato" class="contato" value="<?php echo @$campo['contato']; ?>">
-                          </label>
-                      </section>
+if ($id) {
+    $sql = "SELECT * FROM fornecedores WHERE id = ? AND empresa = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("is", $id, $idempresa);
+    $stmt->execute();
+    $campo = $stmt->get_result()->fetch_assoc();
+}
+?>
 
-                      <section class="col col-3">
-                          <label class="label">Referência</label>
-                          <label class="input">
-                              <input type="text" name="referencia" class="referencia" value="<?php echo @$campo['referencia']; ?>">
-                          </label>
-                      </section>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Fornecedores</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/css/bootstrap.min.css" integrity="sha384-pzjw8f+ua7Kw1TIq0v8FqFjcJ6pajs/rfdfs3SO+kD4Ck5BdPtF+to8xMmcke49" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSGFpoO9xmv/+/z7nU7ELJ6EeAZWlCmGKZk4M1RtIDZOt6Xq/YD" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoJtKh7z7lGz7fuP4F8nfdFvAOA6Gg/z6Y5J6XqqyGXYM2ntX5" crossorigin="anonymous"></script>
 
-                    <section class="col col-10">
-                      <label class="label">Endereço</label>
-                      <label class="input">
-                        <input type="text" name="endereco" value="<?php echo @$campo['endereco']; ?>" required>
-                      </label>
-                    </section>
-                    
-                    <section class="col col-2">
-                      <label class="label">Nº</label>
-                      <label class="input">
-                        <input type="text" name="numero" onKeyUp="kbps(this);" value="<?php echo @$campo['numero']; ?>" required>
-                      </label>
-                    </section>
-                    
-                    <section class="col col-3">
-                      <label class="label">Bairro</label>
-                      <label class="input">
-                        <input type="text" name="bairro" value="<?php echo @$campo['bairro']; ?>" required>
-                      </label>
-                    </section>
-                    
-                   <section class="col col-3">
-                      <label class="label">Complemento</label>
-                      <label class="input">
-                        <input type="text" name="complemento" value="<?php echo @$campo['complemento']; ?>">
-                      </label>
-                    </section>
-                    
-                   <section class="col col-3">
-                      <label class="label">Estado</label>
-                      <label class="select">
-                      <select name="estado" id="estado" value="<?php echo @$campo['estado']; ?>"></select>
-                      </label>
-                    </section>
-                    
-                    <section class="col col-3">
-                      <label class="label">Cidade</label>
-                      <label class="select">
-                      <select name="cidade" id="cidade" value="<?php echo @$campo['cidade']; ?>" required></select>
-                      </label>
-                    </section>
-                    
-                    <section class="col col-3">
-                      <label class="label">CEP</label>
-                      <label class="input">
-                        <input type="text" name="cep" class="cep" value="<?php echo @$campo['cep']; ?>">
-                      </label>
-                    </section>
-                    
-              
-                    
-                    <section class="col col-3">
-                      <label class="label">Situação do Fornecedor</label>
-                      <div class="inline-group">
-                        <label class="radio">
-                          <input type="radio" name="status" value="S" <?php if ($campo['status'] == 'S') { echo "checked"; } ?> required>
-                          <i></i>Ativo</label>
-                        
-                        <label class="radio">
-                          <input type="radio" name="status" value="N" <?php if ($campo['status'] == 'N') { echo "checked"; } ?>>
-                          <i></i>Bloqueado</label>
-                      </div>
-                    </section>
-                    
-                  </fieldset>
-                  <footer>
-                  <?php if (@$campo['id'] <> '') { ?>
-
-		  <input type="submit" name="editar" class="btn btn-primary" value="Atualizar">
-		  <input type="hidden" name="fornecedorid" value="<?php echo @$campo['id']; ?>"> 
-		  <?php } else { ?>
-		  <input type="submit" name="cadastrar" class="btn btn-success" value="Cadastrar">
-		  <?php } ?>
-
-                  </footer>
-                </form>
-              </div>
-            </div>
-            
-            
-            <?php } else { ?>
-      	    
-      	    <div class="page-header">
-            <h1>Permissão <small>Negada!</small></h1>  
-            </div>
-        
-            <div class="row" id="powerwidgets">
-            <div class="col-md-12 bootstrap-grid">
-            
-            <div class="alert alert-danger alert-dismissable">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	    <i class="fa fa-times-circle"></i></button>
-            <strong>Atenção!</strong> Você não possui permissão para esse modulo. </div>
-            
-            </div></div>
-          <?php } ?>

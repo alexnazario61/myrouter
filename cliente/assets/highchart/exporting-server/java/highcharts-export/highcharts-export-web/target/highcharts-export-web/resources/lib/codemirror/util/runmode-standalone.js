57ba32d6d93e2b4699296d4d7ba5e1cf760df26c
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:40:56 -0300

    comment

--- cliente/assets/highchart/exporting-server/java/highcharts-export/highcharts-export-web/target/highcharts-export-web/resources/lib/codemirror/util/runmode-standalone.js
+++ cliente/assets/highchart/exporting-server/java/highcharts-export/highcharts-export-web/target/highcharts-export-web/resources/lib/codemirror/util/runmode-standalone.js
@@ -1,90 +1,148 @@
-/* Just enough of CodeMirror to run runMode under node.js */
+/*
+Just enough of CodeMirror to run runMode under node.js
+This module contains a subset of CodeMirror functionality, specifically for running
+runMode under node.js. It includes functions for splitting strings into lines,
+creating a StringStream object, and defining and getting modes for syntax highlighting.
+*/
 
-function splitLines(string){ return string.split(/\r?\n|\r/); };
+// Split a string into lines, handling both \n and \r\n line breaks
+function splitLines(string) {
+  return string.split(/\r?\n|\r/);
+}
 
+// StringStream class for reading a string character by character
 function StringStream(string) {
   this.pos = this.start = 0;
   this.string = string;
 }
+
 StringStream.prototype = {
-  eol: function() {return this.pos >= this.string.length;},
-  sol: function() {return this.pos == 0;},
-  peek: function() {return this.string.charAt(this.pos) || null;},
-  next: function() {
-    if (this.pos < this.string.length)
-      return this.string.charAt(this.pos++);
+  // Check if the end of the string has been reached
+  eol: function() {
+    return this.pos >= this.string.length;
   },
+
+  // Check if the current position is at the start of the string
+  sol: function() {
+    return this.pos == 0;
+  },
+
+  // Peek at the next character in the string without advancing the position
+  peek: function() {
+    return this.string.charAt(this.pos) || null;
+  },
+
+  // Advance the position and return the next character
+  next: function() {
+    if (this.pos < this.string.length) return this.string.charAt(this.pos++);
+  },
+
+  // Consume a character if it matches the given match string or regular expression
   eat: function(match) {
     var ch = this.string.charAt(this.pos);
     if (typeof match == "string") var ok = ch == match;
     else var ok = ch && (match.test ? match.test(ch) : match(ch));
-    if (ok) {++this.pos; return ch;}
+    if (ok) {
+      ++this.pos;
+      return ch;
+    }
   },
+
+  // Consume characters while they match the given match string or regular expression
   eatWhile: function(match) {
     var start = this.pos;
-    while (this.eat(match)){}
+    while (this.eat(match)) {}
     return this.pos > start;
   },
+
+  // Consume whitespace characters
   eatSpace: function() {
     var start = this.pos;
     while (/[\s\u00a0]/.test(this.string.charAt(this.pos))) ++this.pos;
     return this.pos > start;
   },
-  skipToEnd: function() {this.pos = this.string.length;},
+
+  // Skip to the end of the string
+  skipToEnd: function() {
+    this.pos = this.string.length;
+  },
+
+  // Skip to the next occurrence of the given character
   skipTo: function(ch) {
     var found = this.string.indexOf(ch, this.pos);
-    if (found > -1) {this.pos = found; return true;}
+    if (found > -1) {
+      this.pos = found;
+      return true;
+    }
   },
-  backUp: function(n) {this.pos -= n;},
-  column: function() {return this.start;},
-  indentation: function() {return 0;},
+
+  // Move the position back by the given number of characters
+  backUp: function(n) {
+    this.pos -= n;
+  },
+
+  // Return the current column (number of characters from the start of the line)
+  column: function() {
+    return this.start;
+  },
+
+  // Return the current indentation level (number of leading whitespace characters)
+  indentation: function() {
+    return 0;
+  },
+
+  // Match the given pattern, consuming it if consume is true
   match: function(pattern, consume, caseInsensitive) {
     if (typeof pattern == "string") {
-      function cased(str) {return caseInsensitive ? str.toLowerCase() : str;}
-      if (cased(this.string).indexOf(cased(pattern), this.pos) == this.pos) {
+      function cased(str) {
+        return caseInsensitive
+          ? str.toLowerCase()
+          : str;
+      }
+      if (
+        cased(this.string).indexOf(cased(pattern), this.pos) == this.pos
+      ) {
         if (consume !== false) this.pos += pattern.length;
         return true;
       }
-    }
-    else {
+    } else {
       var match = this.string.slice(this.pos).match(pattern);
       if (match && consume !== false) this.pos += match[0].length;
       return match;
     }
   },
-  current: function(){return this.string.slice(this.start, this.pos);}
+
+  // Return the current substring from the start position to the current position
+  current: function() {
+    return this.string.slice(this.start, this.pos);
+  },
 };
+
+// Export the StringStream object
 exports.StringStream = StringStream;
 
+// Default start state function
 exports.startState = function(mode, a1, a2) {
   return mode.startState ? mode.startState(a1, a2) : true;
 };
 
-var modes = exports.modes = {}, mimeModes = exports.mimeModes = {};
-exports.defineMode = function(name, mode) { modes[name] = mode; };
-exports.defineMIME = function(mime, spec) { mimeModes[mime] = spec; };
+// Mode and MIME mode objects
+var modes = exports.modes = {},
+  mimeModes = exports.mimeModes = {};
+
+// Define a new mode
+exports.defineMode = function(name, mode) {
+  modes[name] = mode;
+};
+
+// Define a new MIME mode
+exports.defineMIME = function(mime, spec) {
+  mimeModes[mime] = spec;
+};
+
+// Get a mode object based on the given options and mode specification
 exports.getMode = function(options, spec) {
   if (typeof spec == "string" && mimeModes.hasOwnProperty(spec))
     spec = mimeModes[spec];
   if (typeof spec == "string")
-    var mname = spec, config = {};
-  else if (spec != null)
-    var mname = spec.name, config = spec;
-  var mfactory = modes[mname];
-  if (!mfactory) throw new Error("Unknown mode: " + spec);
-  return mfactory(options, config || {});
-};
-
-exports.runMode = function(string, modespec, callback) {
-  var mode = exports.getMode({indentUnit: 2}, modespec);
-  var lines = splitLines(string), state = exports.startState(mode);
-  for (var i = 0, e = lines.length; i < e; ++i) {
-    if (i) callback("\n");
-    var stream = new exports.StringStream(lines[i]);
-    while (!stream.eol()) {
-      var style = mode.token(stream, state);
-      callback(stream.current(), style, i, stream.start);
-      stream.start = stream.pos;
-    }
-  }
-};
+   

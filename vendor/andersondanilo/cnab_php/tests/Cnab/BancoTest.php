f19792b13992a00aeac73cabda376a207291739d
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:50:16 -0300

    improve

--- vendor/andersondanilo/cnab_php/tests/Cnab/BancoTest.php
+++ vendor/andersondanilo/cnab_php/tests/Cnab/BancoTest.php
@@ -1,15 +1,35 @@
 <?php
+
 namespace Cnab\Tests;
 
 use Cnab\Banco;
 
-class BancoTest extends \PHPUnit_Framework_TestCase
+class BancoTest extends \PHPUnit\Framework\TestCase
 {
     public function testContemOsBancosEsperados()
     {
-        $this->assertTrue(Banco::existBanco(Banco::ITAU));
-        $this->assertTrue(Banco::existBanco(Banco::CEF));
-        $this->assertTrue(Banco::existBanco(Banco::SANTANDER));
-        $this->assertTrue(Banco::existBanco(Banco::BRADESCO));
+        $this->assertTrue(Banco::existsBanco(Banco::ITAU));
+        $this->assertTrue(Banco::existsBanco(Banco::CEF));
+        $this->assertTrue(Banco::existsBanco(Banco::SANTANDER));
+        $this->assertTrue(Banco::existsBanco(Banco::BRADESCO));
+    }
+
+    /**
+     * @dataProvider provideBancoConstants
+     */
+    public function testBancoConstantIsValid($const)
+    {
+        $this->assertTrue(defined('Cnab\\Banco:' . $const));
+        $this->assertTrue(constant('Cnab\\Banco:' . $const) !== false);
+    }
+
+    public function provideBancoConstants()
+    {
+        return [
+            [Banco::ITAU],
+            [Banco::CEF],
+            [Banco::SANTANDER],
+            [Banco::BRADESCO],
+        ];
     }
 }

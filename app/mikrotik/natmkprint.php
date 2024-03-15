55a8705d8cbdc7bdaa5646316fda7b76a1e6f7b7
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:36 -0300

    comment

--- app/mikrotik/natmkprint.php
+++ app/mikrotik/natmkprint.php
@@ -1,4 +1,3 @@
-
         <div class="breadcrumb clearfix">
           <ul>
             <li><a href="index.php?app=Dashboard">Dashboard</a></li>
@@ -7,99 +6,101 @@
           </ul>
         </div>
         
-        <?php if($permissao['fr3'] == S) { ?>
+        <?php 
+        // Check if the user has permission to access this page
+        if($permissao['fr3'] == 'S') { 
         
-        <?php if ($_GET['reg'] == '1') { ?>
-	<div class="alert alert-success alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	<i class="fa fa-times-circle"></i></button>
-        <strong>AtenÁ„o!</strong> Filtro Nat cadastrado com sucesso. </div>
-	<?php } ?>
-	<?php if ($_GET['reg'] == '3') { ?>
-	<div class="alert alert-danger alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	<i class="fa fa-times-circle"></i></button>
-        <strong>AtenÁ„o!</strong> Filtro Nat excluÌdo com sucesso. </div>
-	<?php } ?>
+            // Display success messages based on GET parameters
+            if ($_GET['reg'] == '1') { 
+                echo 
+                '<div class="alert alert-success alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                        <i class="fa fa-times-circle"></i></button>
+                    <strong>Aten√ß√£o!</strong> Filtro Nat cadastrado com sucesso. </div>';
+            }
+            if ($_GET['reg'] == '3') { 
+                echo 
+                '<div class="alert alert-danger alert-dismissable">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+                        <i class="fa fa-times-circle"></i></button>
+                    <strong>Aten√ß√£o!</strong> Filtro Nat exclu√≠do com sucesso. </div>';
+            }
         
-        <div class="page-header">
-          <h1>Firewall Nat / Redirecionamentos</h1>
-        </div>
+            // Page header for Firewall Nat / Redirecionamentos
+            echo 
+            '<div class="page-header">
+              <h1>Firewall Nat / Redirecionamentos</h1>
+            </div>';
         
-        <div class="row" id="powerwidgets">
-          <div class="col-md-12 bootstrap-grid"> 
-            <a href="index.php?app=NatMK" class="btn btn-info">NOVO FILTRO</a><br><br>
-            <div class="powerwidget" id="" data-widget-editbutton="false">
-              <header>
-                <h2>Gerenciar<small>Filtros de Nat</small></h2>
-              </header>
-              <div class="inner-spacer">
-                <table class="table table-striped table-hover" id="table-1">
-                  <thead>
-                    <tr>
-                      <th>Key</th>
-                      <th>Cliente</th>
-                      <th>Porta</th>
-                      <th>COMENT¡RIO</th>
-                      <th>AutenticaÁ„o</th>
-                      <th>AÁıes</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                  <?php
-              $servidor = $mysqli->query("SELECT * FROM servidores");
-    		  $mk = mysqli_fetch_array($servidor);
+            // Display a button for creating a new filter and the table for managing filters
+            echo 
+            '<div class="row" id="powerwidgets">
+              <div class="col-md-12 bootstrap-grid"> 
+                <a href="index.php?app=NatMK" class="btn btn-info">NOVO FILTRO</a><br><br>
+                <div class="powerwidget" id="" data-widget-editbutton="false">
+                  <header>
+                    <h2>Gerenciar<small>Filtros de Nat</small></h2>
+                  </header>
+                  <div class="inner-spacer">
+                    <table class="table table-striped table-hover" id="table-1">
+                      <thead>
+                        <tr>
+                          <th>Key</th>
+                          <th>Cliente</th>
+                          <th>Porta</th>
+                          <th>COMENT√ÅRIO</th>
+                          <th>Autentica√ß√£o</th>
+                          <th>A√ß√µes</th>
+                        </tr>
+                      </thead>
+                      <tbody>';
+                      
+            // Fetch data from the router and display it in the table
+            $servidor = $mysqli->query("SELECT * FROM servidores");
+            $mk = mysqli_fetch_array($servidor);
 
-		  $API = new routeros_api();
-		  $API->debug = false;
-		  if ($API->connect(''.$mk[ip].'', ''.$mk[login].'', ''.$mk[senha].'')) {
-		  $ARRAY = $API->comm("/ip/firewall/nat/print");
+            $API = new routeros_api();
+            $API->debug = false;
+            if ($API->connect(''.$mk[ip].'', ''.$mk[login].'', ''.$mk[senha].''')) {
+                $ARRAY = $API->comm("/ip/firewall/nat/print");
 
+                for ($i = 0; $i < count($ARRAY); ++$i) {
+                    $first = $ARRAY[$i];
 
-		  for ($i = 0; $i < count($ARRAY); ++$i)
-		  {
- 		  $first = $ARRAY[$i];
-		  ?>
+                    $ipfl = $first['to-addresses'];
+                    $consultas = $mysqli->query("SELECT * FROM firewall WHERE toaddresses = '$ipfl' AND group by cliente order by id DESC");
+                    $campo = mysqli_fetch_array($consultas);
 
-          <?php
-          $ipfl = $first['to-addresses'];
- 		  $consultas = $mysqli->query("SELECT * FROM firewall WHERE toaddresses = '$ipfl' AND group by cliente order by id DESC");
- 		  $campo = mysqli_fetch_array($consultas);
+                    $cliente = $campo['cliente'];
+                    $qtds = $mysqli->query("SELECT * FROM clientes WHERE id = '$cliente'");
+                    $cliente = mysqli_fetch_array($qtds)
 
-		  $cliente = $campo['cliente'];
-		  $qtds = $mysqli->query("SELECT * FROM clientes WHERE id = '$cliente'");
- 		  $cliente = mysqli_fetch_array($qtds)
-
-		  ?>
-		  <?php if($campo['cliente'] <> '') { ?>
-		  <tr>
-		  <td><?php echo $first['.id']; ?></td>
-		  <td><?php echo $cliente['nome']; ?></td>
-          <td><?php echo $campo['dstport']; ?></td>
-          <td><?php echo $campo['comentario']; ?></td>
-          <td><?php echo $campo['action']; ?></td>
-                  <td>
-		 
-	      	 <a href="javascript:void(0);" onclick="javascript: if (confirm('Deseja realmente excluir esse registro ?')) { window.location.href='?app=NatMK&id=<?php echo base64_encode($campo['id']); ?>&srv=<?php echo base64_encode($campo['servidor']); ?>&rmv=<?php echo base64_encode($first['.id']); ?>&Ex=Del' } else { void('') };" class="btn btn-danger tooltiped" data-toggle="tooltip" data-placement="top" title="Excluir"><i class="entypo-trash"></i></a>
-	      	 </td>
-		  </tr>
-    		  <?php } ?>
-    		  
-
-		  <?php  
-		    }
-		     $API->disconnect();
-		     } ?>
-
-                  </tbody>
+                    // Display data in the table rows
+                    echo 
+                    '<tr>
+                      <td>' . $first['.id'] . '</td>
+                      <td>' . $cliente['nome'] . '</td>
+                      <td>' . $campo['dstport'] . '</td>
+                      <td>' . $campo['comentario'] . '</td>
+                      <td>' . $campo['action'] . '</td>
+                      <td>
+                        <a href="javascript:void(0);" onclick="javascript: if (confirm('Deseja realmente excluir esse registro ?')) { window.location.href='?app=NatMK&id=' . base64_encode($campo['id']) . '&srv=' . base64_encode($campo['servidor']) . '&rmv=' . base64_encode($first['.id']) . '&Ex=Del' } else { void('') };" class="btn btn-danger tooltiped" data-toggle="tooltip" data-placement="top" title="Excluir"><i class="entypo-trash"></i></a>
+                      </td>
+                    </tr>';
+                }
+            }
+            $API->disconnect();
+            
+            echo 
+                  '</tbody>
                   <tfoot>
                     <tr>
-                    <th>ID</th>
+                      <th>ID</th>
                       <th>Cliente</th>
                       <th>Porta</th>
-                      <th>COMENT¡RIO</th>
-                      <th>AutenticaÁ„o</th>
-                      <th>AÁıes</th>
+                      <th>COMENT√ÅRIO</th>
+                      <th>Autentica√ß√£o</th>
+                      <th>A√ß√µes</th>
                     </tr>
                   </tfoot>
                 </table>
@@ -111,19 +112,6 @@
       </div>
       
       
-      <?php } else { ?>
-      	    
-      	    <div class="page-header">
-            <h1>Permiss„o <small>Negada!</small></h1>  
-            </div>
-        
-            <div class="row" id="powerwidgets">
-            <div class="col-md-12 bootstrap-grid">
-            
-            <div class="alert alert-danger alert-dismissable">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	    <i class="fa fa-times-circle"></i></button>
-            <strong>AtenÁ„o!</strong> VocÍ n„o possui permiss„o para esse modulo. </div>
-            
-            </div></div>
-          <?php } ?>
+      <?php 
+        } else { 
+        // Display an error message if

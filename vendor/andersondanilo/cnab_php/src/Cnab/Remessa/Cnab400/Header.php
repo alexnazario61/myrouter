1095bb3205893e511439c1cb07ab47231f55e29e
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:40:49 -0300

    comment

--- vendor/andersondanilo/cnab_php/src/Cnab/Remessa/Cnab400/Header.php
+++ vendor/andersondanilo/cnab_php/src/Cnab/Remessa/Cnab400/Header.php
@@ -1,12 +1,20 @@
 <?php
+// Namespace for CNAB 400 remessa header classes
 namespace Cnab\Remessa\Cnab400;
 
+// Header class representing the header of a CNAB 400 remessa file
 class Header extends \Cnab\Format\Linha
 {
-	public function __construct(\Cnab\Remessa\IArquivo $arquivo)
+    // Constructor for the Header class
+    //
+    // @param \Cnab\Remessa\IArquivo $arquivo The remessa file object
+    public function __construct(\Cnab\Remessa\IArquivo $arquivo)
     {
-    	$codigo_banco = $arquivo->codigo_banco;
+        // Get the codigo_banco value from the remessa file object
+        $codigo_banco = $arquivo->codigo_banco;
+
+        // Load the YAML file for the CNAB 400 remessa header based on the codigo_banco
         $yamlLoad = new \Cnab\Format\YamlLoad($codigo_banco);
         $yamlLoad->load($this, 'cnab400', 'remessa/header_arquivo');
-	}
+    }
 }

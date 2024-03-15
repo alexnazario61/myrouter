12a106acaf20fd25cf867aef5d58709b4cced770
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:48:52 -0300

    improve

--- app/autobloqueio.php
+++ app/autobloqueio.php
@@ -1,19 +1,20 @@
 <?php
+
 // Start a PHP session
 session_start();
 
 // Start output buffering
 ob_start();
 
-// Enable URL fopen and display errors
-ini_set("allow_url_fopen", 1);
-ini_set("display_errors", 1);
-
-// Set error reporting level
-error_reporting(1);
+// Enable error reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 // Enable tracking of errors
-ini_set("track_errors","1"); 
+ini_set("track_errors","1");
+
+// Enable URL fopen
+ini_set("allow_url_fopen", 1);
 
 // Require necessary files
 require_once '../config/conexao.class.php';
@@ -21,7 +22,7 @@ require_once '../config/crud.class.php';
 require_once '../config/mikrotik.class.php';
 
 // Decode the IP address from the GET request
-@$getIP = base64_decode($_GET['ip']);
+$getIP = isset($_GET['ip']) ? base64_decode($_GET['ip']) : '';
 
 // Instantiate the database connection class
 $con = new conexao();
@@ -30,41 +31,38 @@ $con = new conexao();
 $con->connect();
 
 // Query for all servers
-$servidor = mysql_query("SELECT * FROM servidores");
+$servidor = $con->query("SELECT * FROM servidores");
 
 // Fetch the first row of the result set
-$mk = mysql_fetch_array($servidor);
+$server = $servidor->fetch_array();
 
 // Set the IP and secret variables
-$ipnas = $mk['ip'];
-$secret = $mk['secret'];
+$ipnas = $server['ip'];
+$secret = $server['secret'];
 
 // Query for all records in the financeiro table with a status of 'B'
 $query = "SELECT * FROM  financeiro WHERE situacao = 'B'";
 
 // Execute the query
-$result = mysql_query($query) or die(mysql_error());
+$result = $con->query($query) or die($con->error);
 
 // Loop through the result set
-while($row = mysql_fetch_array($result)){
-     // Set the pedido and login variables to the current row's values
-     $row['pedido'];
-     $row['login'];
+while($row = $result->fetch_array()){
+    // Set the pedido and login variables to the current row's values
+    $pedido = $row['pedido'];
+    $login = $row['login'];
 
     // Call the radDesloga function with the current row's login value
-    radDesloga($row['login']);
-
-   // Uncomment the following line to disconnect the user using radclient
-   // shell_exec(" echo  User-Name=".$row['login']." | radclient -x $ipnas:3799 disconnect $secret");
+    radDesloga($con, $ipnas, $secret, $login);
 }
 
 // Define the radDesloga function
-function radDesloga($username) {
+function radDesloga($con, $ipnas, $secret, $username) {
     // Query for the framedipaddress and nasipaddress for the given username
     $consulta_rsDesl = "SELECT framedipaddress, nasipaddress FROM radacct WHERE username = '" . $username . "' ORDER BY radacctid DESC";
-    $rsDesl = mysql_query($consulta_rsDesl);
-    $row_rsDesl = mysql_fetch_assoc($rsDesl);
-    $totalRows_rsDesl = mysql_num_rows($rsDesl);
+    $rsDesl = $con->query($consulta_rsDesl);
+    $row_rsDesl = $rsDesl->fetch_assoc();
+    $totalRows_rsDesl = $rsDesl->num_rows;
 
     // Set the ipcli and nascli variables to the fetched values
     $ipcli = $row_rsDesl['framedipaddress'];
@@ -74,4 +72,14 @@ function radDesloga($username) {
     if ($totalRows_rsDesl) {
         // Query for the secret for the given nasipaddress
         $consulta_rsRamais = "SELECT secret FROM nas WHERE nasname = '" . $nascli . "'";
-        $rsRamais = mysql_query($consult
+        $rsRamais = $con->query($consulta_rsRamais);
+        $row_rsRamais = $rsRamais->fetch_assoc();
+        $secret_nas = $row_rsRamais['secret'];
+
+        // Build the disconnect command
+        $command = "echo User-Name=" . $username . " | radclient -x " . $ipnas . ":3799 disconnect " . $secret_nas;
+
+        // Execute the command
+        shell_exec($command);
+    }
+}

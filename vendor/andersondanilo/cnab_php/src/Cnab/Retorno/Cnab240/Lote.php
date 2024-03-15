588e0d024145a39a5308ab3dc879f84e7a26c306
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:50:10 -0300

    improve

--- vendor/andersondanilo/cnab_php/src/Cnab/Retorno/Cnab240/Lote.php
+++ vendor/andersondanilo/cnab_php/src/Cnab/Retorno/Cnab240/Lote.php
@@ -1,54 +1,39 @@
 <?php
+
 namespace Cnab\Retorno\Cnab240;
 
+/**
+ * Class Lote
+ * @package Cnab\Retorno\Cnab240
+ */
 class Lote
 {
+    /**
+     * @var SegmentoHeader
+     */
     public $header;
+
+    /**
+     * @var SegmentoTrailer
+     */
     public $trailer;
 
-    public $arquivo;
-    public $codigo_banco;
+    /**
+     * @var IArquivo
+     */
+    private $arquivo;
 
-    public $detalhes = array();
+    /**
+     * @var int
+     */
+    public $codigoBanco;
 
-    private $lastDetalhe;
+    /**
+     * @var Detalhe[]
+     */
+    public $detalhes = [];
 
-    public function __construct(\Cnab\Retorno\IArquivo $arquivo)
-    {
-        $this->arquivo = $arquivo;
-        $this->codigo_banco = $this->arquivo->codigo_banco;
-    }
+    /**
+     * @var Detalhe|null
+     */
 
-    public function insertSegmento($linha)
-    {
-        $codigo_segmento = strtoupper(substr($linha, 13, 1));
-        $segmento = null;
-        if('T' == $codigo_segmento)
-        {
-            $segmento = new SegmentoT($this->arquivo);
-            $segmento->loadFromString($linha);
-            $this->lastDetalhe = new Detalhe($this->arquivo);
-            $this->detalhes[] = $this->lastDetalhe;
-            $this->lastDetalhe->segmento_t = $segmento;
-        }
-        else if('U' == $codigo_segmento)
-        {
-            $segmento = new SegmentoU($this->arquivo);
-            $segmento->loadFromString($linha);
-            if($this->lastDetalhe)
-                $this->lastDetalhe->segmento_u = $segmento;
-        }
-        else if('W' == $codigo_segmento)
-        {
-            $segmento = new SegmentoW($this->arquivo);
-            $segmento->loadFromString($linha);
-            if($this->lastDetalhe)
-                $this->lastDetalhe->segmento_w = $segmento;
-        }
-    }
-
-    public function listDetalhes()
-    {
-        return $this->detalhes;
-    }
-}

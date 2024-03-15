2b6a858aeeb7ed211793ab6ded1824c5483feb23
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:20 -0300

    comment

--- app/juniper/comandos_ubnt.php
+++ app/juniper/comandos_ubnt.php
@@ -1,4 +1,26 @@
 <?php
 
+// This is a simple PHP script with no code inside the body.
+// It is currently not doing anything and can be used as a starting point
+// for a new PHP script.
+
+// To add functionality to this script, you can start by defining variables,
+// functions, or classes within the body of the script.
+
+// For example, you might define a variable like this:
+$myVariable = "Hello, world!";
+
+// Or, you might define a function like this:
+function myFunction() {
+  // Do something here
+}
+
+// Or, you might define a class like this:
+class MyClass {
+  // Define properties and methods here
+}
+
+// Once you've added some functionality to the script, be sure to update
+// the comments to reflect the new behavior.
 
 ?>

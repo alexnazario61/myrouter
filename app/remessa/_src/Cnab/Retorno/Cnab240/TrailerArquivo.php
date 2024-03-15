aafc551095c03acac50bb505bb16fc296df5674d
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:22 -0300

    comment

--- app/remessa/_src/Cnab/Retorno/Cnab240/TrailerArquivo.php
+++ app/remessa/_src/Cnab/Retorno/Cnab240/TrailerArquivo.php
@@ -1,11 +1,17 @@
 <?php
+// Define a namespace for the TrailerArquivo class
 namespace Cnab\Retorno\Cnab240;
 
+// Define the TrailerArquivo class that extends the \Cnab\Format\Linha class
 class TrailerArquivo extends \Cnab\Format\Linha
 {
-	public function __construct(\Cnab\Retorno\IArquivo $arquivo)
+    // Define the constructor method for the TrailerArquivo class
+    public function __construct(\Cnab\Retorno\IArquivo $arquivo)
     {
+        // Create a new instance of the YamlLoad class, passing the bank code and layout version
         $yamlLoad = new \Cnab\Format\YamlLoad($arquivo->codigo_banco, $arquivo->layoutVersao);
+
+        // Load the trailer_arquivo format from the cnab240 file
         $yamlLoad->load($this, 'cnab240', 'trailer_arquivo');
-	}
+    }
 }

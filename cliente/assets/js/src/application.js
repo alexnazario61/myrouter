352af1b73ad78854f31896ef9b4b8b84687f5227
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:41:00 -0300

    comment

--- cliente/assets/js/src/application.js
+++ cliente/assets/js/src/application.js
@@ -1,176 +1,164 @@
-// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
-// IT'S ALL JUST JUNK FOR OUR DOCS!
-// ++++++++++++++++++++++++++++++++++++++++++
+// This is the main entry point for the JavaScript code in Bootstrap's docs.
+// It initializes various components and plugins used in the documentation.
+// The code is wrapped in an immediately-invoked function expression (IIFE)
+// to avoid polluting the global namespace.
 
-/*!
- * JavaScript for Bootstrap's docs (http://getbootstrap.com)
- * Copyright 2011-2014 Twitter, Inc.
- * Licensed under the Creative Commons Attribution 3.0 Unported License. For
- * details, see http://creativecommons.org/licenses/by/3.0/.
- */
-
-/* global ZeroClipboard */
+/* global ZeroClipboard */ // This line is used to declare ZeroClipboard as a global variable.
 
 !function ($) {
   'use strict';
 
+  // The code inside this function will be executed after the DOM is fully loaded.
   $(function () {
 
-    // Scrollspy
-    var $window = $(window)
-    var $body   = $(document.body)
+    // Initialize Scrollspy
+    var $window = $(window);
+    var $body   = $(document.body);
 
     $body.scrollspy({
       target: '.bs-docs-sidebar'
-    })
+    });
+
     $window.on('load', function () {
-      $body.scrollspy('refresh')
-    })
+      $body.scrollspy('refresh');
+    });
 
-    // Kill links
+    // Prevent links with href="#" from causing a full page reload
     $('.bs-docs-container [href=#]').click(function (e) {
-      e.preventDefault()
-    })
+      e.preventDefault();
+    });
 
-    // Sidenav affixing
+    // Initialize Sidenav affixing
     setTimeout(function () {
-      var $sideBar = $('.bs-docs-sidebar')
+      var $sideBar = $('.bs-docs-sidebar');
 
       $sideBar.affix({
         offset: {
           top: function () {
-            var offsetTop      = $sideBar.offset().top
-            var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10)
-            var navOuterHeight = $('.bs-docs-nav').height()
+            var offsetTop      = $sideBar.offset().top;
+            var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 10);
+            var navOuterHeight = $('.bs-docs-nav').height();
 
-            return (this.top = offsetTop - navOuterHeight - sideBarMargin)
+            // Calculate the top offset based on the sidebar's position and the height of the navigation bar.
+            return (this.top = offsetTop - navOuterHeight - sideBarMargin);
           },
           bottom: function () {
-            return (this.bottom = $('.bs-docs-footer').outerHeight(true))
+            // Set the bottom offset to the height of the footer.
+            return (this.bottom = $('.bs-docs-footer').outerHeight(true));
           }
         }
-      })
-    }, 100)
+      });
+    }, 100);
 
     setTimeout(function () {
-      $('.bs-top').affix()
-    }, 100)
+      $('.bs-top').affix();
+    }, 100);
 
-    // theme toggler
+    // Initialize theme toggler
     ;(function () {
-      var stylesheetLink = $('#bs-theme-stylesheet')
-      var themeBtn = $('.bs-docs-theme-toggle')
+      var stylesheetLink = $('#bs-theme-stylesheet');
+      var themeBtn = $('.bs-docs-theme-toggle');
 
       var activateTheme = function () {
-        stylesheetLink.attr('href', stylesheetLink.attr('data-href'))
-        themeBtn.text('Disable theme preview')
-        localStorage.setItem('previewTheme', true)
-      }
+        // Set the href attribute of the stylesheet link to the data-href attribute value.
+        stylesheetLink.attr('href', stylesheetLink.attr('data-href'));
+        themeBtn.text('Disable theme preview');
+        localStorage.setItem('previewTheme', true);
+      };
 
       if (localStorage.getItem('previewTheme')) {
-        activateTheme()
+        activateTheme();
       }
 
       themeBtn.click(function () {
-        var href = stylesheetLink.attr('href')
+        var href = stylesheetLink.attr('href');
         if (!href || href.indexOf('data') === 0) {
-          activateTheme()
+          activateTheme();
         } else {
-          stylesheetLink.attr('href', '')
-          themeBtn.text('Preview theme')
-          localStorage.removeItem('previewTheme')
+          // Clear the href attribute of the stylesheet link.
+          stylesheetLink.attr('href', '');
+          themeBtn.text('Preview theme');
+          localStorage.removeItem('previewTheme');
         }
-      })
+      });
     })();
 
-    // Tooltip and popover demos
+    // Initialize Tooltip and Popover demos
     $('.tooltip-demo').tooltip({
       selector: '[data-toggle="tooltip"]',
       container: 'body'
-    })
+    });
+
     $('.popover-demo').popover({
       selector: '[data-toggle="popover"]',
       container: 'body'
-    })
+    });
 
-    // Demos within modals
-    $('.tooltip-test').tooltip()
-    $('.popover-test').popover()
+    // Initialize Popover demos within modals
+    $('.tooltip-test').tooltip();
+    $('.popover-test').popover();
 
-    // Popover demos
-    $('.bs-docs-popover').popover()
+    // Initialize Popover demos
+    $('.bs-docs-popover').popover();
 
-    // Button state demo
+    // Initialize Button state demo
     $('#loading-example-btn').on('click', function () {
-      var btn = $(this)
-      btn.button('loading')
+      var btn = $(this);
+      btn.button('loading');
       setTimeout(function () {
-        btn.button('reset')
-      }, 3000)
-    })
+        btn.button('reset');
+      }, 3000);
+    });
 
-    // Modal relatedTarget demo
+    // Initialize Modal relatedTarget demo
     $('#exampleModal').on('show.bs.modal', function (event) {
-      var button = $(event.relatedTarget) // Button that triggered the modal
-      var recipient = button.data('whatever') // Extract info from data-* attributes
-      // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-      // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-      var modal = $(this)
-      modal.find('.modal-title').text('New message to ' + recipient)
-      modal.find('.modal-body input').val(recipient)
-    })
+      var button = $(event.relatedTarget); // Button that triggered the modal
+      var recipient = button.data('whatever'); // Extract info from data-* attributes
 
-    // Activate animated progress bar
+      // Update the modal's content
+      var modal = $(this);
+      modal.find('.modal-title').text('New message to ' + recipient);
+      modal.find('.modal-body input').val(recipient);
+    });
+
+    // Initialize animated progress bar
     $('.bs-docs-activate-animated-progressbar').on('click', function () {
-      $(this).siblings('.progress').find('.progress-bar-striped').toggleClass('active')
-    })
+      $(this).siblings('.progress').find('.progress-bar-striped').toggleClass('active');
+    });
 
-    // Config ZeroClipboard
+    // Initialize ZeroClipboard
+    // Configure ZeroClipboard with the path to the SWF file and the hover class
     ZeroClipboard.config({
       moviePath: '/assets/flash/ZeroClipboard.swf',
       hoverClass: 'btn-clipboard-hover'
-    })
+    });
 
     // Insert copy to clipboard button before .highlight
     $('.highlight').each(function () {
-      var btnHtml = '<div class="zero-clipboard"><span class="btn-clipboard">Copy</span></div>'
-      $(this).before(btnHtml)
-    })
-    var zeroClipboard = new ZeroClipboard($('.btn-clipboard'))
-    var htmlBridge = $('#global-zeroclipboard-html-bridge')
+      var btnHtml = '<div class="zero-clipboard"><span class="btn-clipboard">Copy</span></div>';
+      $(this).before(btnHtml);
+    });
 
-    // Handlers for ZeroClipboard
+    // Initialize ZeroClipboard instances
+    var zeroClipboard = new ZeroClipboard($('.btn-clipboard'));
+
+    // Initialize HTML bridge for ZeroClipboard
+    var htmlBridge = $('#global-zeroclipboard-html-bridge');
+
+    // Set up event handlers for ZeroClipboard
     zeroClipboard.on('load', function () {
+      // Initialize tooltip for the HTML bridge
       htmlBridge
         .data('placement', 'top')
         .attr('title', 'Copy to clipboard')
-        .tooltip()
-    })
+        .tooltip();
+    });
 
-    // Copy to clipboard
+    // Handle copy to clipboard request
     zeroClipboard.on('dataRequested', function (client) {
-      var highlight = $(this).parent().nextAll('.highlight').first()
-      client.setText(highlight.text())
-    })
+      var highlight = $(this).parent().nextAll('.highlight').first();
+      client.setText(highlight.text());
+    });
 
-    // Notify copy success and reset tooltip title
-    zeroClipboard.on('complete', function () {
-      htmlBridge
-        .attr('title', 'Copied!')
-        .tooltip('fixTitle')
-        .tooltip('show')
-        .attr('title', 'Copy to clipboard')
-        .tooltip('fixTitle')
-    })
-
-    // Notify copy failure
-    zeroClipboard.on('noflash wrongflash', function () {
-      htmlBridge
-        .attr('title', 'Flash required')
-        .tooltip('fixTitle')
-        .tooltip('show')
-    })
-
-  })
-
-}(jQuery)
+    // Handle copy success and update tooltip
+    zeroClip

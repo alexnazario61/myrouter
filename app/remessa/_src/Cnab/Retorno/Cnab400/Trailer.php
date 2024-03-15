f43677167e1d934078211b45e1d46b44dbc5208f
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:19 -0300

    comment

--- app/remessa/_src/Cnab/Retorno/Cnab400/Trailer.php
+++ app/remessa/_src/Cnab/Retorno/Cnab400/Trailer.php
@@ -3,9 +3,18 @@ namespace Cnab\Retorno\Cnab400;
 
 class Trailer extends \Cnab\Format\Linha
 {
-	public function __construct(\Cnab\Retorno\IArquivo $arquivo)
-	{
-		$yamlLoad = new \Cnab\Format\YamlLoad($arquivo->codigo_banco, $arquivo->layoutVersao);
+    /**
+     * Constructor for the Trailer class.
+     *
+     * @param \Cnab\Retorno\IArquivo $arquivo The input arquivo object.
+     */
+    public function __construct(\Cnab\Retorno\IArquivo $arquivo)
+    {
+        // Load the YAML configuration for the specific bank and layout version
+        $yamlLoad = new \Cnab\Format\YamlLoad($arquivo->codigo_banco, $arquivo->layoutVersao);
+
+        // Load the configuration for the CNAB400 return trailer file
         $yamlLoad->load($this, 'cnab400', 'retorno/trailer_arquivo');
-	}	
+    }	
 }
+

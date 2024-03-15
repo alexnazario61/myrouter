b5731732b29f45a41dd6c277724e0ee6ca916f3e
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:50:03 -0300

    improve

--- mobile/sair.php
+++ mobile/sair.php
@@ -1,6 +1,18 @@
 <?php
-  session_start();
-  session_unset();
-  session_destroy();
-  header("Location: login.php");
+
+// Check if the user is already logged in, if so redirect to the home page
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+    header("location: home.php");
+    exit;
+}
+
+// Unset and destroy the session
+session_start();
+session_unset();
+session_destroy();
+
+// Redirect to the login page
+header("Location: login.php");
+exit;
+
 ?>

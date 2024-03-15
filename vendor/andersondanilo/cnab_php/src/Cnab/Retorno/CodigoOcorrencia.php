f4f8cc8dbe3cd48bdd0699a4c77002620e07e5e7
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:40:54 -0300

    comment

--- vendor/andersondanilo/cnab_php/src/Cnab/Retorno/CodigoOcorrencia.php
+++ vendor/andersondanilo/cnab_php/src/Cnab/Retorno/CodigoOcorrencia.php
@@ -1,23 +1,38 @@
 <?php
 namespace Cnab\Retorno;
 
-
 class CodigoOcorrencia
 {
     /**
-     * Por enquanto só foi implementado no Cnab400
+     * This method returns the name of the ocorrence code for a given bank code and ocorrence code.
+     * It currently only supports the CNAB400 format.
+     *
+     * @param int $codigo_banco The bank code
+     * @param int $codigo_ocorrencia The ocorrence code
+     * @param string $format The format of the ocorrence code, either 'cnab400' (default)
+     * @return string The name of the ocorrence code
+     *
+     * @throws \Exception If the format is not supported
      */
     public function getNome($codigo_banco, $codigo_ocorrencia, $format='cnab400')
     {
+        // Convert the format, bank code, and ocorrence code to lowercase and integers
         $format             = strtolower($format);
         $codigo_banco       = (int)$codigo_banco;
         $codigo_ocorrencia  = (int)$codigo_ocorrencia;
+
+        // Load the format from a YAML file
         $yamlLoad           = new \Cnab\Format\YamlLoad($codigo_banco);
         $array              = $yamlLoad->loadFormat($format, 'retorno/codigo_ocorrencia');
 
+        // Check if the bank code and ocorrence code exist in the array
         if(array_key_exists($codigo_banco, $array) && array_key_exists($codigo_ocorrencia, $array[$codigo_banco]))
         {
+            // Return the name of the ocorrence code
             return $array[$codigo_banco][$codigo_ocorrencia];
         }
+
+        // If the bank code or ocorrence code do not exist in the array, throw an exception
+        throw new \Exception("The format '{$format}' is not supported or the bank code or ocorrence code is invalid.");
     }
 }

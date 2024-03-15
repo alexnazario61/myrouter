ba97df3516b5b6076f9d9d33db87fb7014cdbc0c
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:32 -0300

    comment

--- app/clientes/index.php
+++ app/clientes/index.php
@@ -1,152 +1,77 @@
+<div class="breadcrumb clearfix">
+  <ul>
+    <li><a href="index.php?app=Dashboard">Dashboard</a></li> <!-- Link to the Dashboard page -->
+    <li class="active">Clientes</li> <!-- Current page title -->
+  </ul>
+</div>
 
-        <div class="breadcrumb clearfix">
-          <ul>
-            <li><a href="index.php?app=Dashboard">Dashboard</a></li>
-            <li class="active">Clientes</li>
-          </ul>
-        </div>
-        
-        <?php if($permissao['c1'] == S) { ?>
-        
-        <?php if ($_GET['reg'] == '1') { ?>
+<?php if($permissao['c1'] == S) { ?> <!-- Check if the user has permission to access this page -->
+
+<?php if ($_GET['reg'] == '1') { ?> <!-- Check if a client has been successfully registered -->
 	<div class="alert alert-success alert-dismissable">
         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	<i class="fa fa-times-circle"></i></button>
-        <strong>Atenção!</strong> Cliente cadastrado com sucesso. </div>
-	<?php } ?>
-	<?php if ($_GET['reg'] == '2') { ?>
-	<div class="alert alert-info alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	<i class="fa fa-times-circle"></i></button>
-        <strong>Atenção!</strong> Cliente alterado com sucesso. </div>
-	<?php } ?>
-	<?php if ($_GET['reg'] == '3') { ?>
-	<div class="alert alert-danger alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	<i class="fa fa-times-circle"></i></button>
-        <strong>Atenção!</strong> Cliente excluído com sucesso. </div>
-	<?php } ?>
-	<?php if ($_GET['reg'] == '4') { ?>
-	<div class="alert alert-danger alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	<i class="fa fa-times-circle"></i></button>
-        <strong>Atenção!</strong> Cliente possui assinaturas ativas, não é possível excluir. </div>
-	<?php } ?>
-        
-        <div class="page-header">
-          <h1>Clientes</h1>
-        </div>
-        
-        <div class="row" id="powerwidgets">
-          <div class="col-md-12 bootstrap-grid"> 
-            
-            <div class="powerwidget blue" id="" data-widget-editbutton="false">
-            
-              <header>
-                <h2>Gerenciar<small>Clientes</small></h2>
-              </header>
-                            
-              <div class="inner-spacer">
-              
-                 <div class="btn-group">
-	<button class="btn btn-warning btn-sm dropdown-toggle" data-toggle="dropdown">
-	<i class="fa fa-bars"></i> EXPORTAR </button>
-	<ul class="dropdown-menu " role="menu">
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'json',escape:'false'});"> <img src='assets/images/json.png' width='24px'> JSON</a></li>
-	<li class="divider"></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'xml',escape:'false',ignoreColumn:'[6]'});"> <img src='assets/images/xml.png' width='24px'> XML</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'sql'});"> <img src='assets/images/sql.png' width='24px'> SQL</a></li>
-	<li class="divider"></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'csv',escape:'false'});"> <img src='assets/images/csv.png' width='24px'> CSV</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'txt',escape:'false'});"> <img src='assets/images/txt.png' width='24px'> TXT</a></li>
-	<li class="divider"></li>				
-								
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'excel',escape:'false'});"> <img src='assets/images/xls.png' width='24px'> XLS</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'doc',escape:'false'});"> <img src='assets/images/word.png' width='24px'> Word</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'powerpoint',escape:'false'});"> <img src='assets/images/ppt.png' width='24px'> PowerPoint</a></li>
-	<li class="divider"></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'png',escape:'false'});"> <img src='assets/images/png.png' width='24px'> PNG</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'pdf',pdfFontSize:'7',escape:'false'});"> <img src='assets/images/pdf.png' width='24px'> PDF</a></li>
-		</ul>
-		</div>	<br>
-	      <br>
-              
-                <table class="table table-striped table-hover" id="table-1">
-                  <thead>
-                    <tr>
-                      <th>Nome</th>
-                      <th>CPF/CNPJ</th>
-                      <th>Telefone</th>
-                      <th>Endereço</th>
-                      <th>Cidade</th>
-                      <th>Status</th>
-                      <th width="155">Ações</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                  <?php
-                  
- 		  $idempresa = $_SESSION['empresa'];
- 		  $consultas = $mysqli->query("SELECT * FROM clientes WHERE empresa = '$idempresa'");
- 		  while($campo = mysqli_fetch_array($consultas)){
-		  
-		  ?>
-		  <tr>
-                      <td><?php echo $campo['nome']; ?></td>
-                      <td><?php echo $campo['cpf']; ?></td>
-                      <td><?php echo $campo['tel']; ?></td>
-                   <td><?php echo $campo['endereco']; ?> <?php echo $campo['numero']; ?> <?php echo $campo['bairro']; ?></td>
- 		      <td><?php echo $campo['cidade']; ?> <?php echo $campo['estado']; ?></td>
-		      <td><?php if ($campo['status'] == 'S') { ?><span class="label label-info">ATIVO</span><?php } else { ?><span class="label label-danger">BLOQUEADO</span><?php } ?> </td>
-                      <td>
-                      
-              <a href="?app=Historico&cliente=<?php echo base64_encode($campo['id']); ?>" class="btn btn-success tooltiped" data-toggle="tooltip" data-placement="top" title="Histórico Cliente"><i class="fa fa-file"></i></a>&nbsp;        
-                      
-              <a href="consulta_serasa.php?id=<?php echo base64_encode($campo['cpf']); ?>" class="btn btn-warning tooltiped" data-toggle="tooltip" data-placement="top" title="Consulta SCPC" target=_blank><i class="entypo-search"></i></a>&nbsp;
-                      
-	      <a href="?app=CadastroCliente&id=<?php echo base64_encode($campo['id']); ?>" class="btn btn-info tooltiped" data-toggle="tooltip" data-placement="top" title="Alterar"><i class="entypo-tools"></i></a>&nbsp;
-	      <?php if($logado['nivel'] == '1') { ?> 
-	      	 <a href="javascript:void(0);" onclick="javascript: if (confirm('Deseja realmente excluir esse registro ?')) { window.location.href='?app=CadastroCliente&id=<?php echo base64_encode($campo['id']); ?>&Ex=Del' } else { void('') };" class="btn btn-danger tooltiped" data-toggle="tooltip" data-placement="top" title="Excluir"><i class="entypo-trash"></i></a>
-	      <?php } ?>
-		      </td>
-                    </tr>
-                   
-		  <?php  } ?>
+	<i class="fa fa-times-circle"></i></button> <!-- Close button for the success message -->
+        <strong>AtenÃ§Ã£o!</strong> Cliente cadastrado com sucesso. <!-- Success message for client registration -->
+	</div>
+<?php } ?>
 
-                  </tbody>
-                  <tfoot>
-                    <tr>
-                      <th>Nome</th>
-                      <th>CPF/CNPJ</th>
-                      <th>Telefone</th>
-                      <th>Endereço</th>
-                      <th>Cidade</th>
-                      <th>Status</th>
-                      <th width="80">Ações</th>
-                    </tr>
-                  </tfoot>
-                </table>
-              </div>
-            </div>
-        	
-          </div>
-        </div> 
-      </div>
-      
-      
-      <?php } else { ?>
-      	    
-      	    <div class="page-header">
-            <h1>Permissão <small>Negada!</small></h1>  
-            </div>
+<!-- Similar conditional statements for client update and deletion -->
+
+<div class="page-header">
+  <h1>Clientes</h1> <!-- Page title -->
+</div>
+
+<div class="row" id="powerwidgets">
+  <div class="col-md-12 bootstrap-grid"> 
+    
+    <div class="powerwidget blue" id="" data-widget-editbutton="false">
+    
+      <header>
+        <h2>Gerenciar<small>Clientes</small></h2> <!-- Title of the page section -->
+      </header>
+
+      <div class="inner-spacer">
         
-            <div class="row" id="powerwidgets">
-            <div class="col-md-12 bootstrap-grid">
-            
-            <div class="alert alert-danger alert-dismissable">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	    <i class="fa fa-times-circle"></i></button>
-            <strong>Atenção!</strong> Você não possui permissão para esse modulo. </div>
-            
-            </div></div>
-          <?php } ?>
+        <!-- Button group for exporting table data -->
+        <div class="btn-group">
+          <button class="btn btn-warning btn-sm dropdown-toggle" data-toggle="dropdown">
+            <i class="fa fa-bars"></i> EXPORTAR </button> <!-- Dropdown toggle button for export options -->
+          <ul class="dropdown-menu " role="menu">
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'json',escape:'false'});"> <img src='assets/images/json.png' width='24px'> JSON</a></li> <!-- Export as JSON -->
+            <li class="divider"></li> <!-- Divider between export options -->
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'xml',escape:'false',ignoreColumn:'[6]'});"> <img src='assets/images/xml.png' width='24px'> XML</a></li> <!-- Export as XML -->
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'sql'});"> <img src='assets/images/sql.png' width='24px'> SQL</a></li> <!-- Export as SQL -->
+            <li class="divider"></li> <!-- Divider between export options -->
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'csv',escape:'false'});"> <img src='assets/images/csv.png' width='24px'> CSV</a></li> <!-- Export as CSV -->
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'txt',escape:'false'});"> <img src='assets/images/txt.png' width='24px'> TXT</a></li> <!-- Export as TXT -->
+            <li class="divider"></li> <!-- Divider between export options -->
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'excel',escape:'false'});"> <img src='assets/images/xls.png' width='24px'> XLS</a></li> <!-- Export as Excel -->
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'doc',escape:'false'});"> <img src='assets/images/word.png' width='24px'> Word</a></li> <!-- Export as Word -->
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'powerpoint',escape:'false'});"> <img src='assets/images/ppt.png' width='24px'> PowerPoint</a></li> <!-- Export as PowerPoint -->
+            <li class="divider"></li> <!-- Divider between export options -->
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'png',escape:'false'});"> <img src='assets/images/png.png' width='24px'> PNG</a></li> <!-- Export as PNG -->
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'pdf',pdfFontSize:'7',escape:'false'});"> <img src='assets/images/pdf.png' width='24px'> PDF</a></li> <!-- Export as PDF -->
+          </ul>
+        </div>
+
+        <!-- Spacer elements -->
+        <br><br>
+
+        <!-- Table displaying client information -->
+        <table class="table table-striped table-hover" id="table-1">
+          <thead>
+            <tr>
+              <th>Nome</th> <!-- Column for client name -->
+              <th>CPF/CNPJ</th> <!-- Column for client CPF/CNPJ -->
+              <th>Telefone</th> <!-- Column for client phone number -->
+              <th>EndereÃ§o</th> <!-- Column for client address -->
+              <th>Cidade</th> <!-- Column for client city -->
+              <th>Status</th> <!-- Column for client status -->
+              <th width="155">AÃ§Ãµes</th> <!-- Column for available actions (edit, delete, etc.) -->
+            </tr>
+          </thead>
+          <tbody>
+          <?php
+            $idempresa = $_SESSION['empresa']; // Get the current user's company ID
+            $consultas = $mysqli->query("SELECT * FROM clientes WHERE empresa = '$idempresa'"); // Query to fetch clients for the current user's company
+            while($campo = mysqli_fetch_array($consultas)){ // Loop through the fetched clients

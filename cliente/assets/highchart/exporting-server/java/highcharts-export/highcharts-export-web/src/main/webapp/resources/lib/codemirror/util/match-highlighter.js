b67ad23ec660cb254097ddd99af0f6d2050e69e0
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:49:54 -0300

    improve

--- cliente/assets/highchart/exporting-server/java/highcharts-export/highcharts-export-web/src/main/webapp/resources/lib/codemirror/util/match-highlighter.js
+++ cliente/assets/highchart/exporting-server/java/highcharts-export/highcharts-export-web/src/main/webapp/resources/lib/codemirror/util/match-highlighter.js
@@ -1,44 +1,63 @@
-// Define match-highlighter commands. Depends on searchcursor.js
-// Use by attaching the following function call to the onCursorActivity event:
-	//myCodeMirror.matchHighlight(minChars);
-// And including a special span.CodeMirror-matchhighlight css class (also optionally a separate one for .CodeMirror-focused -- see demo matchhighlighter.html)
-
+/**
+ * Define match-highlighter commands. Depends on searchcursor.js
+ * Use by attaching the following function call to the onCursorActivity event:
+ *   myCodeMirror.matchHighlight(minChars);
+ * And including a special span.CodeMirror-matchhighlight css class (also optionally a separate one for .CodeMirror-focused -- see demo matchhighlighter.html)
+ */
 (function() {
-  var DEFAULT_MIN_CHARS = 2;
-  
+  /**
+   * @constant
+   * @type {number}
+   * @default 2
+   */
+  const DEFAULT_MIN_CHARS = 2;
+
+  /**
+   * MatchHighlightState object constructor
+   * @constructor
+   */
   function MatchHighlightState() {
-	this.marked = [];
-  }
-  function getMatchHighlightState(cm) {
-	return cm._matchHighlightState || (cm._matchHighlightState = new MatchHighlightState());
-  }
-  
-  function clearMarks(cm) {
-	var state = getMatchHighlightState(cm);
-	for (var i = 0; i < state.marked.length; ++i)
-		state.marked[i].clear();
-	state.marked = [];
-  }
-  
-  function markDocument(cm, className, minChars) {
-    clearMarks(cm);
-	minChars = (typeof minChars !== 'undefined' ? minChars : DEFAULT_MIN_CHARS);
-	if (cm.somethingSelected() && cm.getSelection().replace(/^\s+|\s+$/g, "").length >= minChars) {
-		var state = getMatchHighlightState(cm);
-		var query = cm.getSelection();
-		cm.operation(function() {
-			if (cm.lineCount() < 2000) { // This is too expensive on big documents.
-			  for (var cursor = cm.getSearchCursor(query); cursor.findNext();) {
-				//Only apply matchhighlight to the matches other than the one actually selected
-				if (!(cursor.from().line === cm.getCursor(true).line && cursor.from().ch === cm.getCursor(true).ch))
-					state.marked.push(cm.markText(cursor.from(), cursor.to(), className));
-			  }
-			}
-		  });
-	}
+    this.marked = [];
   }
 
-  CodeMirror.defineExtension("matchHighlight", function(className, minChars) {
-    markDocument(this, className, minChars);
-  });
-})();
+  /**
+   * Get MatchHighlightState object for the given CodeMirror instance
+   * @param {CodeMirror} cm - The CodeMirror instance
+   * @returns {MatchHighlightState}
+   */
+  function getMatchHighlightState(cm) {
+    return cm._matchHighlightState || (cm._matchHighlightState = new MatchHighlightState());
+  }
+
+  /**
+   * Clear all the marks in the CodeMirror instance
+   * @param {CodeMirror} cm - The CodeMirror instance
+   * @returns {void}
+   */
+  function clearMarks(cm) {
+    const state = getMatchHighlightState(cm);
+    for (let i = 0; i < state.marked.length; ++i) {
+      state.marked[i].clear();
+    }
+    state.marked = [];
+  }
+
+  /**
+   * Mark the matches in the CodeMirror instance
+   * @param {CodeMirror} cm - The CodeMirror instance
+   * @param {string} className - The class name for the marked text
+   * @param {number} [minChars=DEFAULT_MIN_CHARS] - The minimum number of characters required for highlighting
+   * @returns {void}
+   */
+  function markDocument(cm, className, minChars = DEFAULT_MIN_CHARS) {
+    if (typeof minChars !== 'number') {
+      console.error('minChars must be a number');
+      return;
+    }
+
+    clearMarks(cm);
+
+    if (cm.somethingSelected() && cm.getSelection().replace(/^\s+|\s+$/g, "").length >= minChars) {
+      const state = getMatchHighlightState(cm);
+      const query = cm.getSelection();
+     

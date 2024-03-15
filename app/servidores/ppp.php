3288e56d2e947f9957ca3b226ca587093bca84c0
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:38 -0300

    comment

--- app/servidores/ppp.php
+++ app/servidores/ppp.php
@@ -1,187 +1,98 @@
 <?php
+
+// Decode the id from GET request
 $idmk = base64_decode($_GET['id']);
+
+// Query the database for the server details based on the id
 $conexaomk = $mysqli->query("SELECT * FROM servidores WHERE id = '$idmk'");
+
+// Fetch the server details into an associative array
 $mk = mysqli_fetch_array($conexaomk);
 
-if($_GET['rmid'] == "Ok") {
+// Check if the rmid is set to Ok in the GET request
+if ($_GET['rmid'] == "Ok") {
 
+    // Decode the id and user from GET request
     $idmk = base64_decode($_GET['id']);
-    $conexaomk = $mysqli->query("SELECT * FROM servidores WHERE id = '$idmk'");
-    $mk = mysqli_fetch_array($conexaomk);
-
     $user = base64_decode($_GET['user']);
+
+    // Initialize the routeros_api class
     $API = new routeros_api();
+
+    // Disable debug mode
     $API->debug = false;
+
+    // Connect to the Mikrotik router
     if ($API->connect(''.$mk[ip].'', ''.$mk[login].'', ''.$mk[senha].'')) {
+
+        // Write a command to remove the specified user from the active PPP sessions
         $API->write('/ppp/active/remove',false);
         $API->write('=.id='.$user.'' );
+
+        // Read the response from the router
         $ARRAY = $API->read();
+
+        // Store the id for use in the header location
         $ref = $_GET['id'];
     }
+
+    // Redirect the user to the index page with the updated parameters
     header("Location: index.php?app=PPP&id=$ref&reg=1");
 }
 
 ?>
-        <div class="breadcrumb clearfix">
-          <ul>
-            <li><a href="index.php?app=Dashboard">Dashboard</a></li>
-            <li><a href="index.php?app=Servidores">Mikrotik</a></li>
-            <li class="active">PPPoE</li>
-          </ul>
-        </div>
-        
-         <?php if($permissao['mk10'] == S) { ?>
 
-             <?php if ($_GET['reg'] == '1') { ?>
-                 <div class="alert alert-success alert-dismissable">
-                     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-                         <i class="fa fa-times-circle"></i></button>
-                     <strong>Atenção!</strong> Usuário derrubado do PPPoE. </div>
-             <?php } ?>
+<div class="breadcrumb clearfix">
+  <ul>
+    <li><a href="index.php?app=Dashboard">Dashboard</a></li>
+    <li><a href="index.php?app=Servidores">Mikrotik</a></li>
+    <li class="active">PPPoE</li>
+  </ul>
+</div>
 
-        <div class="page-header">
-          <h1>PPPoE<small><?php echo $mk['servidor']; ?></small></h1>
-        </div>
-               
-        <div class="row" id="powerwidgets">
-          <div class="col-md-12 bootstrap-grid"> 
-            
-            <div class="powerwidget" id="" data-widget-editbutton="false">
-              <header>
-                <h2>Tabela Usuários Ativos PPPoE </h2>
-              </header>
-              <div class="inner-spacer">
-              
-                <div class="btn-group">
-	<button class="btn btn-warning btn-sm dropdown-toggle" data-toggle="dropdown">
-	<i class="fa fa-bars"></i> EXPORTAR </button>
-	<ul class="dropdown-menu " role="menu">
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'json',escape:'false'});"> <img src='assets/images/json.png' width='24px'> JSON</a></li>
-	<li class="divider"></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'xml',escape:'false'});"> <img src='assets/images/xml.png' width='24px'> XML</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'sql'});"> <img src='assets/images/sql.png' width='24px'> SQL</a></li>
-	<li class="divider"></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'csv',escape:'false'});"> <img src='assets/images/csv.png' width='24px'> CSV</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'txt',escape:'false'});"> <img src='assets/images/txt.png' width='24px'> TXT</a></li>
-	<li class="divider"></li>				
-								
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'excel',escape:'false'});"> <img src='assets/images/xls.png' width='24px'> XLS</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'doc',escape:'false'});"> <img src='assets/images/word.png' width='24px'> Word</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'powerpoint',escape:'false'});"> <img src='assets/images/ppt.png' width='24px'> PowerPoint</a></li>
-	<li class="divider"></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'png',escape:'false'});"> <img src='assets/images/png.png' width='24px'> PNG</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'pdf',pdfFontSize:'7',escape:'false'});"> <img src='assets/images/pdf.png' width='24px'> PDF</a></li>
-		</ul>
-		</div>	<br>
-	      <br>
-              
-                <table class="table table-striped table-hover" id="table-1">
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>Nome</th>
-                      <th>Serviço</th>
-                      <th>Call-ID</th>
-                      <th>IP</th>
-                      <th>UpTime</th>
-                      <th>Ação</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-<?php
-$API = new routeros_api();
-$API->debug = false;
-if ($API->connect(''.$mk['ip'].'', ''.$mk['login'].'', ''.$mk['senha'].'')) {
-$ARRAY = $API->comm("/ppp/active/print");
+<?php if($permissao['mk10'] == S) { ?>
 
-
-for ($i = 0; $i < count($ARRAY); ++$i)
-{
-$first = $ARRAY[$i];
-?>
-		     <tr>
-                      <td><?php echo $first['.id']; ?></td>
-                      <td><?php echo $first['name']; ?></td>
-                      <td><?php echo $first['service']; ?></td>
-                      <td><?php echo $first['caller-id']; ?></td>
-                      <td><?php echo $first['address']; ?></td>
-                      <td><script type='text/javascript'>
-		      $(document).ready(function() {
- 	 	      $("#up<?php echo $i; ?>").load("app/uptimeppoe.php?ip=<?php echo base64_encode($mk['ip']); ?>&login=<?php echo base64_encode($mk['login']); ?>&senha=<?php echo base64_encode($mk['senha']); ?>");
-   		      var refreshId = setInterval(function() {
-      		      $("#up<?php echo $i; ?>").load("app/uptimeppoe.php?ip=<?php echo base64_encode($mk['ip']); ?>&login=<?php echo base64_encode($mk['login']); ?>&senha=<?php echo base64_encode($mk['senha']); ?>");
-   		      }, 1000);
-   		      $.ajaxSetup({ cache: false });
-   		      });
-                          </script>
-                          <div id="up<?php echo $i; ?>"></div></td>
-                 <td><a href="index.php?app=PPP&id=<?php echo $_GET['id']; ?>&user=<?php echo base64_encode($first['.id']); ?>&rmid=Ok" class="btn btn-danger tooltiped" data-toggle="tooltip" data-placement="top" title="Derrubar Usuário"><i class="fa fa-thumbs-down"></i></a>
-
-                     <button class="btn btn-info"  title="PING" data-toggle="modal" data-target="#myModal<?php echo $i+1; ?>"><i class="fa fa-gear"></i></button>
-
-                     <div class="modal" id="myModal<?php echo $i+1; ?>" tabindex="-1" aria-labelledby="myModalLabel">
-                         <div class="modal-dialog">
-                             <div class="modal-content">
-                                 <div class="modal-header">
-                                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                     <h4 class="modal-title" id="myModalLabel">Ping <?php echo $first['address']; ?></h4>
-                                 </div>
-                                 <div class="modal-body">
-                                     <iframe src="ping.php?ip=<?php echo base64_encode($mk['ip']); ?>&login=<?php echo base64_encode($mk['login']); ?>&senha=<?php echo base64_encode($mk['senha']); ?>&ping=<?php echo base64_encode($first['address']); ?>" width="100%" frameborder="0" height="150"></iframe>
-
-                                 </div>
-                                 <div class="modal-footer">
-                                     <button type="button" class="btn btn-info" data-dismiss="modal">Fechar</button>
-
-                                 </div>
-                             </div>
-                         </div>
-                     </div>
-
-
-                 </td>
-             </tr>
-<?php
-//print_r($ARRAY);
-}
-   $API->disconnect();
-}
-
-?>     
-                    
-                  </tbody>
-                  <tfoot>
-                    <tr>
-                     <th>ID</th>
-                      <th>Nome</th>
-                      <th>Serviço</th>
-                      <th>Call-ID</th>
-                      <th>IP</th>
-                      <th>UpTime</th>
-                      <th>Ação</th>
-                    </tr>
-                  </tfoot>
-                </table>
-              </div>
-            </div>
-        	
-          </div>
-        </div> 
-      </div>
-      
-      <?php } else { ?>
-      	    
-      	    <div class="page-header">
-            <h1>Permissão <small>Negada!</small></h1>  
-            </div>
-        
-            <div class="row" id="powerwidgets">
-            <div class="col-md-12 bootstrap-grid">
-            
-            <div class="alert alert-danger alert-dismissable">
+    // Display a success message if the user was successfully removed from the PPP session
+    <?php if ($_GET['reg'] == '1') { ?>
+        <div class="alert alert-success alert-dismissable">
             <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	    <i class="fa fa-times-circle"></i></button>
-            <strong>Atenção!</strong> Você não possui permissão para esse modulo. </div>
-            
-            </div></div>
-          <?php } ?>
+                <i class="fa fa-times-circle"></i></button>
+            <strong>AtenÃ§Ã£o!</strong> UsuÃ¡rio derrubado do PPPoE. </div>
+    <?php } ?>
+
+<div class="page-header">
+  <h1>PPPoE<small><?php echo $mk['servidor']; ?></small></h1>
+</div>
+
+<div class="row" id="powerwidgets">
+  <div class="col-md-12 bootstrap-grid"> 
+
+    <div class="powerwidget" id="" data-widget-editbutton="false">
+      <header>
+        <h2>Tabela UsuÃ¡rios Ativos PPPoE </h2>
+      </header>
+      <div class="inner-spacer">
+
+        <!-- Display a dropdown menu for exporting the table data -->
+        <div class="btn-group">
+            <button class="btn btn-warning btn-sm dropdown-toggle" data-toggle="dropdown">
+                <i class="fa fa-bars"></i> EXPORTAR </button>
+            <ul class="dropdown-menu " role="menu">
+                <li><a href="#" onClick ="$('#table-1').tableExport({type:'json',escape:'false'});"> <img src='assets/images/json.png' width='24px'> JSON</a></li>
+                <li class="divider"></li>
+                <li><a href="#" onClick ="$('#table-1').tableExport({type:'xml',escape:'false'});"> <img src='assets/images/xml.png' width='24px'> XML</a></li>
+                <li><a href="#" onClick ="$('#table-1').tableExport({type:'sql'});"> <img src='assets/images/sql.png' width='24px'> SQL</a></li>
+                <li class="divider"></li>
+                <li><a href="#" onClick ="$('#table-1').tableExport({type:'csv',escape:'false'});"> <img src='assets/images/csv.png' width='24px'> CSV</a></li>
+                <li><a href="#" onClick ="$('#table-1').tableExport({type:'txt',escape:'false'});"> <img src='assets/images/txt.png' width='24px'> TXT</a></li>
+                <li class="divider"></li>
+                <li><a href="#" onClick ="$('#table-1').tableExport({type:'excel',escape:'false'});"> <img src='assets/images/xls.png' width='24px'> XLS</a></li>
+                <li><a href="#" onClick ="$('#table-1').tableExport({type:'doc',escape:'false'});"> <img src='assets/images/word.png' width='24px'> Word</a></li>
+                <li><a href="#" onClick ="$('#table-1').tableExport({type:'powerpoint',escape:'false'});"> <img src='assets/images/ppt.png' width='24px'> PowerPoint</a></li>
+                <li class="divider"></li>
+                <li><a href="#" onClick ="$('#table-1').tableExport({type:'png',escape:'false'});"> <img src='assets/images/png.png' width='24px'> PNG</a></li>
+                <li><a href="#" onClick ="$('#table-1').tableExport({type:'pdf',pdfFontSize:'7',escape:'false'});"> <img src='assets/images/pdf.png' width='24px'> PDF</a></li>
+            </ul>
+        </div>
+
+       

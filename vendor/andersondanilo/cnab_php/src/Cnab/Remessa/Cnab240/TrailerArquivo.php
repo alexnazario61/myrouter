0bba768ed06ec943f5af336895f01d8b3b129662
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:40:53 -0300

    comment

--- vendor/andersondanilo/cnab_php/src/Cnab/Remessa/Cnab240/TrailerArquivo.php
+++ vendor/andersondanilo/cnab_php/src/Cnab/Remessa/Cnab240/TrailerArquivo.php
@@ -1,11 +1,17 @@
 <?php
+// Define the namespace for this class
 namespace Cnab\Remessa\Cnab240;
 
+// TrailerArquivo class extends \Cnab\Format\Linha, meaning it inherits properties and methods from the Linha class
 class TrailerArquivo extends \Cnab\Format\Linha
 {
+    // Constructor method for the TrailerArquivo class
     public function __construct(\Cnab\Remessa\IArquivo $arquivo)
     {
+        // Create a new instance of the YamlLoad class, passing in the bank code and layout version as arguments
         $yamlLoad = new \Cnab\Format\YamlLoad($arquivo->codigo_banco, $arquivo->layoutVersao);
+
+        // Load the trailer_arquivo configuration for the specified bank and layout version using the YamlLoad instance
         $yamlLoad->load($this, 'cnab240', 'trailer_arquivo');
     }
 }

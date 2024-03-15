63ddff9f949f6120ad9d5cf6231cacdee2c51832
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:48:37 -0300

    improve

--- app/highchart/js/modules/map.js
+++ app/highchart/js/modules/map.js
@@ -1,47 +1,4 @@
 /*
 Map plugin v0.1 for Highcharts
 
-(c) 2011-2013 Torstein Hønsi
 
-License: www.highcharts.com/license
-*/
-
-
-f.splitPath = function(a) {
-  // ...
-};
-
-
-f.maps = {};
-
-
-f.wrap(s.prototype, "init", function(a,b,c) {
-  // ...
-});
-
-
-f.wrap(t.prototype, "render", function(a) {
-  // ...
-});
-
-
-q(t.prototype, {
-  renderMapNavigation: function() {
-    // ...
-  }
-});
-
-
-w.map = p(w.scatter, {
-  // ...
-});
-
-
-f.seriesTypes.map = f.extendClass(f.seriesTypes.scatter, {
-  // ...
-});
-
-
-f.Map = function(a, b) {
-  // ...
-};

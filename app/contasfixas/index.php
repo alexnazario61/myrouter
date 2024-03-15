15f9cd92958e8d9e76a0739966490a7e7c0048da
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:31 -0300

    comment

--- app/contasfixas/index.php
+++ app/contasfixas/index.php
@@ -1,150 +1,73 @@
+<div class="breadcrumb clearfix">
+  <ul>
+    <!-- The 'Dashboard' link will take the user to the index.php page of the Dashboard application. -->
+    <li><a href="index.php?app=Dashboard">Dashboard</a></li>
+    <!-- The 'Contas Fixas' link is currently active, indicating that the user is on the 'Contas Fixas' page. -->
+    <li class="active">Contas Fixas</li>
+  </ul>
+</div>
 
-        <div class="breadcrumb clearfix">
-          <ul>
-            <li><a href="index.php?app=Dashboard">Dashboard</a></li>
-            <li class="active">Contas Fixas</li>
-          </ul>
-        </div>
-        
-        <?php if($permissao['p1'] == S) { ?>
-        
-        <?php if ($_GET['reg'] == '1') { ?>
-	<div class="alert alert-success alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	<i class="fa fa-times-circle"></i></button>
-        <strong>Atenção!</strong> Conta Fixa cadastrado com sucesso. </div>
-	<?php } ?>
-	<?php if ($_GET['reg'] == '2') { ?>
-	<div class="alert alert-info alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	<i class="fa fa-times-circle"></i></button>
-        <strong>Atenção!</strong> Conta Fixa alterado com sucesso. </div>
-	<?php } ?>
-	<?php if ($_GET['reg'] == '3') { ?>
-	<div class="alert alert-danger alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	<i class="fa fa-times-circle"></i></button>
-        <strong>Atenção!</strong> Conta Fixa excluído com sucesso. </div>
-	<?php } ?>
-	<?php if ($_GET['reg'] == '4') { ?>
-	<div class="alert alert-danger alert-dismissable">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	<i class="fa fa-times-circle"></i></button>
-        <strong>Atenção!</strong> Existe assinaturas para esse plano, não é possível remover. </div>
-	<?php } ?>
-        
-        <div class="page-header">
-          <h1>Contas<small>Fixas</small></h1>
-        </div>
-        
-        <div class="row" id="powerwidgets">
-          <div class="col-md-12 bootstrap-grid"> 
-            
-            <div class="powerwidget" id="" data-widget-editbutton="false">
-              <header>
-                <h2>Gerenciar<small>Contas Fixas</small></h2>
-              </header>
-              <div class="inner-spacer">
-              
-                    <div class="btn-group">
-	<button class="btn btn-warning btn-sm dropdown-toggle" data-toggle="dropdown">
-	<i class="fa fa-bars"></i> EXPORTAR </button>
-	<ul class="dropdown-menu " role="menu">
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'json',escape:'false'});"> <img src='assets/images/json.png' width='24px'> JSON</a></li>
-	<li class="divider"></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'xml',escape:'false'});"> <img src='assets/images/xml.png' width='24px'> XML</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'sql'});"> <img src='assets/images/sql.png' width='24px'> SQL</a></li>
-	<li class="divider"></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'csv',escape:'false'});"> <img src='assets/images/csv.png' width='24px'> CSV</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'txt',escape:'false'});"> <img src='assets/images/txt.png' width='24px'> TXT</a></li>
-	<li class="divider"></li>				
-								
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'excel',escape:'false'});"> <img src='assets/images/xls.png' width='24px'> XLS</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'doc',escape:'false'});"> <img src='assets/images/word.png' width='24px'> Word</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'powerpoint',escape:'false'});"> <img src='assets/images/ppt.png' width='24px'> PowerPoint</a></li>
-	<li class="divider"></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'png',escape:'false'});"> <img src='assets/images/png.png' width='24px'> PNG</a></li>
-	<li><a href="#" onClick ="$('#table-1').tableExport({type:'pdf',pdfFontSize:'7',escape:'false'});"> <img src='assets/images/pdf.png' width='24px'> PDF</a></li>
-		</ul>
-		</div>	<br>
-	      <br>
-              
-                <table class="table table-striped table-hover" id="table-1">
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>Descrição</th>
-                      <th>Categoria</th>
-                      <th>Dia Vencimento</th>
-                      <th>Valor</th>
-                      <th width="110">Ações</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                  <?php
-                  
- 		  $idempresa = $_SESSION['empresa'];
- 		  $consultas = $mysqli->query("SELECT * FROM lc_fixas WHERE empresa = '$idempresa'");
- 		  while($campo = mysqli_fetch_array($consultas)){
-		  
-		  ?>
-		  <tr>
-                     <td><?php echo $campo['id']; ?></td>
-                      <td><?php echo $campo['descricao_fixa']; ?></td>
+<!-- Check if the user has permission to view this page. If not, display a warning message and stop further processing. -->
+<?php if($permissao['p1'] == S) { ?>
 
-                       <td>
-		      <?php
-		      $ids = $campo['cat'];
-		      $ccat = $mysqli->query("SELECT * FROM lc_cat WHERE id = '$ids'");
- 		      $ccategoria = mysqli_fetch_array($ccat);
- 		      ?><?php echo $ccategoria['nome']; ?></td>
+<!-- Display success, info, or danger messages based on the value of the 'reg' GET parameter. -->
+<?php if ($_GET['reg'] == '1') { ?>
+<div class="alert alert-success alert-dismissable">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+    <i class="fa fa-times-circle"></i></button>
+  <strong>AtenÃ§Ã£o!</strong> Conta Fixa cadastrado com sucesso. </div>
+<?php } ?>
+<?php if ($_GET['reg'] == '2') { ?>
+<div class="alert alert-info alert-dismissable">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+    <i class="fa fa-times-circle"></i></button>
+  <strong>AtenÃ§Ã£o!</strong> Conta Fixa alterado com sucesso. </div>
+<?php } ?>
+<?php if ($_GET['reg'] == '3') { ?>
+<div class="alert alert-danger alert-dismissable">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+    <i class="fa fa-times-circle"></i></button>
+  <strong>AtenÃ§Ã£o!</strong> Conta Fixa excluÃ­do com sucesso. </div>
+<?php } ?>
+<?php if ($_GET['reg'] == '4') { ?>
+<div class="alert alert-danger alert-dismissable">
+  <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+    <i class="fa fa-times-circle"></i></button>
+  <strong>AtenÃ§Ã£o!</strong> Existe assinaturas para esse plano, nÃ£o Ã© possÃ­vel remover. </div>
+<?php } ?>
 
-                      <td><?php echo $campo['dia_vencimento']; ?></td>
-                      <td>R$ <?php echo number_format($campo['valor_fixa'], 2, ',','.'); ?></td>
+<div class="page-header">
+  <!-- The page title is 'Contas' with a small subtitle of 'Fixas'. -->
+  <h1>Contas<small>Fixas</small></h1>
+</div>
 
-                      <td>
-	      <a href="?app=CadastroContasFixas&id=<?php echo base64_encode($campo['id']); ?>" class="btn btn-info tooltiped" data-toggle="tooltip" data-placement="top" title="Alterar"><i class="entypo-tools"></i></a>&nbsp;
-	      <?php if($logado['nivel'] == '1') { ?> 
-	      	 <a href="javascript:void(0);" onclick="javascript: if (confirm('Deseja realmente excluir esse registro ?')) { window.location.href='?app=CadastroContasFixas&id=<?php echo base64_encode($campo['id']); ?>&Ex=Del' } else { void('') };"  class="btn btn-danger tooltiped" data-toggle="tooltip" data-placement="top" title="Excluir"><i class="entypo-trash"></i></a>
-	      <?php } ?>
-		      </td>
-                    </tr>
-                   
-		  <?php  } ?>
+<div class="row" id="powerwidgets">
+  <div class="col-md-12 bootstrap-grid">
 
-                  </tbody>
-                  <tfoot>
-                    <tr>
-                      <th>ID</th>
-                      <th>Descrição</th>
-                      <th>Categoria</th>
-                      <th>Dia Vencimento</th>
-                      <th>Valor</th>
-                      <th>Ações</th>
-                    </tr>
-                  </tfoot>
-                </table>
-              </div>
-            </div>
-        	
-          </div>
-        </div> 
-      </div>
-      
-      <?php } else { ?>
-      	    
-      	    <div class="page-header">
-            <h1>Permissão <small>Negada!</small></h1>  
-            </div>
-        
-            <div class="row" id="powerwidgets">
-            <div class="col-md-12 bootstrap-grid">
-            
-            <div class="alert alert-danger alert-dismissable">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	    <i class="fa fa-times-circle"></i></button>
-            <strong>Atenção!</strong> Você não possui permissão para esse modulo. </div>
-            
-            </div></div>
-          <?php } ?>
-      
+    <!-- A PowerWidget container for managing Contas Fixas. -->
+    <div class="powerwidget" id="" data-widget-editbutton="false">
+      <header>
+        <!-- The widget header has a title of 'Gerenciar Contas Fixas'. -->
+        <h2>Gerenciar<small>Contas Fixas</small></h2>
+      </header>
+      <div class="inner-spacer">
+
+        <!-- A button group for exporting the table data in various formats. -->
+        <div class="btn-group">
+          <button class="btn btn-warning btn-sm dropdown-toggle" data-toggle="dropdown">
+            <i class="fa fa-bars"></i> EXPORTAR </button>
+          <ul class="dropdown-menu " role="menu">
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'json',escape:'false'});"> <img src='assets/images/json.png' width='24px'> JSON</a></li>
+            <li class="divider"></li>
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'xml',escape:'false'});"> <img src='assets/images/xml.png' width='24px'> XML</a></li>
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'sql'});"> <img src='assets/images/sql.png' width='24px'> SQL</a></li>
+            <li class="divider"></li>
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'csv',escape:'false'});"> <img src='assets/images/csv.png' width='24px'> CSV</a></li>
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'txt',escape:'false'});"> <img src='assets/images/txt.png' width='24px'> TXT</a></li>
+            <li class="divider"></li>
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'excel',escape:'false'});"> <img src='assets/images/xls.png' width='24px'> XLS</a></li>
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'doc',escape:'false'});"> <img src='assets/images/word.png' width='24px'> Word</a></li>
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'powerpoint',escape:'false'});"> <img src='assets/images/ppt.png' width='24px'> PowerPoint</a></li>
+            <li class="divider"></li>
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'png',escape:'false'});"> <img src='assets/images/png.png' width='24px'> PNG</a></li>
+            <li><a href="#" onClick ="$('#table-1').tableExport({type:'pdf',pdfFontSize:'7',escape:'false'});"> <img src='assets/images/pdf.png' width='24px'> PDF</a></li

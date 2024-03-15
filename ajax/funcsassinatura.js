ff7e467d78cb5d37f858709fd1b8a022b79afacd
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:48:46 -0300

    improve

--- ajax/funcsassinatura.js
+++ ajax/funcsassinatura.js
@@ -1,43 +1,26 @@
-// Variável que receberá o objeto XMLHttpRequest
-var req;
+// Function to validate login for subscriptions
+async function validarLoginAssinaturas(campo, valor) {
+  try {
+    // URL for the request
+    const url = `ajax/assinatura.php?campo=${campo}&valor=${valor}`;
 
-function validarLoginAssinaturas (campo, valor) {
+    // Send the request and wait for a response
+    const response = await fetch(url);
 
-// Verificar o Browser
-// Firefox, Google Chrome, Safari e outros
-    if(window.XMLHttpRequest) {
-        req = new XMLHttpRequest();
+    // If the request was successful, parse the response as text
+    if (response.ok) {
+      const resposta = await response.json();
+
+      // Update the 'campo_login' element with the response
+      document.getElementById('campo_login').innerHTML = resposta.mensagem;
+    } else {
+      // If the request was not successful, show an error message
+      document.getElementById('campo_login').innerHTML =
+        'Erro na requisiÃ§Ã£o. Por favor, tente novamente.';
     }
-// Internet Explorer
-    else if(window.ActiveXObject) {
-        req = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-
-// Aqui vai o valor e o nome do campo que pediu a requisição.
-    var url = "ajax/assinatura.php?campo="+campo+"&valor="+valor;
-
-// Chamada do método open para processar a requisição
-    req.open("Get", url, true);
-
-// Quando o objeto recebe o retorno, é chamada a seguinte função;
-    req.onreadystatechange = function() {
-
-        // Exibe a mensagem "Verificando" enquanto carrega
-        if(req.readyState == 1) {
-            document.getElementById('campo_login').innerHTML = '<font color="gray">Verificando...</font>';
-        }
-
-        // Verifica se o Ajax realizou todas as operações corretamente
-        if(req.readyState == 4 && req.status == 200) {
-            // Resposta retornada pelo validacao.php (pagina de conexão com o BD)
-            var resposta = req.responseText;
-
-            // Resposta na div do campo que fez a requisição
-            document.getElementById('campo_login').innerHTML = resposta;
-        }
-
-    }
-
-    req.send(null);
-
+  } catch (error) {
+    // If there was an error with the request, show an error message
+    document.getElementById('campo_login').innerHTML =
+      'Erro no processamento da requisiÃ§Ã£o. Por favor, tente novamente.';
+  }
 }

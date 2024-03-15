11019e3bd1a1dd961d001f410e7ec59896106f89
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:23 -0300

    comment

--- app/remessa/_src/Cnab/Remessa/Cnab240/SegmentoQ.php
+++ app/remessa/_src/Cnab/Remessa/Cnab240/SegmentoQ.php
@@ -3,9 +3,17 @@ namespace Cnab\Remessa\Cnab240;
 
 class SegmentoQ extends \Cnab\Format\Linha
 {
+    /**
+     * Constructs a new instance of the SegmentoQ class.
+     * @param \Cnab\Remessa\IArquivo $arquivo The remittance archival object that this segment belongs to.
+     */
     public function __construct(\Cnab\Remessa\IArquivo $arquivo)
     {
+        // Load the YAML configuration for this segment based on the bank code and layout version
         $yamlLoad = new \Cnab\Format\YamlLoad($arquivo->codigo_banco, $arquivo->layoutVersao);
+        
+        // Load the configuration for this segment from the YAML file
         $yamlLoad->load($this, 'cnab240', 'remessa/detalhe_segmento_q');
     }
 }
+

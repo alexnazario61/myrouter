11a76a046da95dd17e391d860069aa857597ef19
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:40:27 -0300

    comment

--- cliente/assets/highchart/exporting-server/java/highcharts-export/highcharts-export-convert/src/main/java/com/highcharts/export/pool/ObjectFactory.java
+++ cliente/assets/highchart/exporting-server/java/highcharts-export/highcharts-export-convert/src/main/java/com/highcharts/export/pool/ObjectFactory.java
@@ -1,9 +1,47 @@
-package com.highcharts.export.pool;
+/**
+ * This interface defines the methods for creating, validating, destroying, activating, and passivating objects of type T.
+ * Implementations of this interface should provide a concrete implementation for each method.
+ */
+public interface ObjectFactory<T> {
 
-public interface ObjectFactory<T>{
-	public T create();
-	public boolean validate(T object);
-	public void destroy(T object);
-	public void activate(T object);
-	public void passivate(T Object);
+    /**
+     * Creates a new object of type T.
+     * This method should allocate and initialize any resources necessary for the object.
+     *
+     * @return a new object of type T
+     */
+    public T create();
+
+    /**
+     * Validates the given object of type T.
+     * This method should check whether the object is in a valid state and meets any necessary conditions.
+     *
+     * @param object the object to validate
+     * @return true if the object is valid, false otherwise
+     */
+    public boolean validate(T object);
+
+    /**
+     * Destroys the given object of type T.
+     * This method should release any resources allocated by the create() method for the object.
+     *
+     * @param object the object to destroy
+     */
+    public void destroy(T object);
+
+    /**
+     * Activates the given object of type T.
+     * This method should make the object ready for use, for example by initializing any necessary state.
+     *
+     * @param object the object to activate
+     */
+    public void activate(T object);
+
+    /**
+     * Passivates the given object of type T.
+     * This method should make the object no longer ready for use, for example by clearing any state.
+     *
+     * @param object the object to passivate
+     */
+    public void passivate(T object);
 }

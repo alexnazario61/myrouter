ad167b1b0f614e0af9a07f7aafa74cde7e423c78
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:48:56 -0300

    improve

--- assets/js/i18n/defaults-en_US.js
+++ assets/js/i18n/defaults-en_US.js
@@ -1,50 +1,74 @@
 /*!
- * Bootstrap-select v1.6.3 (http://silviomoreto.github.io/bootstrap-select/)
+ * Bootstrap-select v1.6.3
+ * http://silviomoreto.github.io/bootstrap-select/
  *
  * Copyright 2013-2014 bootstrap-select
  * Licensed under MIT (https://github.com/silviomoreto/bootstrap-select/blob/master/LICENSE)
  */
 
-// Defining the default options for the selectpicker plugin
+// Default options for the selectpicker plugin
 (function ($) {
   $.fn.selectpicker.defaults = {
-    // The text to display when no options are selected
+    /**
+     * The text to display when no options are selected
+     * @type {string}
+     */
     noneSelectedText: 'Nothing selected',
 
-    // The text to display when there are no matching results
+    /**
+     * The text to display when there are no matching results
+     * @type {string}
+     */
     noneResultsText: 'No results match',
 
-    // The function to format the count of selected items
+    /**
+     * The function to format the count of selected items
+     * @param {number} numSelected - The number of selected items
+     * @param {number} numTotal - The total number of options
+     * @return {string}
+     */
     countSelectedText: function (numSelected, numTotal) {
       // If there is only one selected item, return the singular format
-      if (numSelected == 1) {
+      if (numSelected === 1) {
         return numSelected + " item selected";
       }
+
       // Otherwise, return the plural format
-      else {
-        return numSelected + " items selected";
-      }
+      return numSelected + " items selected";
     },
 
-    // The text to display when the limit of options or group options is reached
+    /**
+     * The text to display when the limit of options or group options is reached
+     * @param {number} numAll - The total number of options
+     * @param {number} numGroup - The number of group options
+     * @return {string}
+     */
     maxOptionsText: function (numAll, numGroup) {
-      var arr = [];
-
-      // Create an array with two format strings: one for all options and one for group options
-      arr[0] = (numAll == 1) ? 'Limit reached ({n} item max)' : 'Limit reached ({n} items max)';
-      arr[1] = (numGroup == 1) ? 'Group limit reached ({n} item max)' : 'Group limit reached ({n} items max)';
+      const arr = [
+        (numAll === 1) ? 'Limit reached (1 item max)' : 'Limit reached ({n} items max)',
+        (numGroup === 1) ? 'Group limit reached (1 item max)' : 'Group limit reached ({n} items max)',
+      ];
 
       // Return the appropriate format string based on the context
       return arr[+(numGroup > 0)];
     },
 
-    // The text for the "Select All" button
+    /**
+     * The text for the "Select All" button
+     * @type {string}
+     */
     selectAllText: 'Select All',
 
-    // The text for the "Deselect All" button
+    /**
+     * The text for the "Deselect All" button
+     * @type {string}
+     */
     deselectAllText: 'Deselect All',
 
-    // The separator for multiple selected options
+    /**
+     * The separator for multiple selected options
+     * @type {string}
+     */
     multipleSeparator: ', '
   };
 }(jQuery));

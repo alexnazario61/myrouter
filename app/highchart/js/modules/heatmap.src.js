8e689e8f2f8ed8293b977ee23243c76c1232e91e
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:48:47 -0300

    improve

--- app/highchart/js/modules/heatmap.src.js
+++ app/highchart/js/modules/heatmap.src.js
@@ -1,52 +1,41 @@
 (function (Highcharts) {
-	var seriesTypes = Highcharts.seriesTypes,
-		each = Highcharts.each;
-	
-	seriesTypes.heatmap = Highcharts.extendClass(seriesTypes.map, {
-		colorKey: 'z',
-		pointArrayMap: ['y', 'z'],
-		translate: function () {
-			var series = this,
-				options = series.options,
-				dataMin = Number.MAX_VALUE,
-				dataMax = Number.MIN_VALUE;
+    var seriesTypes = Highcharts.seriesTypes,
+        each = Highcharts.each;
 
-			series.generatePoints();
-	
-			each(series.data, function (point) {
-				var x = point.x,
-					y = point.y,
-					value = point.z,
-					xPad = (options.colsize || 1) / 2,
-					yPad = (options.rowsize || 1) / 2;
+    seriesTypes.heatmap = Highcharts.extendClass(seriesTypes.map, {
+        colorKey: 'z',
+        pointArrayMap: ['y', 'z'],
 
-				point.path = [
-					'M', x - xPad, y - yPad,
-					'L', x + xPad, y - yPad,
-					'L', x + xPad, y + yPad,
-					'L', x - xPad, y + yPad,
-					'Z'
-				];
-				
-				point.shapeType = 'path';
-				point.shapeArgs = {
-					d: series.translatePath(point.path)
-				};
-				
-				if (typeof value === 'number') {
-					if (value > dataMax) {
-						dataMax = value;
-					} else if (value < dataMin) {
-						dataMin = value;
-					}
-				}
-			});
-			
-			series.translateColors(dataMin, dataMax);
-		},
-		
-		getBox: function () {}
-			
-	});
-	
-}(Highcharts));
+        translate: function () {
+            var series = this,
+                options = series.options,
+                dataMin = Number.MAX_VALUE,
+                dataMax = Number.MIN_VALUE;
+
+            series.generatePoints();
+
+            each(series.data, function (point) {
+                var x = point.x,
+                    y = point.y,
+                    value = point.z,
+                    xPad = (options.colsize || 1) / 2,
+                    yPad = (options.rowsize || 1) / 2;
+
+                if (typeof options.colsize !== 'number' || options.colsize <= 0) {
+                    console.error('options.colsize must be a positive number');
+                    return;
+                }
+
+                if (typeof options.rowsize !== 'number' || options.rowsize <= 0) {
+                    console.error('options.rowsize must be a positive number');
+                    return;
+                }
+
+                if (typeof value !== 'number') {
+                    console.error('point.z must be a number');
+                    return;
+                }
+
+                point.path = [
+                    'M', x - xPad, y - yPad,
+                    'L', x + xPad, y

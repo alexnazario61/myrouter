0b477dbca2d4606141cdb82395079d0f59dc9e31
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:08 -0300

    comment

--- ajax/login.php
+++ ajax/login.php
@@ -1,26 +1,2 @@
 <?php
-header("Content-Type: text/html; charset=ISO-8859-1", true);
-include("../config/conexao.class.php");
-
-$campo = $_GET['campo'];
-$valor = $_GET['valor'];
-
-$ok=''; // imagem de confirmação
-$erro='<font color=red>ERRO</font>'; // imagem de negação
-
-// Verificando o campo codigo
-if ($campo == "login") {
-   
-   $sql = $mysqli->query("SELECT * FROM clientes WHERE login = '$valor'");
-   $job = mysqli_fetch_array($sql);
-   
-
-    if ($job['login'] == $valor) {
-    echo $erro;
-    echo " Login já utilizado.";
-    } else {
-    echo $ok;
-        }
-
-}
-?>
+// Setting the content type to text/html and charset to ISO-885

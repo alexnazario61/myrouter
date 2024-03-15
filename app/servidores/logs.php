d01cda0c7a207a1e9a65d4f38f8c66361659c12e
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:39:36 -0300

    comment

--- app/servidores/logs.php
+++ app/servidores/logs.php
@@ -1,97 +1,98 @@
 <?php
+// Decode the 'id' parameter from GET request
 $idmk = base64_decode($_GET['id']);
+
+// Query the 'servidores' table in the database to fetch server details
 $conexaomk = $mysqli->query("SELECT * FROM servidores WHERE id = '$idmk'");
+
+// Fetch the server details into an associative array
 $mk = mysqli_fetch_array($conexaomk);
-
-
 ?>
-        <div class="breadcrumb clearfix">
-          <ul>
-            <li><a href="index.php?app=Dashboard">Dashboard</a></li>
-            <li><a href="index.php?app=Servidores">Mikrotik</a></li>
-            <li class="active">Logs do Mikrotik</li>
-          </ul>
-        </div>
-        
-        <?php if($permissao['mk6'] == S) { ?>
-        
-        <div class="page-header">
-          <h1>Logs Servidor<small><?php echo $mk['servidor']; ?></small></h1>
-        </div>
-               
-        <div class="row" id="powerwidgets">
-          <div class="col-md-12 bootstrap-grid"> 
-            
-            <div class="powerwidget" id="" data-widget-editbutton="false">
-              <header>
-                <h2>Tabela de Logs </h2>
-              </header>
-              <div class="inner-spacer">
-                <table class="table table-striped table-hover" id="table-1">
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>Timer</th>
-                      <th>Ação</th>
-                      <th>Política</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-<?php
-$API = new routeros_api();
-$API->debug = false;
-if ($API->connect(''.$mk['ip'].'', ''.$mk['login'].'', ''.$mk['senha'].'')) {
-$ARRAY = $API->comm("/system/history/print");
 
+<div class="breadcrumb clearfix">
+  <!-- Breadcrumb navigation links -->
+</div>
 
-for ($i = 0; $i < count($ARRAY); ++$i)
-{
-$first = $ARRAY[$i];
-?>
-		     <tr>
-                     <td><?php echo $first['by']; ?></td>
-                      <td><?php echo $first['time']; ?></td>
-                      <td><?php echo $first['action']; ?></td>
-                      <td><?php echo $first['policy']; ?></td>
-                    </tr>
-<?php 
-//print_r($ARRAY);
-}
-   $API->disconnect();
-}
+<?php if($permissao['mk6'] == S) { ?>
+<div class="page-header">
+  <!-- Page title -->
+</div>
 
-?>     
-                    
-                  </tbody>
-                  <tfoot>
-                    <tr>
-                     <th>ID</th>
-                      <th>Timer</th>
-                      <th>Ação</th>
-                      <th>Política</th>
-                    </tr>
-                  </tfoot>
-                </table>
-              </div>
-            </div>
-        	
-          </div>
-        </div> 
+<div class="row" id="powerwidgets">
+  <!-- Container for the table -->
+  <div class="col-md-12 bootstrap-grid">
+    <!-- Powerwidget element for the table -->
+    <div class="powerwidget" id="" data-widget-editbutton="false">
+      <header>
+        <!-- Widget header with the table title -->
+        <h2>Tabela de Logs </h2>
+      </header>
+      <div class="inner-spacer">
+        <!-- Table container -->
+        <table class="table table-striped table-hover" id="table-1">
+          <thead>
+            <!-- Table header row -->
+            <tr>
+              <th>ID</th>
+              <th>Timer</th>
+              <th>AÃ§Ã£o</th>
+              <th>PolÃ­tica</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- Table body rows generated dynamically based on Mikrotik server logs -->
+            <?php
+            // Connect to the Mikrotik server using the routeros_api class
+            $API = new routeros_api();
+            $API->debug = false;
+            if ($API->connect(''.$mk['ip'].'', ''.$mk['login'].'', ''.$mk['senha'].'')) {
+              // Fetch logs from the Mikrotik server
+              $ARRAY = $API->comm("/system/history/print");
+
+              // Loop through the logs and generate table rows
+              for ($i = 0; $i < count($ARRAY); ++$i) {
+                $first = $ARRAY[$i];
+            ?>
+              <tr>
+                <td><?php echo $first['by']; ?></td>
+                <td><?php echo $first['time']; ?></td>
+                <td><?php echo $first['action']; ?></td>
+                <td><?php echo $first['policy']; ?></td>
+              </tr>
+            <?php
+              }
+              // Disconnect from the Mikrotik server
+              $API->disconnect();
+            }
+            ?>
+          </tbody>
+          <tfoot>
+            <!-- Table footer row -->
+            <tr>
+              <th>ID</th>
+              <th>Timer</th>
+              <th>AÃ§Ã£o</th>
+              <th>PolÃ­tica</th>
+            </tr>
+          </tfoot>
+        </table>
       </div>
-      
-      <?php } else { ?>
-      	    
-      	    <div class="page-header">
-            <h1>Permissão <small>Negada!</small></h1>  
-            </div>
-        
-            <div class="row" id="powerwidgets">
-            <div class="col-md-12 bootstrap-grid">
-            
-            <div class="alert alert-danger alert-dismissable">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
-	    <i class="fa fa-times-circle"></i></button>
-            <strong>Atenção!</strong> Você não possui permissão para esse modulo. </div>
-            
-            </div></div>
-          <?php } ?>
+    </div>
+  </div>
+</div>
+<?php } else { ?>
+<div class="page-header">
+  <!-- Page title when the user doesn't have permission -->
+</div>
+
+<div class="row" id="powerwidgets">
+  <div class="col-md-12 bootstrap-grid">
+    <!-- Alert box when the user doesn't have permission -->
+    <div class="alert alert-danger alert-dismissable">
+      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">
+        <i class="fa fa-times-circle"></i></button>
+      <strong>AtenÃ§Ã£o!</strong> VocÃª nÃ£o possui permissÃ£o para esse modulo.
+    </div>
+  </div>
+</div>
+<?php } ?>

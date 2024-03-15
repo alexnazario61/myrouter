b24b25074da6cb804753f2aa3a5a26070f42472a
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:40:51 -0300

    comment

--- vendor/andersondanilo/cnab_php/src/Cnab/Retorno/Cnab240/SegmentoT.php
+++ vendor/andersondanilo/cnab_php/src/Cnab/Retorno/Cnab240/SegmentoT.php
@@ -3,9 +3,15 @@ namespace Cnab\Retorno\Cnab240;
 
 class SegmentoT extends \Cnab\Format\Linha
 {
-	public function __construct(\Cnab\Retorno\IArquivo $arquivo)
+    /**
+     * Constructs a new SegmentoT object.
+     * 
+     * @param \Cnab\Retorno\IArquivo $arquivo The input arquivo object.
+     */
+    public function __construct(\Cnab\Retorno\IArquivo $arquivo)
     {
+        // Load the YAML configuration for this segmento based on the bank code and layout version.
         $yamlLoad = new \Cnab\Format\YamlLoad($arquivo->codigo_banco, $arquivo->layoutVersao);
         $yamlLoad->load($this, 'cnab240', 'retorno/detalhe_segmento_t');
-	}
+    }
 }

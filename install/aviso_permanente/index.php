3a50ecc2d3d1e877ed826fa3e4cda7f1d84c8d94
Alexsander Lima Nazario <alexnazario61@gmail.com>
2024-03-15 11:50:02 -0300

    improve

--- install/aviso_permanente/index.php
+++ install/aviso_permanente/index.php
@@ -1,3 +1,14 @@
 <?php
 // Establishing a connection to the database
-$link = mysql_connect('localhost','root','33#erp@myrouter#3
+$link = mysqli_connect('localhost', 'root', '33#erp@myrouter#3', 'database_name');
+
+// Checking the connection
+if (!$link) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Your database query goes here
+
+// Closing the connection
+mysqli_close($link);
+?>
